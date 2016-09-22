56c2b05cd8092a5b591de220f65bd447db5e1a17
Romans Malinovskis <romans@agiletoolkit.org>
2016-09-22 05:52:08 +0100

    Added travis

---
+++ tests/TemplateTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace atk4\ui\tests;
+
+class TemplateTest extends \atk4\core\PHPUnit_AgileTestCase
+{
+    /**
+     * Test constructor.
+     */
+    public function testTesting()
+    {
+        $this->assertEquals('foo', 'foo');
+    }
+
+}
