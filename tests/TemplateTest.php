8cfb24a23fdbe7795ee19adc3435fd4c95285940
Romans Malinovskis <romans@agiletoolkit.org>
2016-09-22 06:02:29 +0100

    add failing test test

--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -10,6 +10,8 @@ class TemplateTest extends \atk4\core\PHPUnit_AgileTestCase
     public function testTesting()
     {
         $this->assertEquals('foo', 'foo');
+
+        throw new \atk4\core\Exception(["testing", 'foo'=>'bar']);
     }
 
 }
