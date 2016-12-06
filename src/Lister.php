6f8cc943ef7576708ce6acd4b0d6c6fe3c6445ba
Romans Malinovskis <romans@agiletoolkit.org>
2016-12-06 01:56:23 +0000

    add view implementation and lister

--- src/Lister.php
+++ src/Lister.php
@@ -4,4 +4,22 @@ namespace atk4\ui;
 
 class Lister extends View {
 
+    public $t_row    = null;
+    public $t_totals = null;
+
+    public function render()
+    {
+        $this->t_row    = $this->template->cloneRegion('row');
+        //$this->t_totals = isset($this->template['totals']) ? $this->template->cloneRegion('totals') : null;
+
+        $this->template->del('rows');
+
+        foreach ($this->model as $this->current_id => $this->current_row) {
+            $row_html = $this->t_row->set($this->current_row)->render();
+            $this->template->appendHTML('rows', $row_html);
+        }
+
+        return $this->template->render();
+    }
+
 }
--- src/View.php
+++ src/View.php
@@ -7,12 +7,14 @@ namespace atk4\ui;
  */
 class View {
 
-    public $template = null;
+    use \atk4\core\ContainerTrait {
+        add as _add;
+    }
+    use \atk4\core\InitializerTrait;
+    use \atk4\core\TrackableTrait;
+    use \atk4\core\AppScopeTrait;
 
-    /**
-     * When you call render() this will be populated.
-     */
-    public $output;
+    public $template = null;
 
     /**
      * When you call render() this will be populated with JavaScript
@@ -22,16 +24,56 @@ class View {
 
     public $model;
 
+    /**
+     * Relative path to a template
+     */
+    public $default_template = null;
+
     function __construct($template = null) {
 
         if ($template) {
             $this->template = new Template();
             $this->template->load($template);
+        } elseif ($this->default_template) {
+
+            var_Dump(dirname(dirname(__FILE__)));
+
+            $this->template = new Template();
+            $this->template->load($template);
+
+        }
+    }
+
+    function add($object, $spot = 'Content') {
+        $object = $this->_add($object);
+
+        $object->spot = $spot;
+
+
+        if(!$object->template) {
+            $object->template = $this->template->cloneRegion($spot);
+            $this->template->del($spot);
+        }
+        return $object;
+    }
+
+    function recursiveRender() {
+        foreach($this->elements as $view) {
+            if (!$view instanceof View) { 
+                continue;
+            }
+
+            $this->template->appendHTML($view->spot, $view->render());
         }
     }
 
     function render() {
-        $this->output = $this->template->render();
+
+        $this->recursiveRender();
+        
+        return 
+            $this->getJS().
+            $this->template->render();
     }
 
     function setModel($m) {
