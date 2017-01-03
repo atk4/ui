aca5ac025ae2906a71542884624d433c197df6f7
Romans Malinovskis <me@nearly.guru>
2017-01-03 10:33:40 +0000

    Apply fixes from StyleCI

--- src/View.php
+++ src/View.php
@@ -239,7 +239,7 @@ class View implements jsExpressionable
      * In addition to adding a child object, set up it's template
      * and associate it's output with the region in our template.
      *
-     * @param View $object New object to add
+     * @param View   $object New object to add
      * @param string $region
      *
      * @return View
