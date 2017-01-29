35ad3eba1a1160797f0710dd311ca444de22a617
Romans Malinovskis <romans@agiletoolkit.org>
2017-01-29 00:41:37 +0000

    Headers are so cool!!

--- demos/header.php
+++ demos/header.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
-$app = new \atk4\ui\App('Hello World');
+$app = new \atk4\ui\App('Hello World', ['icon'=>'user']);
 $app->initLayout('Centered');
 
 $img = 'https://github.com/atk4/ui/raw/07208a0af84109f0d6e3553e242720d8aeedb784/public/logo.png';
