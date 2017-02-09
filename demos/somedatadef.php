38440781f16aad13d38664d55fac514aeb7a2350
Romans Malinovskis <me@nearly.guru>
2017-02-09 15:51:18 +0000

    Apply fixes from StyleCI

--- demos/somedatadef.php
+++ demos/somedatadef.php
@@ -1,82 +1,82 @@
 <?php
 
-if(!class_exists('SomeData')) {
-class SomeData extends \atk4\data\Model
-{
-    public function __construct()
+if (!class_exists('SomeData')) {
+    class SomeData extends \atk4\data\Model
     {
-        $p = new Persistence_Faker();
+        public function __construct()
+        {
+            $p = new Persistence_Faker();
 
-        parent::__construct($p);
+            parent::__construct($p);
+        }
+
+        public function init()
+        {
+            parent::init();
+            $m = $this;
+
+            $m->addField('name');
+            $m->addField('surname', ['actual'=>'name']);
+            $m->addField('date', ['type'=>'date']);
+        }
     }
 
-    public function init()
+    class Persistence_Faker extends \atk4\data\Persistence
     {
-        parent::init();
-        $m = $this;
+        public $faker = null;
 
-        $m->addField('name');
-        $m->addField('surname', ['actual'=>'name']);
-        $m->addField('date', ['type'=>'date']);
-    }
-}
+        public $count = 50;
 
-class Persistence_Faker extends \atk4\data\Persistence
-{
-    public $faker = null;
-
-    public $count = 50;
-
-    public function __construct($opts = [])
-    {
-        //parent::__construct($opts);
+        public function __construct($opts = [])
+        {
+            //parent::__construct($opts);
 
         if (!$this->faker) {
             $this->faker = Faker\Factory::create();
         }
-    }
-
-    public function prepareIterator($m)
-    {
-        foreach ($this->export($m) as $row) {
-            yield $row;
         }
-    }
 
-    public function export($m, $fields = [])
-    {
-        if (!$fields) {
-            foreach ($m->elements as $name=>$e) {
-                if ($e instanceof \atk4\data\Field) {
-                    $fields[] = $name;
-                }
+        public function prepareIterator($m)
+        {
+            foreach ($this->export($m) as $row) {
+                yield $row;
             }
         }
 
-        $data = [];
-        for ($i = 0; $i < $this->count; $i++) {
-            $row = [];
-            foreach ($fields as $field) {
-                $type = $field;
-
-                if ($field == $m->id_field) {
-                    $row[$field] = $i + 1;
-                    continue;
+        public function export($m, $fields = [])
+        {
+            if (!$fields) {
+                foreach ($m->elements as $name=>$e) {
+                    if ($e instanceof \atk4\data\Field) {
+                        $fields[] = $name;
+                    }
                 }
-
-                $actual = $m->getElement($field)->actual;
-                if ($actual) {
-                    $type = $actual;
-                }
-
-                $row[$field] = $this->faker->$type;
             }
-            $data[] = $row;
-        }
 
-        return array_map(function ($r) use ($m) {
-            return $this->typecastLoadRow($m, $r);
-        }, $data);
+            $data = [];
+            for ($i = 0; $i < $this->count; $i++) {
+                $row = [];
+                foreach ($fields as $field) {
+                    $type = $field;
+
+                    if ($field == $m->id_field) {
+                        $row[$field] = $i + 1;
+                        continue;
+                    }
+
+                    $actual = $m->getElement($field)->actual;
+                    if ($actual) {
+                        $type = $actual;
+                    }
+
+                    $row[$field] = $this->faker->$type;
+                }
+                $data[] = $row;
+            }
+
+            return array_map(function ($r) use ($m) {
+                return $this->typecastLoadRow($m, $r);
+            }, $data);
+        }
     }
-}
 }
