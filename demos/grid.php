5bc66fdacf070a908e6b655b0a7aa2f8f1a62d2e
Romans Malinovskis <me@nearly.guru>
2017-02-14 00:28:09 +0000

    Apply fixes from StyleCI

--- demos/grid.php
+++ demos/grid.php
@@ -19,7 +19,7 @@ $g->addColumn('title', new \atk4\ui\Column\Status([
 ]));
 
 $g->addColumn('date');
-$g->addColumn('salary', new \atk4\ui\Column\Money());//->addClass('right aligned single line', 'all'));
+$g->addColumn('salary', new \atk4\ui\Column\Money()); //->addClass('right aligned single line', 'all'));
 
 $g->addHook('getHTMLTags', function ($grid, $row) {
     if ($row->id == 1) {
