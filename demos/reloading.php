5715fa4e23fc74950f122152fdc9984a79ecfbe5
Romans Malinovskis <me@nearly.guru>
2017-03-12 23:28:44 +0000

    Apply fixes from StyleCI

--- demos/reloading.php
+++ demos/reloading.php
@@ -1,9 +1,10 @@
 <?php
+
 require 'init.php';
 
 // Test 1 - Basic reloading
 $layout->add(['Header', 'Button reloading segment']);
-$v = $layout->add(['View', 'ui'=>'segment'])->set((string)rand(1,100));
+$v = $layout->add(['View', 'ui'=>'segment'])->set((string) rand(1, 100));
 $layout->add(['Button', 'Reload random number'])->js('click', new \atk4\ui\jsReload($v));
 
 // Test 2 - Reloading self
@@ -11,7 +12,7 @@ $layout->add(['Header', 'JS-actions will be re-applied']);
 $b2 = $layout->add(['Button', 'Reload Myself']);
 $b2->js('click', new \atk4\ui\jsReload($b2));
 
-// Test 3 - avoid duplicate 
+// Test 3 - avoid duplicate
 $layout->add(['Header', 'No duplicate JS bindings']);
 $b3 = $layout->add(['Button', 'Reload other button']);
 $b4 = $layout->add(['Button', 'Add one dot']);
--- src/jsReload.php
+++ src/jsReload.php
@@ -3,7 +3,7 @@
 namespace atk4\ui;
 
 /**
- * This class generates action, that will be able to loop-back to the callback method
+ * This class generates action, that will be able to loop-back to the callback method.
  */
 class jsReload implements jsExpressionable
 {
@@ -11,20 +11,21 @@ class jsReload implements jsExpressionable
 
     public $cb = null;
 
-    function __construct($view) {
-
+    public function __construct($view)
+    {
         $this->view = $view;
 
         $this->cb = $this->view->add(new Callback('reload'));
 
-        $this->cb->set(function() {
+        $this->cb->set(function () {
             echo $this->view->render();
             $this->view->app->run_called = true; // prevent shutdown function from triggering.
             exit;
         });
     }
 
-    function jsRender() {
+    public function jsRender()
+    {
 
         // Temporarily here
         //$r = new jsExpression('document.location=[]', [$this->cb->getURL()]);
