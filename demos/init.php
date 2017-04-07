22e1bfe5ae407f5287be965582dcea40498d1a5a
DarkSide <imants.horsts@inbox.lv>
2017-04-07 17:15:57 +0300

    * fix #113 * fix bug in init.php "View Source" button * layouts now work really nicely in demo :)

--- demos/init.php
+++ demos/init.php
@@ -41,8 +41,8 @@ if (isset($layout->leftMenu)) {
 
     $f = basename($_SERVER['PHP_SELF']);
 
-    $url = 'https://github.com/atk4/ui/blob/feature/grid-part2/demos/';
-    //$url = 'https://github.com/atk4/ui/blob/develop/demos/';
+    //$url = 'https://github.com/atk4/ui/blob/feature/grid-part2/demos/';
+    $url = 'https://github.com/atk4/ui/blob/develop/demos/';
 
     // Would be nice if this would be a link.
     $layout->menu->addItem()->add(new \atk4\ui\Button(['View Source', 'teal', 'icon'=>'github']))
--- demos/nolayout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include '../vendor/autoload.php';
-
-$b = new \atk4\ui\Button(['Hello', 'icon'=>'cubes']);
-
-$b->js('click')->hide();
-
-echo htmlspecialchars($b->render());
