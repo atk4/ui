0852c2149d2c94b69876c5d60b330d4b1791c7aa
Romans Malinovskis <romans@agiletoolkit.org>
2017-04-12 09:46:17 +0100

    using un-stable dependencies

--- tests/TableTest.php
+++ tests/TableTest.php
@@ -10,13 +10,14 @@ class TableTest extends \atk4\core\PHPUnit_AgileTestCase
     public function testButtonIcon()
     {
         $b = new \atk4\ui\Table();
+        $b->init();
         $b->setSource([
             ['one'=>1, 'two'=>2, 'three'=>3, 'four'=>4],
             ['one'=> 11, 'two'=>12, 'three'=>13, 'four'=>14],
         ]);
 
         // 4 ways to add column
-        $b->addColumn(null, new \atk4\ui\TableColumn\Link('test.php?id=1'));
+        $b->addColumn(new \atk4\ui\TableColumn\Link('test.php?id=1'));
 
         $b->render();
     }
