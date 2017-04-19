55290345b0b1a37c4efe954681ffbbc509c514b8
Romans Malinovskis <romans@agiletoolkit.org>
2017-04-19 13:33:25 +0100

    Partially fixes #134

--- src/Table.php
+++ src/Table.php
@@ -127,7 +127,7 @@ class Table extends Lister
         }
 
         // No such field or not a string, so use it as columnDef
-        if (!$field) {
+        if (!$field && !$columnDef) {
             $columnDef = $name;
             $name = null;
         }
