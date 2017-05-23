63d0c19e53461f1d52ba786b6c5b4b044cb791ff
Romans Malinovskis <romans@agiletoolkit.org>
2017-05-23 16:23:09 +0100

    cleanup menu logic

--- template/semantic-ui/js/agileui.js
+++ template/semantic-ui/js/agileui.js
@@ -1,40 +1,48 @@
 // This .js file exists only for demonstrational purposes.
 
+
+/*
+
 function leftMenu() {
   var mobileWidth = 991,
       leftMenuWidth = $('.ui.left.sidebar').outerWidth(),
-      windowWidth = $(window).width();
+      windowWidth = $(window).width()
+      menuVisible = $('body').hasClass('atk-leftMenu-visible');
 
-  console.log(mobileWidth, windowWidth, leftMenuWidth);
+  console.log(mobileWidth, windowWidth, leftMenuWidth, menuVisible);
 
+  if ( windowWidth < mobileWidth && menuVisible) {
+    //$('.atk-leftMenuTrigger').click(function(){
+      $('body').removeClass('atk-leftMenu-visible');
+    //});
+  } else if ( windowWidth >= mobileWidth && !menuVisible ) {
+    //$('.atk-leftMenuClose').click();
+    $('body').addClass('atk-leftMenu-visible');
+  }
+
+}
+*/
+
+
+$(function(){
   $('.ui.left.sidebar').prepend('<a href="javascript:void(0)" class="item atk-leftMenuClose"><i class="close icon"></i></a>');
   $('.atk-leftMenuClose').click(function(){
     $('body').removeClass('atk-leftMenu-visible');
   });
 
-  if ( windowWidth < mobileWidth ) {
-    $('.atk-leftMenuTrigger').click(function(){
-      $('body').toggleClass('atk-leftMenu-visible');
-    });
-  } else if ( windowWidth > mobileWidth && $('body.atk-leftMenu-visible').length ) {
-    $('body').removeClass('atk-leftMenu-visible');
-  }
-
-}
-
-
-$(function(){
-
-  leftMenu();
+  //leftMenu();
   $('.atk-leftMenuTrigger').click(function(){
     $('.ui.left.sidebar').toggleClass('visible');
+     $('body').toggleClass('atk-leftMenu-visible');
   });
 
 });
 
 
+/*
 $(window).resize(function() {
 
-  leftMenu();
+  //leftMenu();
 
 });
+*/
