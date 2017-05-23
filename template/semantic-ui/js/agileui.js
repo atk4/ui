2fc75a5e3f7098ea768148970d6cac5891492f50
Mayack <m@agiletech.ie>
2017-05-23 16:55:05 +0300

    Added responsive left menu.

--- template/semantic-ui/js/agileui.js
+++ template/semantic-ui/js/agileui.js
@@ -1,27 +1,40 @@
 // This .js file exists only for demonstrational purposes.
 
 function leftMenu() {
-  var mobileWidth = 768,
+  var mobileWidth = 991,
+      leftMenuWidth = $('.ui.left.sidebar').outerWidth(),
       windowWidth = $(window).width();
 
-  console.log(mobileWidth, windowWidth);
+  console.log(mobileWidth, windowWidth, leftMenuWidth);
+
+  $('.ui.left.sidebar').prepend('<a href="javascript:void(0)" class="item atk-leftMenuClose"><i class="close icon"></i></a>');
+  $('.atk-leftMenuClose').click(function(){
+    $('body').removeClass('atk-leftMenu-visible');
+  });
 
   if ( windowWidth < mobileWidth ) {
-
+    $('.atk-leftMenuTrigger').click(function(){
+      $('body').toggleClass('atk-leftMenu-visible');
+    });
+  } else if ( windowWidth > mobileWidth && $('body.atk-leftMenu-visible').length ) {
+    $('body').removeClass('atk-leftMenu-visible');
   }
 
 }
 
+
 $(function(){
 
   leftMenu();
-
-  $('.leftMenuTrigger').click(function(){
+  $('.atk-leftMenuTrigger').click(function(){
     $('.ui.left.sidebar').toggleClass('visible');
   });
 
 });
 
+
 $(window).resize(function() {
+
   leftMenu();
+
 });
