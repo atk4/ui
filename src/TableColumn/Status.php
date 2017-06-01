886a89ca1596a96bf9f3d4234d80e639f6d5beb8
Romans Malinovskis <romans@agiletoolkit.org>
2017-06-01 16:15:17 +0300

    clean up status clasifications in tables

--- src/TableColumn/Status.php
+++ src/TableColumn/Status.php
@@ -63,8 +63,14 @@ class Status extends Generic
         case 'negative':
             $ic = 'close';
             break;
-        case 'default':
+        case 'disabled':
+            $ic = '';
+            break;
+        case 'unknown':
             $ic = 'question';
+            break;
+        default:
+            $ic = '';
             break;
 
         }
