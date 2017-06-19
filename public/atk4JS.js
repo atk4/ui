1e3e00edac7d512104a8461261ec09d2a160d2b1
Romans Malinovskis <romans@agiletoolkit.org>
2017-06-19 17:15:48 +0100

    Add pre-built version of JS libraries

---
+++ public/atk4JS.js
@@ -0,0 +1,441 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("jQuery"));
+	else if(typeof define === 'function' && define.amd)
+		define("atk4JS", ["jQuery"], factory);
+	else if(typeof exports === 'object')
+		exports["atk4JS"] = factory(require("jQuery"));
+	else
+		root["atk4JS"] = factory(root["jQuery"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_0__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_0__;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.default = plugin;
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* https://gist.github.com/monkeymonk/c08cb040431f89f99928132ca221d647 */
+
+/**
+ * Generate a jQuery plugin
+ * @param pluginName [string] Plugin name
+ * @param className [object] Class of the plugin
+ * @param shortHand [bool] Generate a shorthand as $.pluginName
+ *
+ * @example
+ * import plugin from 'plugin';
+ *
+ * class MyPlugin {
+ *     constructor(element, options) {
+ *         // ...
+ *     }
+ * }
+ *
+ * MyPlugin.DEFAULTS = {};
+ *
+ * plugin('myPlugin', MyPlugin);
+ */
+function plugin(pluginName, className) {
+    var shortHand = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+    var dataName = '__' + pluginName;
+    var old = _jquery2.default.fn[pluginName];
+
+    _jquery2.default.fn[pluginName] = function (option) {
+        return this.each(function () {
+            var $this = (0, _jquery2.default)(this);
+            var data = $this.data(dataName);
+            var options = _jquery2.default.extend({}, className.DEFAULTS, $this.data(), (typeof option === 'undefined' ? 'undefined' : _typeof(option)) === 'object' && option);
+
+            if (!data) {
+                $this.data(dataName, data = new className(this, options));
+            }
+
+            if (typeof option === 'string') {
+                data[option]();
+            }
+        });
+    };
+
+    // - Short hand
+    if (shortHand) {
+        _jquery2.default[pluginName] = function (options) {
+            return (0, _jquery2.default)({})[pluginName](options);
+        };
+    }
+
+    // - No conflict
+    _jquery2.default.fn[pluginName].noConflict = function () {
+        return _jquery2.default.fn[pluginName] = old;
+    };
+}
+module.exports = exports['default'];
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var addParams = function addParams(element, options) {
+    _classCallCheck(this, addParams);
+
+    var url = options.url;
+    if (!_jquery2.default.isEmptyObject(options.params)) {
+        url += (url.indexOf('?') >= 0 ? '&' : '?') + _jquery2.default.param(options.params);
+    }
+
+    return url;
+};
+
+exports.default = addParams;
+
+
+addParams.DEFAULTS = {
+    url: null,
+    params: {}
+};
+module.exports = exports['default'];
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ajaxec = function ajaxec(element, options) {
+    _classCallCheck(this, ajaxec);
+
+    var $element = (0, _jquery2.default)(element);
+
+    // ask for user confirmation just before
+    // TODO: because this is constructor, button can't be clicked again :(
+    if (options.confirm) {
+        if (!confirm(options.confirm)) {
+            return;
+        }
+    }
+
+    $element.api({
+        on: 'now',
+        url: options.uri,
+        data: options.uri_options,
+        method: 'POST',
+        obj: $element
+    });
+};
+
+exports.default = ajaxec;
+
+
+ajaxec.DEFAULTS = {
+    uri: null,
+    uri_options: {},
+    confirm: null
+};
+module.exports = exports['default'];
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var reloadView = function reloadView(element, options) {
+    _classCallCheck(this, reloadView);
+
+    var $element = (0, _jquery2.default)(element);
+
+    $element.spinner({
+        'loaderText': '',
+        'active': true,
+        'inline': true,
+        'centered': true,
+        'replace': true });
+
+    if (options.uri) {
+        _jquery2.default.get(options.uri, options.uri_options, function (data) {
+            $element.replaceWith(data);
+        });
+    }
+};
+
+exports.default = reloadView;
+
+
+reloadView.DEFAULTS = {
+    uri: null,
+    uri_options: {}
+};
+module.exports = exports['default'];
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var spinner = function () {
+    function spinner(element, options) {
+        _classCallCheck(this, spinner);
+
+        var $element = (0, _jquery2.default)(element);
+
+        // Remove any existing dimmers/spinners
+        $element.remove('.dimmer');
+        $element.remove('.spinner');
+
+        var $baseDimmer = (0, _jquery2.default)(options.baseDimmerMarkup);
+        var $baseLoader = (0, _jquery2.default)(options.baseLoaderMarkup);
+
+        var $finalSpinner = null;
+
+        $baseLoader.toggleClass('active', options.active);
+        $baseLoader.toggleClass('indeterminate', options.indeterminate);
+        $baseLoader.toggleClass('centered', options.centered);
+        $baseLoader.toggleClass('inline', options.inline);
+
+        var isText = !!options.loaderText;
+        if (isText) {
+            $baseLoader.toggleClass('text', true);
+            $baseLoader.text(options.loaderText);
+        }
+
+        if (options.dimmed) {
+            $baseDimmer.toggleClass('active', options.active);
+            $finalSpinner = $baseDimmer.append($baseLoader);
+        } else {
+            $finalSpinner = $baseLoader;
+        }
+
+        // If replace is true we remove the existing content in the $element.
+        this.showSpinner($element, $finalSpinner, options.replace);
+    }
+
+    _createClass(spinner, [{
+        key: 'showSpinner',
+        value: function showSpinner($element, $spinner) {
+            var replace = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+            if (replace) $element.empty();
+
+            $element.append($spinner);
+        }
+    }]);
+
+    return spinner;
+}();
+
+exports.default = spinner;
+
+
+spinner.DEFAULTS = {
+    active: false,
+    replace: false,
+    dimmed: false,
+    inline: false,
+    indeterminate: false,
+    loaderText: 'Loading',
+    centered: false,
+    baseDimmerMarkup: '<div class="ui dimmer"></div>',
+    baseLoaderMarkup: '<div class="ui loader"></div>'
+};
+module.exports = exports['default'];
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+var _plugin = __webpack_require__(1);
+
+var _plugin2 = _interopRequireDefault(_plugin);
+
+var _spinner = __webpack_require__(5);
+
+var _spinner2 = _interopRequireDefault(_spinner);
+
+var _reloadView = __webpack_require__(4);
+
+var _reloadView2 = _interopRequireDefault(_reloadView);
+
+var _ajaxec = __webpack_require__(3);
+
+var _ajaxec2 = _interopRequireDefault(_ajaxec);
+
+var _addParams = __webpack_require__(2);
+
+var _addParams2 = _interopRequireDefault(_addParams);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Register our plugins
+
+
+// Import our plugins
+(0, _plugin2.default)('spinner', _spinner2.default);
+(0, _plugin2.default)('reloadView', _reloadView2.default);
+(0, _plugin2.default)('ajaxec', _ajaxec2.default);
+(0, _plugin2.default)('addParams', _addParams2.default);
+
+_jquery2.default.addParams = function (url, data) {
+    if (!_jquery2.default.isEmptyObject(data)) {
+        url += (url.indexOf('?') >= 0 ? '&' : '?') + _jquery2.default.param(data);
+    }
+
+    return url;
+};
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=atk4JS.js.map
