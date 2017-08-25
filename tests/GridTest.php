bb689068d03f084db1f3e113bfe2aaed7778dc11
Romans Malinovskis <romans@agiletoolkit.org>
2017-08-25 13:55:16 +0100

    work in progress

--- tests/GridTest.php
+++ tests/GridTest.php
@@ -12,8 +12,8 @@ class GridTest extends \atk4\core\PHPUnit_AgileTestCase
     public function setUp()
     {
         $a = [];
-        $a[1] = ['email'=>'test@test.com', 'password'=>'abc123'];
-        $a[2] = ['email'=>'test@yahoo.com', 'password'=>'secret'];
+        $a[1] = ['id'=>1, 'email'=>'test@test.com', 'password'=>'abc123', 'xtra'=>'xtra'];
+        $a[2] = ['id'=>2, 'email'=>'test@yahoo.com', 'password'=>'secret'];
 
         $this->m = new MyModel(new \atk4\data\Persistence_Array($a));
     }
@@ -21,16 +21,56 @@ class GridTest extends \atk4\core\PHPUnit_AgileTestCase
     public function test1()
     {
         $t = new Table();
-        $t->addColumn('hello');
+        $t->setModel($this->m, false);
+
+        $t->addColumn('email');
         $t->addColumn(new Template('password={$password}'));
+
+        $this->assertEquals('<td>{$email}</td><td>password={$password}</td>', $t->getDataRowHTML());
+        $this->assertEquals(
+            '<tr data-id="1"><td>test@test.com</td><td>password=abc123</td></tr>',
+            $this->extract($t)
+        );
     }
 
+    public function test1a()
+    {
+        $t = new Table();
+        $t->setModel($this->m, false);
+
+        $t->addColumn('email');
+        $t->addColumn('password');
+
+        $this->assertEquals('<td>{$email}</td><td>***</td>', $t->getDataRowHTML());
+        $this->assertEquals(
+            '<tr data-id="1"><td>test@test.com</td><td>***</td></tr>',
+            $this->extract($t)
+        );
+    }
     public function test2()
     {
         $t = new Table();
-        $t->setModel($this->m);
+        $t->setModel($this->m, ['email']);
+        $t->addColumn('TableColumn/Delete');
 
-        //$t->addColumn('hello');
+        $this->assertEquals('<td>{$email}</td><td><a href="#" title="Delete {$email}?" class="delete"><i class="ui red trash icon"></i>Delete</a></td>', $t->getDataRowHTML());
+        $this->assertEquals(
+            '<tr data-id="1"><td>test@test.com</td><td><a href="#" title="Delete test@test.com?" class="delete"><i class="ui red trash icon"></i>Delete</a></td></tr>',
+            $this->extract($t)
+        );
+    }
+
+    public function test3()
+    {
+        $t = new Table();
+        $t->setModel($this->m, ['email']);
+        $t->addColumn('xtra', ['type'=>'password']);
+
+        $this->assertEquals('<td>{$email}</td><td>***</td>', $t->getDataRowHTML());
+        $this->assertEquals(
+            '<tr data-id="1"><td>test@test.com</td><td>***</td></tr>',
+            $this->extract($t)
+        );
     }
 
     public function extract($t)
@@ -39,14 +79,13 @@ class GridTest extends \atk4\core\PHPUnit_AgileTestCase
         $val = $t->render();
         preg_match('/<.*data-id="1".*/m', $val, $matches);
 
-        var_dump($matches[0]);
-
         return $matches[0];
     }
 }
 
 class MyModel extends \atk4\data\Model
 {
+    public $title_field='email';
     public function init()
     {
         parent::init();
