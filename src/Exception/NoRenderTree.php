0324dd4b5ba265c28dccdb175bf9405c3649204d
Romans Malinovskis <romans@agiletoolkit.org>
2017-09-14 12:45:47 +0100

    I think this was used somewhere, forgot to add

---
+++ src/Exception/NoRenderTree.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace atk4\ui\Exception;
+
+class NoRenderTree extends \atk4\ui\Exception {
+    function __construct($object, $action = '') { 
+        parent::__construct(['You must use either add($obj) or $obj->init() before '.($action ?: 'performing this action'), 'obj'=>$object]);
+    }
+}
