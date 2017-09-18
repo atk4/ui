875be2ece40c0fd5a1c9ea78172610005d46e384
DarkSide <imants.horsts@inbox.lv>
2017-09-18 11:06:53 +0300

    fix some tests

--- tests/TableTest.php
+++ tests/TableTest.php
@@ -7,18 +7,22 @@ class TableTest extends \atk4\core\PHPUnit_AgileTestCase
     /**
      * Test constructor.
      */
-    public function testButtonIcon()
+    public function testAddColumnWithoutModel()
     {
-        $b = new \atk4\ui\Table();
-        $b->init();
-        $b->setSource([
+        $t = new \atk4\ui\Table();
+        $t->init();
+        $t->setSource([
             ['one'=>1, 'two'=>2, 'three'=>3, 'four'=>4],
             ['one'=> 11, 'two'=>12, 'three'=>13, 'four'=>14],
         ]);
 
-        // 4 ways to add column
-        $b->addColumn(new \atk4\ui\TableColumn\Link('test.php?id=1'));
+        // multiple ways to add column which doesn't exist in model
+        $t->addColumn('five', new \atk4\ui\TableColumn\Link('test.php?id=1'));
+        $t->addColumn('six', [new \atk4\ui\TableColumn\Link('test.php?id=2')]);
+        $t->addColumn('seven', ['Link', 'id'=>3]);
+        $t->addColumn('eight', 'Link');
+        $t->addColumn('nine');
 
-        $b->render();
+        $t->render();
     }
 }
