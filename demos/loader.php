9b1107d2166b2be08d9fc7005343ee6f7bdddf49
Romans Malinovskis <me@nearly.guru>
2017-10-16 13:04:16 +0000

    Apply fixes from StyleCI

--- demos/loader.php
+++ demos/loader.php
@@ -1,8 +1,9 @@
 <?php
+
 require 'init.php';
 
 $s = $app->add(['ui'=>'green segment'])->setStyle('min-height', '20em');
-$b = $app->add(['Button','Click me']);
+$b = $app->add(['Button', 'Click me']);
 $b->on('click', new \atk4\ui\jsReload($s, ['a'=>'yes']));
 
 if (isset($_GET['a'])) {
