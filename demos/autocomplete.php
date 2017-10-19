064c0ecebc0b43c74030b491c5777d8b8278a7ae
Alain Belair <belair.alain@gmail.com>
2017-10-19 16:30:53 -0400

    support for icon and label
    Add support for icon and label beside input field.
    Icon still has some problem.

--- demos/autocomplete.php
+++ demos/autocomplete.php
@@ -11,7 +11,7 @@ $form = $layout->add(new \atk4\ui\Form(['segment']));
 $form->add(['Label', 'Input new country information here', 'top attached'], 'AboveFields');
 
 $form->setModel(new Country($db), false);
-
+//$form->addField('test');
 $form->addField('name', ['AutoComplete']);
 
 $form->onSubmit(function ($f) {
@@ -21,3 +21,24 @@ $form->onSubmit(function ($f) {
     return $notifier;
 });
 
+$layout->add(new \atk4\ui\FormField\AutoComplete(['placeholder'=>'Search users', 'left'=>true, 'icon'=>'users']));
+
+$layout->add(new \atk4\ui\Header(['Labels', 'size'=>2]));
+
+$layout->add(new \atk4\ui\FormField\AutoComplete(['placeholder'=>'Search users', 'label'=>'http://']));
+$layout->add(new \atk4\ui\FormField\AutoComplete(['placeholder'=>'Weight', 'labelRight'=>new \atk4\ui\Label(['kg', 'basic'])]));
+$layout->add(new \atk4\ui\FormField\AutoComplete(['label'=>'$', 'labelRight'=>new \atk4\ui\Label(['.00', 'basic'])]));
+
+$layout->add(new \atk4\ui\FormField\AutoComplete([
+    'iconLeft'  => 'tags',
+    'labelRight'=> new \atk4\ui\Label(['Add Tag', 'tag']),
+]));
+
+// left/right corner is not supported, but here is work-around:
+$label = new \atk4\ui\Label();
+$label->addClass('left corner');
+$label->add(new \atk4\ui\Icon('asterisk'));
+
+$layout->add(new \atk4\ui\FormField\AutoComplete([
+    'label'=> $label,
+]))->addClass('left corner');
--- src/FormField/AutoComplete.php
+++ src/FormField/AutoComplete.php
@@ -2,31 +2,38 @@
 
 namespace atk4\ui\FormField;
 
-use atk4\ui\Form;
+use atk4\ui\jQuery;
 
 class AutoComplete extends Input
 {
     public $defaultTemplate = 'formfield/autocomplete.html';
-    public $ui = 'search selection dropdown';
+    public $ui = 'input';
     public $searchClassName = 'search';
 
     public function init()
     {
         parent::init();
+        $this->template->set('input_id', $this->name.'-ac');
 
-        $this->js(true)->dropdown([
-            'fields'      => ['name' => 'name', 'value'=>'id'],
+        $this->template->set('place_holder', $this->placeholder);
+
+        $chain = new jQuery('#'.$this->name.'-ac');
+
+        $chain->dropdown([
+            'fields'      => ['name' => 'name', 'value' => 'id', 'text' => 'description'],
             'apiSettings' => [
                 'mockResponse' => [
                     'success' => true,
                     'results' => [
-                        ['name' => 'd1', 'id' => 'd1v'],
-                        ['name' => 'd2', 'id' => 'd2v'],
-                        ['name' => 'd3', 'id' => 'd3v'],
+                        ['name' => 'd1', 'id' => 'd1v', 'description' => 'd1 apple'],
+                        ['name' => 'd2', 'id' => 'd2v', 'description' => 'd2 google'],
+                        ['name' => 'd3', 'id' => 'd3v', 'description' => 'd3 yahoo'],
                     ]
                 ],
-            ]
+            ],
+            'filterRemoteData'  => true,
         ]);
+        $this->js(true, $chain);
     }
 
     /**
@@ -37,10 +44,9 @@ class AutoComplete extends Input
         return $this->app->getTag('input', [
             'name'       => $this->short_name,
             'type'       => 'hidden',
-            'placeholder'=> $this->placeholder,
+            /*'placeholder'=> $this->placeholder,*/
             'id'         => $this->id.'_input',
             'value'      => $this->getValue(),
         ]);
-        //return '<input name="'.$this->short_name.'" type="'.$this->inputType.'" placeholder="'.$this->placeholder.'" id="'.$this->id.'_input"/>';
     }
 }
