ee2405df55710a95efd88b09f1040b6ab5e611a4
Alain Belair <belair.alain@gmail.com>
2017-10-25 14:29:44 -0400

    Revert "apply PR comment request."
    This reverts commit 3b495017af34f91cc088f8e1e4b4604a8530e690.

--- demos/loader.php
+++ demos/loader.php
@@ -3,8 +3,8 @@
 require 'init.php';
 
 /**** Start on page load ***/
-$app->add(['Header', 'Attach loader to default view and start it on page load.']);
-$l1 = $app->add('Loader');
+$layout->add(['Header', 'Attach loader to default view and start it on page load.']);
+$l1 = $layout->add('Loader');
 $l1->needOnPageLoad = true;
 
 $l1->set(function ($loader) {
@@ -13,10 +13,10 @@ $l1->set(function ($loader) {
     $loader->add(new \atk4\ui\Message(['text' => 'Load using page load']));
 });
 
-$app->add(['Header', 'Attach loader in supplied view and start it using an action.']);
+$layout->add(['Header', 'Attach loader in supplied view and start it using an action.']);
 
 /*** Start from a user action ***/
-$l2 = $app->add(new \atk4\ui\Loader(['loader'=> new atk4\ui\View(['ui'=> 'segment'])]));
+$l2 = $layout->add(new \atk4\ui\Loader(['loader'=> new atk4\ui\View(['ui'=> 'segment'])]));
 $l2->set(function ($loader) {
     //set your time expensive function here.
     sleep(2);
@@ -24,16 +24,16 @@ $l2->set(function ($loader) {
 });
 
 //Start via user action.
-$b = $app->add(['Button', 'Start Loader']);
+$b = $layout->add(['Button', 'Start Loader']);
 $b->on('click', $l2->jsStartLoader());
 
 /*** Start from a user action in a button ***/
-$app->add(['Header', 'Attach loader to supplied button and start it via same button.']);
+$layout->add(['Header', 'Attach loader to supplied button and start it via same button.']);
 
 $btn_load = new atk4\ui\Button();
 $btn_load->set(['Load', 'primary']);
 
-$l3 = $app->add(new \atk4\ui\Loader(['loader'=> $btn_load]));
+$l3 = $layout->add(new \atk4\ui\Loader(['loader'=> $btn_load]));
 $l3->set(function ($loader) {
     //set your time expensive function here.
     sleep(2);
--- src/Loader.php
+++ src/Loader.php
@@ -33,24 +33,24 @@ class Loader extends View
     /**
      * Set callback function for this loader.
      *
-     * The loader view is pass as an argument to the loader callback function.
-     * This allow to easily update the loader view content within the callback.
+     * The loader view is pass as an argument to the loader set function.
+     * This allow to easily update the loader view content.
      *  $l1 = $layout->add('Loader');
      *  $l1->set(function ($loader_view) {
      *    do_long_processing_action();
      *    $loader_view->set('new content');
      *  });
      *
-     * @param callable $fx
-     * @param array $args
+     * @param array|string $fx
+     * @param null $args
      *
      * @return $this
      * @throws Exception
      */
-    public function set($fx, $args = [])
+    public function set($fx, $args = null)
     {
         if (!is_object($fx) && !($fx instanceof Closure)) {
-            throw new Exception('Error: Need to pass a closure function to Loader::set()');
+            throw new Exception('Error: Need to pass a function to Loader::set()');
         }
 
         $this->loaderCallback = $this->loader->add('CallbackLater');
@@ -60,7 +60,10 @@ class Loader extends View
             $this->app->terminate($this->renderJSON());
         } else {
             if ($this->needOnPageLoad) {
-                $this->jsStartLoader(true, $args);
+                $this->loader->js(true)->atkReloadView([
+                    'uri'         => $this->loaderCallback->getURL(),
+                    'uri_options' => $args,
+                ]);
             }
         }
 
@@ -69,8 +72,7 @@ class Loader extends View
 
     /**
      * Return loader callback url when set.
-     *
-     * @return string|null
+     * @return null
      */
     public function getLoaderUrl()
     {
@@ -80,14 +82,13 @@ class Loader extends View
     /**
      * Return a js action that will triggered the loader to start.
      *
-     * @param bool|null $when
      * @param array $args
      *
      * @return mixed
      */
-    public function jsStartLoader($when = null, $args = [])
+    public function jsStartLoader($args = [])
     {
-        return $this->loader->js($when)->atkReloadView([
+        return $this->loader->js()->atkReloadView([
             'uri'         => $this->loaderCallback->getURL(),
             'uri_options' => $args,
         ]);
