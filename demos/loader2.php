a84cd7062b42c4bbf5495af81f069dff894b574c
Romans Malinovskis <romans@agiletoolkit.org>
2017-10-29 02:11:48 +0300

    fix loader2

--- demos/loader2.php
+++ demos/loader2.php
@@ -10,7 +10,7 @@ $grid->setModel(new Country($db), ['name']);
 
 $country_loader = $c->addColumn()->add(['Loader', 'loadEvent'=>false, 'shim'=>['Text', 'Select country on your left']]);
 
-$grid->table->onRowClick($country_loader->jsLoad(['id'=>123]));
+$grid->table->onRowClick($country_loader->jsLoad(['id'=>$grid->table->jsRow()->data('id')]));
 
 $country_loader->set(function($p) use($db) {
     $p->add('Form')->setModel(new Country($db))->load($_GET['id']);
