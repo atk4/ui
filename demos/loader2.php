589745676135424b6abbf71bca74842fa21d9c42
Romans Malinovskis <romans@agiletoolkit.org>
2017-10-29 01:57:44 +0300

    using loader to load country form

---
+++ demos/loader2.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'init.php';
+require 'database.php';
+
+$c = $app->add('Columns');
+
+$grid = $c->addColumn()->add(['Grid', 'ipp'=>10, 'menu'=>false]);
+$grid->setModel(new Country($db), ['name']);
+
+$country_loader = $c->addColumn()->add(['Loader', 'loadEvent'=>false, 'shim'=>['Text', 'Select country on your left']]);
+
+$grid->table->onRowClick($country_loader->jsLoad(['id'=>123]));
+
+$country_loader->set(function($p) use($db) {
+    $p->add('Form')->setModel(new Country($db))->load($_GET['id']);
+});
+
