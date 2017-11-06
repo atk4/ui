5c1b1b695dc82161484b9e1ae736e8d594a38e43
Romans Malinovskis <romans@agiletoolkit.org>
2017-11-06 10:46:51 +0200

    produce local JS files

--- tools/test.sh
+++ tools/test.sh
@@ -6,6 +6,7 @@ chmod 777 coverage
 cp behat-travis.yml behat.yml
 cp tools/coverage.php demos/coverage.php
 sed -i "s|node_modules_path.*|node_modules_path: `npm get prefix`/lib/node_modules|g" behat.yml
+(cd js; npm run build)
 
 echo "Running UI tests"
 ./vendor/bin/behat || exit -1
