3607861e3ab5fb678c0b5abb072df78709269968
Romans Malinovskis <me@nearly.guru>
2017-12-04 15:39:26 +0000

    Apply fixes from StyleCI

--- src/Console.php
+++ src/Console.php
@@ -9,8 +9,7 @@ namespace atk4\ui;
  * Console is a black square component resembling terminal window. It can be programmed
  * to run a job and output results to the user.
  */
-class Console extends View
-    implements \Psr\Log\LoggerInterface
+class Console extends View implements \Psr\Log\LoggerInterface
 {
     public $ui = 'inverted black segment';
     public $element = 'pre';
@@ -19,20 +18,21 @@ class Console extends View
 
     public $sse;
 
-    public function set($callback) {
+    public function set($callback)
+    {
         $this->sse = $this->add('jsSSE');
         //$this->output('bleh');
-        $this->sse->set(function() use($callback) {
+        $this->sse->set(function () use ($callback) {
             $this->sseInProgress = true;
+
             try {
                 call_user_func($callback, $this);
             } catch (\atk4\core\Exception $e) {
                 $lines = explode("\n", $e->getHTMLText());
 
-                foreach($lines as $line) {
+                foreach ($lines as $line) {
                     $this->outputHTML($line);
                 }
-
             } catch (\Error $e) {
                 $this->output('Error: '.$e->getMessage());
             } catch (\Exception $e) {
@@ -41,20 +41,23 @@ class Console extends View
             $this->sseInProgress = false;
         });
         $this->js(true, $this->sse);
+
         return $this;
     }
 
     /**
-     * Output a single line to the console
+     * Output a single line to the console.
      */
-    public function output($text) {
+    public function output($text)
+    {
         $this->sse->send($this->js()->append(htmlspecialchars($text).'<br/>'));
     }
 
     /**
-     * Output un-escaped HTML line
+     * Output un-escaped HTML line.
      */
-    public function outputHTML($text) {
+    public function outputHTML($text)
+    {
         $this->sse->send($this->js()->append($text.'<br/>'));
     }
 
@@ -70,10 +73,11 @@ class Console extends View
     public function runCommand($exec, $args = [])
     {
         if (!$this->sseInProgress) {
-            $this->set(function() {
+            $this->set(function () {
                 $this->runCommand($exec, $args);
             });
-            return null;
+
+            return;
         }
 
         // TODO: implement command
@@ -84,17 +88,18 @@ class Console extends View
 
     /**
      * Execute method of a certain model. That's a short-hand method
-     * for running:
+     * for running:.
      *
      * $app->add('Console')->setModel(new User($db), 'generateReports');
-     *
      */
-    public function setModel(\atk4\data\Model $model, string $method, $args = []) {
+    public function setModel(\atk4\data\Model $model, string $method, $args = [])
+    {
         if (!$this->sseInProgress) {
-            $this->set(function() use ($model, $method, $args) {
+            $this->set(function () use ($model, $method, $args) {
                 $this->setModel($model, $method, $args);
             });
-            return null;
+
+            return;
         }
 
         // temporarily override app logging
@@ -109,39 +114,48 @@ class Console extends View
         $model->app->logger = $old_logger;
     }
 
-    function emergency($str, $args = []) {
-        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str)."</font>");
+    public function emergency($str, $args = [])
+    {
+        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str).'</font>');
     }
 
-    function alert($str, $args = []) {
-        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str)."</font>");
+    public function alert($str, $args = [])
+    {
+        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str).'</font>');
     }
 
-    function critical($str, $args = []) {
-        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str)."</font>");
+    public function critical($str, $args = [])
+    {
+        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str).'</font>');
     }
 
-    function error($str, $args = []) {
-        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str)."</font>");
+    public function error($str, $args = [])
+    {
+        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str).'</font>');
     }
 
-    function warning($str, $args = []) {
-        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str)."</font>");
+    public function warning($str, $args = [])
+    {
+        return $this->outputHTML("<font color='pink'>".htmlspecialchars($str).'</font>');
     }
 
-    function notice($str, $args = []) {
-        return $this->outputHTML("<font color='yellow'>".htmlspecialchars($str)."</font>");
+    public function notice($str, $args = [])
+    {
+        return $this->outputHTML("<font color='yellow'>".htmlspecialchars($str).'</font>');
     }
 
-    function info($str, $args = []) {
-        return $this->outputHTML("<font color='gray'>".htmlspecialchars($str)."</font>");
+    public function info($str, $args = [])
+    {
+        return $this->outputHTML("<font color='gray'>".htmlspecialchars($str).'</font>');
     }
 
-    function debug($str, $args = []) {
-        return $this->outputHTML("<font color='cyan'>".htmlspecialchars($str)."</font>");
+    public function debug($str, $args = [])
+    {
+        return $this->outputHTML("<font color='cyan'>".htmlspecialchars($str).'</font>');
     }
 
-    function log($level, $str, $args = []) {
+    public function log($level, $str, $args = [])
+    {
         return $this->$level($str, $args);
     }
 
