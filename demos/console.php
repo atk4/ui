20fb779607408be3f796c91409354a4fceaf4159
Romans Malinovskis <me@nearly.guru>
2017-12-09 17:13:02 +0000

    Update console.php

--- demos/console.php
+++ demos/console.php
@@ -34,32 +34,3 @@ $app->add('Console')->set(function ($console) {
 });
 
 $app->add('Console')->setModel($app->add(new Test()), 'generateReport');
-
-exit;
-$app->add(['ui' => 'divider']);
-
-$bar = $app->add(['View', 'template' => new \atk4\ui\Template('<div id="{$_id}" class="ui teal progress">
-  <div class="bar"></div>
-  <div class="label">Testing SSE</div>
-</div>')]);
-$bar->js(true)->progress();
-
-$button = $app->add(['Button', 'Turn On']);
-// non-SSE way
-//$button->on('click', $bar->js()->progress(['percent'=> 40]));
-
-$sse = $app->add(['jsSSE', 'showLoader' => true]);
-
-$button->on('click', $sse->set(function () use ($sse, $bar) {
-    $sse->send($bar->js()->progress(['percent' => 20]));
-    sleep(0.5);
-    $sse->send($bar->js()->progress(['percent' => 40]));
-    sleep(1);
-    $sse->send($bar->js()->progress(['percent' => 60]));
-    sleep(2);
-    $sse->send($bar->js()->progress(['percent' => 80]));
-    sleep(1);
-
-    // non-SSE way
-    return $bar->js()->progress(['percent' => 100]);
-}));
