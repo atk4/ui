34fdd994e61684f248bdfed100ab18d351177816
Imants Horsts <DarkSide666@users.noreply.github.com>
2017-12-18 00:18:46 +0200

    cover more cases in demo page

--- demos/form6.php
+++ demos/form6.php
@@ -15,6 +15,13 @@ $f->addField('two', ['Radio'], ['enum'=>['female', 'male']])->set('male');
 
 $f->addField('three', null, ['values'=>['female', 'male']])->set(1);
 $f->addField('four', ['Radio'], ['values'=>['female', 'male']])->set(1);
+
+$f->addField('five', null, ['values'=>[5=>'female', 7=>'male']])->set(7);
+$f->addField('six', ['Radio'], ['values'=>[5=>'female', 7=>'male']])->set(7);
+
+$f->addField('seven', null, ['values'=>['F'=>'female', 'M'=>'male']])->set('M');
+$f->addField('eight', ['Radio'], ['values'=>['F'=>'female', 'M'=>'male']])->set('M');
+
 $f->onSubmit(function ($f) {
     echo json_encode($f->model->get());
 });
