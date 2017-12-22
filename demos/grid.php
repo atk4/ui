f7990fc163aa19c9effc526275696cdf1902ba2b
Romans Malinovskis <romans@agiletoolkit.org>
2017-12-22 16:46:47 +0000

    improve test coverage, add demo of Modal

--- demos/grid.php
+++ demos/grid.php
@@ -19,6 +19,11 @@ $g->addAction('Say HI', function ($j, $id) use ($g) {
     return 'Loaded "'.$g->model->load($id)['name'].'" from ID='.$id;
 });
 
+$g->addModalAction(['icon'=>'external'], 'Modal Test', function ($p, $id) {
+    $p->add(['Message', 'Clicked on ID='.$id]);
+});
+
+
 $sel = $g->addSelection();
 $g->menu->addItem('show selection')->on('click', new \atk4\ui\jsExpression(
     'alert("Selected: "+[])', [$sel->jsChecked()]
