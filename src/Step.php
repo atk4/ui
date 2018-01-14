220f20acdaa97638463cfc916abe594e7c20b927
Romans Malinovskis <romans@agiletoolkit.org>
2018-01-14 15:35:52 +0000

    Added initial implementation

---
+++ src/Step.php
@@ -0,0 +1,49 @@
+<?php
+
+// vim:ts=4:sw=4:et:fdm=marker:fdl=0
+
+namespace atk4\ui;
+
+/**
+ * One step of the wizard
+ */
+class Step extends View
+{
+
+    /**
+     * Use the parent's template
+     */
+    public $defaultTemplate = null;
+
+    public $title = null;
+
+    public $description = null;
+
+    public $wizard = null;
+
+    public $icon = null;
+
+    /**
+     * Will be assigned 0, 1, 2, etc, 
+     */
+    public $sequence = null;
+
+    function __construct($title) { 
+        $this->title = $title;
+    }
+
+    public function renderView()
+    {
+        $this->template->set('title', $this->title);
+        $this->template->set('description', $this->description);
+
+
+        if ($this->icon == false) {
+            $this->template->del('has_icon');
+        } else {
+            $this->template->set('icon', $this->icon);
+        }
+
+        parent::renderView();
+    }
+}
---
+++ src/Wizard.php
@@ -0,0 +1,177 @@
+<?php
+
+// vim:ts=4:sw=4:et:fdm=marker:fdl=0
+
+namespace atk4\ui;
+
+/**
+ * Wizard widget
+ */
+class Wizard extends View
+{
+
+    use \atk4\core\SessionTrait;
+
+    public $defaultTemplate = 'wizard.html';
+    public $ui = 'steps';
+
+    /**
+     * Callback, that triggers selection of a step
+     */
+    public $stepCallback = null;
+
+    /**
+     * List of steps
+     */
+    public $steps = [];
+
+    /**
+     * Current step
+     */
+    public $currentStep = null;
+
+    public $buttonPrev = null;
+
+    public $buttonNext = null;
+
+    /**
+     * Icon that will be used on all steps by default.
+     *  - 'empty' , since no such icon exists, no visible icon will be used unless step is completed
+     *  - 'square outline', use this (or any other) Semantic UI icon by default
+     *  - false,  disables icons alltogether (or using checkboxes for completed steps)
+     */
+    public $defaultIcon = 'empty'; // 'square outline'
+
+    public function init()
+    {
+        parent::init();
+        $this->stepCallback = $this->add('Callback');
+
+        $this->currentStep = $this->stepCallback->triggered() ?: 0;
+
+        $this->stepTemplate = $this->template->cloneRegion('Step');
+        $this->template->del('Step');
+
+        // add buttons
+        if ($this->currentStep) {
+            $this->buttonPrev = $this->add(['Button', 'Back', 'basic'], 'Left');
+            $this->buttonPrev->link($this->stepCallback->getURL($this->currentStep-1));
+        }
+
+        $this->buttonNext = $this->add(['Button', 'Next', 'primary'], 'Right');
+
+        $this->buttonNext->link($this->stepCallback->getURL($this->currentStep+1));
+    }
+
+    /**
+     * Adds step to the wizard.
+     *
+     * @param mixed $name   Name of tab or Tab object
+     * @param mixed $action Optional callback action or URL (or array with url + parameters)
+     *
+     * @return View
+     */
+    public function addStep($name, $callback)
+    {
+        $step = $this->factory([
+            'Step', 
+            'wizard' => $this, 
+            'template'=>clone $this->stepTemplate,
+            'sequence'=>count($this->steps),
+        ], $name);
+
+        // add tabs menu item
+        $this->steps[] = $this->add($step, 'Step');
+
+        if ($step->sequence == $this->currentStep) {
+            $step->addClass('active');
+
+            $this->stepCallback->set($callback, [$this]);
+        } elseif ($step->sequence < $this->currentStep) {
+            $step->addClass('completed');
+        }
+
+        if ($step->icon == null) {
+            $step->icon = $this->defaultIcon;
+        }
+
+        return $step;
+    }
+
+    public function addFinish($callback)
+    {
+        if (count($this->steps) == $this->currentStep + 1) {
+            $this->buttonFinish = $this->add(['Button', 'Finish', 'primary'], 'Right');
+            $this->buttonFinish->link($this->stepCallback->getURL( count($this->steps) ));
+        } elseif ($this->currentStep == count($this->steps)) {
+
+            $this->buttonPrev->destroy();
+            $this->buttonNext->addClass('disabled')->set('Completed');
+
+            $res = call_user_func($callback, $this);
+
+
+        }
+
+    }
+
+    public function add($seed, $region = null)
+    {
+        $result = parent::add($seed, $region);
+
+        if ($result instanceof Form) {
+            // mingle with the button icon
+            $result->buttonSave->destroy();
+            $result->buttonSave = null;
+
+            $this->buttonNext->on('click', $result->js()->submit());
+        }
+
+        return $result;
+    }
+
+    public function urlNext()
+    {
+        return $this->stepCallback->getURL($this->currentStep + 1);
+    }
+
+    public function jsNext()
+    {
+        return new jsExpression('document.location = []', [$this->urlNext()]);
+    }
+
+
+    public function recursiveRender()
+    {
+        if (!$this->steps) {
+            $this->addStep(['No Steps Defined', 'icon'=>'configure', 'description'=>'use $wizard->addStep() now'], function($p){ 
+                $p->add(['Message', 'Step content will appear here', 'type'=>'error', 'text'=>'Specify callback to addStep() which would populate this area.']);
+            });
+        }
+
+        if (count($this->steps) == $this->currentStep + 1) {
+            $this->buttonNext->destroy();
+        }
+
+
+        parent::recursiveRender();
+    }
+
+    public function renderView()
+    {
+        // Set proper width to the wizard
+        $c = count($this->steps);
+        $enumeration = ['one', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+        $this->ui = $enumeration[$c].' '.$this->ui;
+
+        if ($c>6) {
+            $this->addClass('mini');
+        } elseif($c>5) {
+            $this->addClass('tiny');
+        } elseif($c>4) {
+            $this->addClass('small');
+        }
+
+        parent::renderView();
+    }
+}
