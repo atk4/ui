e04756dd09143f14b94f29574d0ee7f0915c134f
Romans Malinovskis <romans@agiletoolkit.org>
2018-01-16 13:00:04 +0000

    add demo for new view-based sticky arguments

---
+++ demos/sticky2.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'init.php';
+
+$b = $app->add(['Button', 'b1='.@$_GET['b1']]);
+$b->link(['b1'=>$b->stickyGet('b1')+1]);
+
+
+
+
+$b = $app->add(['Button', 'b2='.@$_GET['b2']]);
+$b->link(['b2'=>$b->stickyGet('b2')+1]);
