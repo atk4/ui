e7465e24ffc9e0841cc31aad5e0a83b9c84f132e
Romans Malinovskis <romans@agiletoolkit.org>
2018-01-17 18:02:53 +0000

    perform on execution later, as it relies on table's url()

--- src/TableColumn/Delete.php
+++ src/TableColumn/Delete.php
@@ -20,15 +20,16 @@ class Delete extends Generic
             $this->table->app->terminate($reload->renderJSON());
         });
 
+    }
+
+    public function getDataCellTemplate(\atk4\data\Field $f = null)
+    {
         $this->table->on('click', 'a.'.$this->short_name)->atkAjaxec([
             'uri'         => $this->vp->getURL(),
             'uri_options' => [$this->name => $this->table->jsRow()->data('id')],
             'confirm'     => (new \atk4\ui\jQuery())->attr('title'),
         ]);
-    }
 
-    public function getDataCellTemplate(\atk4\data\Field $f = null)
-    {
         return $this->app->getTag(
             'a',
             ['href' => '#', 'title' => 'Delete {$'.$this->table->model->title_field.'}?', 'class' => $this->short_name],
