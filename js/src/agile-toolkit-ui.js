03a420e08c4a7ac490f67473942b010dd5fa0499
Alain Belair <belair.alain@gmail.com>
2018-01-26 12:45:35 -0500

    package update
    - Add version function;
    - a long due Readme file update.

--- js/src/agile-toolkit-ui.js
+++ js/src/agile-toolkit-ui.js
@@ -2,8 +2,10 @@ import atk from 'atk-semantic-ui';
 import 'helpers/addParams';
 import registerPlugin from './plugin';
 
+atk.version = function(){return _ATKVERSION_}
 //Allow to register a plugin with jQuery;
 atk.registerPlugin = registerPlugin;
+
 
 /**
  * Exporting services in order to be available globally
