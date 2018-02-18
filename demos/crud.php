e0279bf9149d02a5b6d23b857d02da6d1b3e0588
Romans Malinovskis <me@nearly.guru>
2018-02-18 12:13:40 +0000

    Apply fixes from StyleCI

--- demos/crud.php
+++ demos/crud.php
@@ -24,21 +24,21 @@ $crud = $cc->add([
 $crud->setModel(new Country($app->db))->addCondition('numcode', '<', 200);
 
 // Because CRUD inherits Grid, you can also define custom actions
-$crud->addModalAction(['icon'=>'cogs'], 'Details', function($p, $id) use($crud) { 
+$crud->addModalAction(['icon'=>'cogs'], 'Details', function ($p, $id) use ($crud) {
     $p->add(['Message', 'Details for: '.$crud->model->load($id)['name'].' (id: '.$id.')']);
 });
-
-
 
 $cc = $c->addColumn();
 $cc->add(['Header', 'Cutomizations']);
 
-class MyVP extends \atk4\ui\VirtualPage {
+class MyVP extends \atk4\ui\VirtualPage
+{
     public $l;
     public $r;
     public $f;
 
-    function init() {
+    public function init()
+    {
         parent::init();
 
         $col = parent::add('Columns');
@@ -46,11 +46,13 @@ class MyVP extends \atk4\ui\VirtualPage {
         $this->r = $col->addColumn();
     }
 
-    function add($seed, $arg = null) {
+    public function add($seed, $arg = null)
+    {
         return $this->f = $this->l->add($seed, $arg);
     }
 
-    function renderView() {
+    public function renderView()
+    {
         if ($this->f instanceof \atk4\ui\Form) {
             if ($this->f->model['is_folder']) {
                 $this->r->add(['Grid', 'menu'=>false, 'ipp'=>5])
@@ -59,6 +61,7 @@ class MyVP extends \atk4\ui\VirtualPage {
                 $this->r->add(['Message', 'Not a folder', 'warning']);
             }
         }
+
         return parent::renderView();
     }
 }
@@ -66,15 +69,12 @@ class MyVP extends \atk4\ui\VirtualPage {
 $crud = $cc->add([
     'CRUD',
     'canCreate'       => false,
-    'canDelete'    => false,
-    'pageUpdate' => ['\MyVP'],
-    'ipp'          => 5,
+    'canDelete'       => false,
+    'pageUpdate'      => ['\MyVP'],
+    'ipp'             => 5,
 ]);
 
 $crud->menu->addItem(['Rescan', 'icon'=>'recycle']);
 
 // Condition on the model can be applied after setting the model
 $crud->setModel(new File($app->db))->addCondition('parent_folder_id', null);
-
-
-
