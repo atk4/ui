d4560192f1c700fc9e5fb9dc91d23dc8dcba94a9
Romans Malinovskis <romans@agiletoolkit.org>
2018-03-06 14:08:45 +0000

    Improvements to console, new demo and more documentation

--- demos/console.php
+++ demos/console.php
@@ -8,28 +8,58 @@ class Test extends \atk4\data\Model
 
     public function generateReport()
     {
-        $this->log('info', 'Starting long process');
-        $this->debug('test=123');
-        sleep(1);
-        $this->debug('test=321');
-        sleep(5);
+        $this->log('info', 'Console will automatically pick up output from all DebugTrait objects');
+        $this->debug('debug {foo}', ['foo'=>'bar']);
+        $this->emergency('emergency {foo}', ['foo'=>'bar']);
+        $this->alert('alert {foo}', ['foo'=>'bar']);
+        $this->critical('critical {foo}', ['foo'=>'bar']);
+        $this->error('error {foo}', ['foo'=>'bar']);
+        $this->warning('warning {foo}', ['foo'=>'bar']);
+        $this->notice('notice {foo}', ['foo'=>'bar']);
+        $this->info('info {foo}', ['foo'=>'bar']);
 
         return 123;
     }
 }
 
-$app->add('Console')->set(function ($console) {
+$tt = $app->add('Tabs');
+
+$t = $tt->addTab('set()');
+$t->add(['Header','icon'=>'terminal', 'Console output streaming', 'subHeader'=>'any output your PHP script produces through console is displayed to user in real-time']);
+$t->add('Console')->set(function ($console) {
     $console->output('Executing test process...');
     sleep(1);
     $console->output('Now trying something dangerous..');
     sleep(1);
+    echo 'direct output is cautpred captured';
 
-    throw new \atk4\data\Exception('BOOM');
-    $console->output('hello there');
+    throw new \atk4\data\Exception('BOOM - exceptions are caught');
 
-    //$console->runCommand('ls', ['/etc']);
-
-    //$console->send(new \atk4\ui\jsExpression('alert(1)'));
 });
 
-$app->add('Console')->setModel($app->add(new Test()), 'generateReport');
+$t = $tt->addTab('runMethod()', function($t) {
+    $t->add(['Header','icon'=>'terminal', 'Non-interractive method invocation', 'subHeader'=>'console can invoke a method, which normaly would be non-interractive and can still capture debug output']);
+    $t->add('Console')->runMethod($t->add(new Test()), 'generateReport');
+});
+
+$t = $tt->addTab('exec() single', function($t) {
+    $t->add(['Header','icon'=>'terminal', 'Command execution', 'subHeader'=>'it is easy to run server-side commands and stream output through console']);
+    $t->add('Console')->exec('/bin/pwd');
+});
+
+
+$t = $tt->addTab('exec() chain', function($t) {
+    $t->add(['Header','icon'=>'terminal', 'Command execution', 'subHeader'=>'it is easy to run server-side commands and stream output through console']);
+    $t->add('Console')->set(function($c){ 
+        $c
+            ->exec('/sbin/ping', ['-c', '5', '-i', '1', '192.168.0.1'])
+            ->exec('/sbin/ping', ['-c', '5', '-i', '2', '8.8.8.8'])
+            ->exec('/bin/no-such-command')
+            ;
+    });
+});
+
+$t = $tt->addTab('composer update', function($t) {
+    $t->add(['Header','icon'=>'terminal', 'Command execution', 'subHeader'=>'it is easy to run server-side commands and stream output through console']);
+    $t->add('Console')->exec('bash', ['-c', 'cd ..; echo "Running \'composer update\' in `pwd`"; composer --no-ansi update; echo "Self-updated. OK to refresh now!"']) ;
+});
