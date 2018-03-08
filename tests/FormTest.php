ee91b46a483b2ab3f8764a7b713fd63ace90e364
Romans Malinovskis <romans@agiletoolkit.org>
2018-03-08 14:06:41 +0000

    add form test

---
+++ tests/FormTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace atk4\ui\tests;
+
+class FormTest extends \atk4\core\PHPUnit_AgileTestCase
+{
+    /**
+     * Some tests for form
+     *
+     */
+    function testGetField()
+    {
+        $f = new \atk4\ui\Form();
+        $f->init();
+
+        $f->addField('test');
+
+        $this->assertTrue($f->getField('test') instanceof \atk4\ui\FormField\Generic);
+        $this->assertInstanceOf(\atk4\ui\FormField\Generic::class, $f->layout->getField('test'));
+    }
+}
