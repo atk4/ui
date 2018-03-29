c09f0f7f72220709cfc28425fb37d6fb66def9bd
Romans Malinovskis <me@nearly.guru>
2018-03-29 15:40:00 +0000

    Apply fixes from StyleCI

--- demos/jscondform.php
+++ demos/jscondform.php
@@ -21,36 +21,33 @@ $f_phone->js(true)->atkConditionalForm(['fieldRules' => [
 
 $app->add(['Header', 'Optional subscription', 'size'=>2]);
 
-
-$f_sub = $app->add( new \atk4\ui\Form( ['segment']));
+$f_sub = $app->add(new \atk4\ui\Form(['segment']));
 $f_sub->add(['Label', 'Click on subscribe and add email to receive your gift.', 'top attached'], 'AboveFields');
 
 $f_sub->addField('name');
 $f_sub->addField('subscribe', ['CheckBox', 'Subscribe to weekly newsletter', 'toggle']);
 $f_sub->addField('email');
-$f_sub->addField('gender',  ['Radio'], ['enum'=>['Female', 'Male']])->set('Female');
+$f_sub->addField('gender', ['Radio'], ['enum'=>['Female', 'Male']])->set('Female');
 $f_sub->addField('m_gift', ['DropDown', 'caption'=>'Gift for Men', 'values' => ['Beer Glass', 'Swiss Knife']]);
 $f_sub->addField('f_gift', ['DropDown', 'caption'=>'Gift for Women', 'values' => ['Wine Glass', 'Lipstick']]);
-
 
 $f_sub->js(true)->atkConditionalForm(['fieldRules' => [
                                                         'email' => ['subscribe' => 'checked'],
                                                         'gender'=> ['subscribe' => 'checked'],
                                                         'm_gift'=> ['gender' => 'isExactly[Male]', 'subscribe' => 'checked'],
                                                         'f_gift'=> ['gender' => 'isExactly[Female]', 'subscribe' => 'checked'],
-                                                     ]
+                                                     ],
                                     ]);
 
 $app->add(['Header', 'Dog registration', 'size'=>2]);
 
-
-$f_dog = $app->add( new \atk4\ui\Form( ['segment']));
+$f_dog = $app->add(new \atk4\ui\Form(['segment']));
 $f_dog->add(['Label', 'You can select type of hair cut only with race that contains "poodle" AND age no more than 5 year OR your dog race is exactly "bichon".', 'top attached'], 'AboveFields');
 $f_dog->addField('race', ['Line']);
 $f_dog->addField('age');
 $f_dog->addField('hair_cut', ['DropDown', 'values' => ['Short', 'Long']]);
 
-$f_dog->js( true)->atkConditionalForm( [ 'fieldRules' => [
-                                        'hair_cut' => [['race' => 'contains[poodle]', 'age'=>'integer[0..5]'],['race' => 'isExactly[bichon]']],
-                                    ]
+$f_dog->js(true)->atkConditionalForm(['fieldRules' => [
+                                        'hair_cut' => [['race' => 'contains[poodle]', 'age'=>'integer[0..5]'], ['race' => 'isExactly[bichon]']],
+                                    ],
                                    ]);
