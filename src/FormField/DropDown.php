0ee20c87ec137608826e414eb6f013300a9b4eb0
PhilippGrashoff <33204878+PhilippGrashoff@users.noreply.github.com>
2018-04-10 20:16:49 +0200

    Fix #418
    Fix for #418, allows empty selection in a dropdown once a "real" value is selected (was not possible before).
    As there is an open bug in Semantic UI for this, this might change with future sematic UI Versions:
    https://github.com/Semantic-Org/Semantic-UI/issues/2072

--- src/FormField/DropDown.php
+++ src/FormField/DropDown.php
@@ -18,7 +18,17 @@ class DropDown extends Input
     public function init()
     {
         parent::init();
-        $this->jsInput(true)->dropdown();
+        //See https://github.com/atk4/ui/issues/418
+        //if field is required, disable empty selection once a value is
+        //selected. Currently standard behaviour of sematic ui dropdown
+        if(isset($this->field) && $this->field->required) {
+            $this->jsInput(true)->dropdown();
+        }
+        //add any (does not have to be $this->empty) placeholder to allow
+        //empty selection even after a value was selected
+        else {
+            $this->jsInput(true)->dropdown(['placeholder' => $this->empty]);
+        }
     }
 
     /**
