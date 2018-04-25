1a6288181df506befa2f1b0f0de378d0883641e7
Alain Belair <belair.alain@gmail.com>
2018-04-25 10:16:03 -0400

    cleanup demo file

--- demos/tablefilter.php
+++ demos/tablefilter.php
@@ -6,7 +6,10 @@ require 'database.php';
 //For popup positioning to work correctly, table need to be inside a view segment.
 $view = $app->add('View', ['ui' => 'basic segment']);
 $g = $view->add(['Grid']);
-$g->setModel(new Country($db));
 
-$g->addFilterColumn(/*['name']*/);
-$g->ipp = 10;
+$g->setModel(new Country($db));
+$g->addFilterColumn();
+
+$g->ipp = 20;
+
+
