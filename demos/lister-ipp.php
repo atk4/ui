2205ae2c3b3567fed4b32389efa65c229e546a47
Romans Malinovskis <me@nearly.guru>
2018-05-08 22:02:23 +0000

    Apply fixes from StyleCI

--- demos/lister-ipp.php
+++ demos/lister-ipp.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'init.php';
 require 'database.php';
 
@@ -9,16 +10,16 @@ $v = $container->add(['View', 'template' => new \atk4\ui\Template('
 {List}<li class="ui icon label"><i class="{iso}ae{/} flag"></i> {name}andorra{/}</li>{/}
 </ul>{$Content}</div>')]);
 
-$l = $v->add('Lister', 'List') ->addHook('beforeRow', function ($l) {
+$l = $v->add('Lister', 'List')->addHook('beforeRow', function ($l) {
     $l->current_row['iso'] = strtolower($l->current_row['iso']);
 });
 
 $m = $l->setModel(new Country($db))->setLimit(10);
 
-
 $ipp = $v->add(new atk4\ui\ItemsPerPageSelector(['label' => 'Select how many countries:']), 'Content');
 
 $ipp->onPageLengthSelect(function ($ipp) use ($m, $container) {
     $m->setLimit($ipp);
+
     return $container;
 });
