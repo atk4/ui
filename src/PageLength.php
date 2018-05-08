42d193c7a3f84f8460b5904fd39391e478080365
Mayack <me@mayack.com>
2018-05-08 11:27:47 +0100

    Added static HTML for pageLength object.

--- src/PageLength.php
+++ src/PageLength.php
@@ -14,6 +14,7 @@ class PageLength extends View
      * @var View|null
      */
     public $pageLength = null;
+    public $defaultTemplate = 'pagelength.html';
 
     /**
      * Default page length menu items.
