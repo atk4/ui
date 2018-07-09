64c487dbb80edf387d597cb653606f784b65b508
Alain Belair <belair.alain@gmail.com>
2018-07-09 14:56:27 -0400

    minor fix
    restore app

--- src/App.php
+++ src/App.php
@@ -287,7 +287,7 @@ class App
 
         // Agile UI
         $url = isset($this->cdn['atk']) ? $this->cdn['atk'] : '../public';
-        $this->requireJS($url.'/atkjs-ui.js');
+        $this->requireJS($url.'/atkjs-ui.min.js');
         $this->requireJS($url.'/agileui.js');
         $this->requireCSS($url.'/agileui.css');
     }
