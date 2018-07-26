403e8d37b8d04f7d53fad9dc6bbf17082e77e6f6
Alain Belair <belair.alain@gmail.com>
2018-07-26 12:27:15 -0400

    fix typo

--- js/src/atk-semantic-ui.js
+++ js/src/atk-semantic-ui.js
@@ -14,7 +14,7 @@ popupService.setPopups($.fn.popup.settings);
 if (typeof FormSerializer != "undefined") {
   //setup jQuery FormSerializer to accept in input name with dash char (-)
   $.extend(FormSerializer.patterns, {
-    validate: /^[_a-z][a-z0-9_-]*(?:\[(?:\d*|[a-z0-9_-]+)\])*$/i,
+    validate: /^[a-z_][a-z0-9_-]*(?:\[(?:\d*|[a-z0-9_-]+)\])*$/i,
     key:      /[a-z0-9_-]+|(?=\[\])/gi,
     named:    /^[a-z0-9_-]+$/i
   });
