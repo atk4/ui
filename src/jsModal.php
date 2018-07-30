ac7846c4313a5fce2ca9845406da748c733c7d2f
Alain Belair <belair.alain@gmail.com>
2018-07-30 12:52:41 -0400

    feature/jsModal options

--- src/jsModal.php
+++ src/jsModal.php
@@ -7,12 +7,61 @@ namespace atk4\ui;
  */
 class jsModal extends jsExpression
 {
-    public function __construct($title, $url, $args = [], $mode = 'json')
+    public function __construct($title, $url, $args = [], $mode = 'json', $hasHeader = true)
     {
         if ($url instanceof VirtualPage) {
             $url = $url->getJSURL('cut');
         }
 
         parent::__construct('$(this).atkCreateModal([arg])', ['arg' => ['uri' => $url, 'title' => $title, 'mode' => $mode, 'uri_options' => $args]]);
+
+        if (!$hasHeader) {
+            $this->removeHeader();
+        }
+    }
+
+    /**
+     * Set additionnal option for this jsModal.
+     *
+     * Valuable option are headerCss and label:
+     *  'headerCss' -> customize css class name for the header.
+     *      ex: changing color text for header
+     *      $jsModal->setOption('headerCss', 'ui blue header');
+     *
+     *  'label' -> set the text loader value.
+     *      ex: changing default 'Loading...' for no text
+     *      $jsModal->setOption('label', '');
+     *
+     * You can set option individually or supply an array.
+     *
+     * @param $options
+     * @param null $value
+     *
+     * @return $this
+     */
+    public function setOption($options, $value = null)
+    {
+        if (is_array($options)) {
+            foreach ($options as $key => $value) {
+                $this->args['arg'][$key] = $value;
+            }
+        } else {
+            $this->args['arg'][$options] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Clear header class and title.
+     *
+     * @return $this
+     */
+    public function removeHeader()
+    {
+        $this->args['arg']['headerCss'] = '';
+        $this->args['arg']['title'] = '';
+
+        return $this;
     }
 }
