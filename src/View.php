7cd5f4fe6e1dbad4d4711a3bcff9520e7125d87c
Romans Malinovskis <me@nearly.guru>
2018-08-13 07:11:26 +0000

    Apply fixes from StyleCI

--- src/View.php
+++ src/View.php
@@ -347,9 +347,9 @@ class View implements jsExpressionable
     protected function initDefaultApp()
     {
         $this->app = new App([
-            'skin' => $this->skin, 
-            'catch_exceptions' => false, 
-            'always_run' => false,
+            'skin'                    => $this->skin,
+            'catch_exceptions'        => false,
+            'always_run'              => false,
             'catch_runaway_callbacks' => false,
         ]);
         $this->app->init();
