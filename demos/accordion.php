cfc8d4b128852a7e21727f88003d0b5b805b083c
Romans Malinovskis <me@nearly.guru>
2018-11-07 21:13:18 +0000

    Apply fixes from StyleCI

--- demos/accordion.php
+++ demos/accordion.php
@@ -8,7 +8,7 @@ $i = $acc->addItem('I am a static Item');
 $i->add(['Message', 'This content is added on page loaded']);
 $i->add(['LoremIpsum', 'size' => 1]);
 
-$i = $acc->addItem('I am a dynamic Item', function($v){
+$i = $acc->addItem('I am a dynamic Item', function ($v) {
     $v->add(['Message', 'Every time you open this accordion item, you will see a different text']);
     $v->add(['LoremIpsum', 'size' => 2]);
 });
