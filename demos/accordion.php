760fb9565701de17d8d3e491e9faf4fd176e26a4
Romans Malinovskis <me@nearly.guru>
2018-11-07 21:06:50 +0000

    Apply fixes from StyleCI

--- demos/accordion.php
+++ demos/accordion.php
@@ -2,12 +2,12 @@
 
 require 'init.php';
 
-$acc = $app->add(['Accordion', 'type' => ['styled','fluid']]);
+$acc = $app->add(['Accordion', 'type' => ['styled', 'fluid']]);
 
 $i = $acc->addItem('Bravo');
 $i->add(['Text', 'slfjlksf slkjf lskjf']);
 
-$i = $acc->addItem('Alpha', function($v){
+$i = $acc->addItem('Alpha', function ($v) {
     $v->add(['Message', 'Every time you open this accordion item, you will see a different text']);
     $v->add(['LoremIpsum', 'size' => 2]);
 });
