4ffb209080a0f69e9927a4e359b43bbae8c49cb9
Alain Belair <belair.alain@gmail.com>
2018-11-09 13:56:09 -0500

    refacrtor form layout
    Use Accordion form layout for thighter integrarion with accordion.

--- demos/accordion-in-form.php
+++ demos/accordion-in-form.php
@@ -2,42 +2,39 @@
 
 require 'init.php';
 
-$f = $app->add(['Form', 'layout' => 'Flexible']);
+$f = $app->add(['Form', 'layout' => 'Accordion']);
 
-$acc = $f->layout->addView(['Accordion', 'type' => ['styled', 'fluid']]);
+$contact_section = $f->layout->addSection('Contact');
 
-//Contact field in accordion section 1.
-//Start by enabling addField to accordion section 1 by adding a form layout to it.
-$acc_item_1 = $f->layout->addLayoutSection($item_1 = $acc->addSection('Contact'));
-
-$gr = $acc_item_1->addGroup('Name');
+$gr = $contact_section->addGroup('Name');
 $gr->addField('first_name', ['width' => 'eight']);
 $gr->addField('last_name', ['width' => 'eight']);
-$gr = $acc_item_1->addGroup('Email');
+
+$gr = $contact_section->addGroup('Email');
 $gr->addField('email', ['width' => 'sixteen'], ['caption' => 'yourEmail@domain.com']);
 
-//Address field in accordion section 2.
-//Start by enabling addField to accordion section 2.
-$acc_item_2 = $f->layout->addLayoutSection($acc->addSection('Address'));
-$gr = $acc_item_2->addGroup('Street and City');
+$adr_section = $f->layout->addSection('Address');
+
+$gr = $adr_section->addGroup('Street and City');
 $gr->addField('address1', ['width' => 'eight']);
 $gr->addField('city', ['width' => 'eight']);
 
-$gr = $acc_item_2->addGroup('State, Country and Postal Code');
+$gr = $adr_section->addGroup('State, Country and Postal Code');
 $gr->addField('state', ['width' => 'six']);
 $gr->addField('country', ['width' => 'six']);
 $gr->addField('postal', ['width' => 'four']);
 
 $f->addField('term', ['CheckBox', 'caption'=>'Accept terms and conditions', null, 'slider']);
 
-$f->onSubmit(function ($f) use ($acc, $item_1) {
+$f->layout->getAccordion()->activate($f->layout->getSection($contact_section));
+
+$f->onSubmit(function ($f) use ($contact_section){
     if (!$f->model['first_name']) {
         // return field error and open proper accordion item where field is located.
         return [
             $f->error('first_name', 'Your first name is required.'),
-            $acc->jsOpen($item_1),
+            $f->layout->getAccordion()->jsOpen($f->layout->getSection($contact_section)),
             ];
     }
 });
 
-$acc->activate($item_1);
