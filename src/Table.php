c907bf12ee03d87e6ae343ff46245ed7f331eccb
Romans Malinovskis <me@nearly.guru>
2018-11-12 15:39:20 +0000

    Apply fixes from StyleCI

--- src/Table.php
+++ src/Table.php
@@ -273,8 +273,8 @@ class Table extends Lister
     /**
      * Add a dynamic paginator, i.e. when user is scrolling content.
      *
-     * @param int   $ipp     Number of item per page to start with.
-     * @param array $options An array with js Scroll plugin options.
+     * @param int    $ipp          Number of item per page to start with.
+     * @param array  $options      An array with js Scroll plugin options.
      * @param View   $container    The container holding the lister for scrolling purpose. Default to view owner.
      * @param string $scrollRegion A specific template region to render. Render output is append to container html element.
      *
