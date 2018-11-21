b9d64537f9edebe0ec74686003f8993b99072daf
Alain Belair <belair.alain@gmail.com>
2018-11-21 10:56:03 -0500

    Section property not needed.

--- src/FormLayout/Section/Accordion.php
+++ src/FormLayout/Section/Accordion.php
@@ -4,15 +4,14 @@ namespace atk4\ui\FormLayout\Section;
 
 class Accordion extends \atk4\ui\Accordion
 {
-    public $section = null;
     public $formLayout = 'FormLayout/Generic';
     public $form = null;
 
     public function addSection($title, $icon = 'dropdown')
     {
-        $this->section = parent::addSection($title, null, $icon);
+        $section = parent::addSection($title, null, $icon);
 
-        return $this->section->add([$this->formLayout, 'form' => $this->form]);
+        return $section->add([$this->formLayout, 'form' => $this->form]);
     }
 
     public function getSectionIdx($section)
