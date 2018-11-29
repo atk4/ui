d6fe133e82ffbed096b54115a718d9c8d6ca4371
Romans Malinovskis <romans@agiletoolkit.org>
2018-11-29 00:00:17 +0000

    working on coverage

--- tools/coverage.php
+++ tools/coverage.php
@@ -6,16 +6,15 @@ use SebastianBergmann\CodeCoverage\CodeCoverage;
 
 $coverage = new CodeCoverage();
 
-$coverage->filter()->addDirectoryToWhitelist('../src');
-$coverage->filter()->addDirectoryToWhitelist('src');
+$coverage->filter()->addDirectoryToWhitelist(dirname(realpath(__FILE__)).'/src');
 
 register_shutdown_function(function () use ($coverage) {
     $coverage->stop();
 
-    $writer = new \SebastianBergmann\CodeCoverage\Report\PHP();
+    $writer = new \SebastianBergmann\CodeCoverage\Report\Clover();
 
-    $output = $writer->process($coverage, null);
-    file_put_contents(dirname(realpath(__FILE__)).'/../coverage/'.basename($_SERVER['SCRIPT_NAME'], '.php').'-'.uniqid().'.cov', $output);
+    $output = $writer->process($coverage, dirname(realpath(__FILE__)).'/../coverage/'.basename($_SERVER['SCRIPT_NAME'], '.php').'-'.uniqid().'.xml');
+#    file_put_contents(dirname(realpath(__FILE__)).'/../coverage/'.basename($_SERVER['SCRIPT_NAME'], '.php').'-'.uniqid().'.cov', $output);
 });
 
 $coverage->start($_SERVER['SCRIPT_NAME']);
