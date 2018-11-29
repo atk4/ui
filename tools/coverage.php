668d44ed5ab235da898a406a55f5ef0aed58eaf2
Romans Malinovskis <romans@agiletoolkit.org>
2018-11-29 10:26:22 +0000

    write both reports, just so i can see file sizes

--- tools/coverage.php
+++ tools/coverage.php
@@ -13,8 +13,10 @@ register_shutdown_function(function () use ($coverage) {
     $coverage->stop();
 
     $writer = new \SebastianBergmann\CodeCoverage\Report\PHP();
+    $clover = new \SebastianBergmann\CodeCoverage\Report\Clover();
 
     $output = $writer->process($coverage, dirname(realpath(__FILE__)).'/../coverage/'.basename($_SERVER['SCRIPT_NAME'], '.php').'-'.uniqid().'.cov');
+    $clover->process($coverage, dirname(realpath(__FILE__)).'/../coverage/'.basename($_SERVER['SCRIPT_NAME'], '.php').'-'.uniqid().'.xml');
 });
 
 $coverage->start($_SERVER['SCRIPT_NAME']);
