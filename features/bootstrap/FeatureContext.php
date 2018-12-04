20677fc13380744ec8efce04047dee8cee7500b1
Romans Malinovskis <romans@agiletoolkit.org>
2018-12-04 11:44:54 +0000

    work in progress - adding semantic dropdown manipulation for unit tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -145,4 +145,68 @@ class FeatureContext extends RawMinkContext implements Context
         //TODO: zombiejs does not support sse :(
         //var_dump($element->getOuterHtml());
     }
+
+    /**
+     * @Then I select :arg1 in dropdown :arg2
+     *
+     * Finds a dropdown, opens it, waits, selects specified value and waits for it to close
+     */
+    public function iSelectInDropdown($select_option, $css_selector)
+    {
+
+
+        // TODO: not sure if initial wait is needed
+        $dropdown_arrow = $this->webDriver->wait(5)->until(
+            \WebDriverExpectedCondition::elementToBeClickable(\WebDriverBy::cssSelector($css_selector.' i.dropdown.icon'))
+        );
+        usleep(100000);
+
+
+        // expand the menu
+        $dropdown_arrow->click();
+
+        // wait until options are visible
+        $this->webDriver->wait(5,200)->until(
+            \WebDriverExpectedCondition::visibilityOfElementLocated(\WebDriverBy::cssSelector($css_selector.' div.menu.visible'))
+        );
+
+        //store current value for later rollback
+        // TODO: probably not needed for our tests
+        $this->dropDownInitiallySelected = $this->webDriver->findElement(\WebDriverBy::cssSelector($css_selector.' div.menu div.selected'))->getAttribute('data-value');
+
+        //select a non-selected element
+        $dropdown_options = $this->webDriver->findElements(\WebDriverBy::cssSelector($css_selector.' div.menu div'));
+        $option_selected = false;
+
+        //if no option to select was specified, select some which is not
+        //empty and not selected yet
+        if($select_option === null)  {
+            foreach($dropdown_options as $option) {
+                //do not select the show all option (....)
+                if($option->getAttribute('data-value') == '') {
+                    continue;
+                }
+                //do not select the option already active
+                if(strpos($option->getAttribute('class'), 'selected') !== false) {
+                    continue;
+                }
+                $option_selected = $option->getAttribute('data-value');
+                $option->click();
+                break;
+            }
+        } else {
+            //select specific value
+            $option = $this->webDriver->findElement(\WebDriverBy::cssSelector($css_selector.' div.menu div[data-value="'.$select_option.'"]'));
+            $option_selected = $option->getAttribute('data-value');
+            $option->click();
+
+            // TODO - need to assert that option exists here
+        }
+
+        // wait for dropdown menu to disappear
+        if($wait_menu_disappear) {
+            $this->waitUntilInvisible($css_selector.' div.menu.visible');
+        }
+    }
+
 }
