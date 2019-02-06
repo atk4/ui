4ade65a0b7f2bebc918c5c668a472af63079bd64
Svetlozar Kondakov <skondakoff@gmail.com>
2019-02-06 11:01:19 +0200

    Add Table Column Image

---
+++ src/TableColumn/Image.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace atk4\ui\TableColumn;
+
+
+/**
+ * Column for formatting image.
+ */
+class Image extends \atk4\ui\TableColumn\Generic
+{
+    // overrides
+    public $attr = ['all' => ['class' => ['center aligned single line']]];
+
+    public function getDataCellTemplate(\atk4\data\Field $f = null)
+    {
+        return '<img src="' . parent::getDataCellTemplate($f) . '" />';
+    }
+}
