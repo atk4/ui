5cc759aaeb5bad204ae733d41c9ad085d50830bc
Imants Horsts <DarkSide666@users.noreply.github.com>
2019-02-25 22:35:32 +0200

    Feature/implement grid layout (#662)
    * implementation plus small demo
    
    * Apply fixes from StyleCI
    
    * fix tests
    
    * Apply fixes from StyleCI

--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -47,16 +47,19 @@ class DemoTest extends \atk4\core\PHPUnit_AgileTestCase
     public function demoList()
     {
         $copy_paste = trim('
+  accordion.php
+  accordion-nested.php
   autocomplete.php
-  button.php
   breadcrumb.php
-  js.php
+  button.php
   card.php
   checkbox.php
   columns.php
   console.php
   crud.php
   crud2.php
+  crud3.php
+  dropdown-plus.php
   field.php
   field2.php
   form.php
@@ -64,17 +67,17 @@ class DemoTest extends \atk4\core\PHPUnit_AgileTestCase
   form3.php
   form4.php
   form5.php
+  form6.php
+  form-custom-layout.php
+  form-section.php
+  form-section-accordion.php
   grid.php
+  grid-layout.php
   header.php
   index.php
   init.php
+  js.php
   jscondform.php
-  label.php
-  layouts.php
-  lister.php
-  lister-ipp.php
-  loader.php
-  loader2.php
   menu.php
   message.php
   modal.php
@@ -82,18 +85,44 @@ class DemoTest extends \atk4\core\PHPUnit_AgileTestCase
   notify.php
   notify2.php
   paginator.php
+  progress.php
   recursive.php
   reloading.php
+  scroll-container.php
+  scroll-grid.php
+  scroll-grid-container.php
+  scroll-lister.php
+  scroll-table.php
   sse.php
   sticky.php
+  sticky2.php
   table.php
   table2.php
   tablecolumnmenu.php
   tabs.php
+  toast.php
+  upload.php
   view.php
   virtual.php
   wizard.php
 ');
+        /* DO NOT WORK
+        jssearch.php.
+        jssortable.php
+        label.php
+        layouts.php
+        layouts_admin.php
+        layouts_error.php
+        layouts_manual.php
+        layouts_nolayout.php
+        lister.php
+        lister-ipp.php
+        loader2.php
+        loader.php
+        modal2.php
+        popup.php
+        tablefilter.php
+        */
         $copy_paste = explode("\n", $copy_paste);
         $copy_paste = array_map(function ($i) {
             return [trim($i)];
