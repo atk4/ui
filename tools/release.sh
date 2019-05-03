8d7ebfa135bb4e69874619f0216669d76556604f
Romans Malinovskis <me@nearly.guru>
2019-05-03 17:08:45 +0100

    Update release.sh

--- tools/release.sh
+++ tools/release.sh
@@ -58,7 +58,7 @@ composer require atk4/core atk4/data
 composer update
 ./vendor/phpunit/phpunit/phpunit  --no-coverage
 
-sed -i "" "s|'https://cdn.rawgit.com/atk4/ui/.*|'https://cdn.rawgit.com/atk4/ui/$version/public',|" src/App.php
+sed -i "" "s|'https://cdn.jsdelivr.net/gh/atk4/ui.*|'https://cdn.jsdelivr.net/gh/atk4/ui@$version/public',|" src/App.php
 sed -i "" "s|public \$version.*|public \$version = '$version';|" src/App.php
 git commit -m "Updated CDN and \$version in App.php to $version" src/App.php || echo "but its ok"
 
