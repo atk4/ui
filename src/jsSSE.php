565b4820e0a10fd6b1d321959b700362aaf16e7f
Romans Malinovskis <me@nearly.guru>
2019-05-09 22:35:24 +0000

    Apply fixes from StyleCI

--- src/jsSSE.php
+++ src/jsSSE.php
@@ -150,6 +150,5 @@ class jsSSE extends jsCallback
         header('Content-Encoding: none');
         header('Pragma: no-cache');
         header('X-Accel-Buffering: no'); // nginx @http://nginx.org/en/docs/http/ngx_http_proxy_module.html#proxy_buffers
-
     }
 }
