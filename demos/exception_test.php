1324961cc1a179353558ab87adafe5ed34391b1c
abbadon1334 <fdanti@gmail.com>
2019-08-02 20:07:31 +0300

    StyleCi 2

--- demos/exception_test.php
+++ demos/exception_test.php
@@ -7,7 +7,7 @@ require 'init.php';
 $modal = $app->add(['Modal']);
 
 $modal->set(function ($m) use ($modal) {
-    throw new \Exception("TEST!");
+    throw new \Exception('TEST!');
 });
 
 $button = $app->add(['Button', 'Test modal exception']);
@@ -16,7 +16,7 @@ $button->on('click', $modal->show());
 $modal2 = $app->add(['Modal']);
 
 $modal2->set(function ($m) use ($modal2) {
-    trigger_error("error triggered");
+    trigger_error('error triggered');
 });
 
 $button2 = $app->add(['Button', 'Test modal error']);
