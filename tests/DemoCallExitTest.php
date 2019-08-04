1edda2b0f9f8b9abd53048137cb48394c4221e35
abbadon1334 <fdanti@gmail.com>
2019-08-04 10:53:33 +0300

    add some more url to tests

--- tests/DemoCallExitTest.php
+++ tests/DemoCallExitTest.php
@@ -126,7 +126,10 @@ class DemoCallExitTest extends BuiltInWebServerAbstract
         $files[] = ['sticky2.php?__atk_reload=atk_admin_button'];
         $files[] = ['sticky2.php?atk_admin_loader_callback=ajax&__atk_callback1'];
         $files[] = ['virtual.php?atk_admin_label_2_click=ajax&__atk_callback=1'];
-
+        $files[] = ['actions.php?__atk_m=atk_admin_crud_modal&atk_admin_crud_modal_view_callbacklater=ajax&__atk_callback=1&atk_admin_crud_view_table_actions=1&atk_admin_crud_view_view_paginator=1&json=true'];
+        $files[] = ['actions.php?atk_admin_crud_edit=cut&__atk_callback=1&atk_admin_crud=1&atk_admin_crud_sort=&json=true'];
+        $files[] = ['notify.php?__atk_m=atk_admin_modal&atk_admin_modal_view_callbacklater=ajax&__atk_callback=1&json=true'];
+        $files[] = ['scroll-lister.php?atk_admin_view_2_view_lister_jspaginator=ajax&__atk_callback=1&page=2'];
         return $files;
     }
 
@@ -164,6 +167,127 @@ class DemoCallExitTest extends BuiltInWebServerAbstract
         $files[] = ['console.php?atk_admin_tabs_tabssubview_console_jssse=ajax&__atk_callback=1&event=sse'];
         $files[] = ['console.php?atk_admin_tabs_tabssubview_2_virtualpage=cut&atk_admin_tabs_tabssubview_2_virtualpage_console_jssse=ajax&__atk_callback=1&event=sse'];
         $files[] = ['console.php?atk_admin_tabs_tabssubview_3_virtualpage=cut&atk_admin_tabs_tabssubview_3_virtualpage_console_jssse=ajax&__atk_callback=1&event=sse'];
+
+        return $files;
+    }
+
+    /**
+     * @dataProvider JSONResponsePOSTDataProvider
+     *
+     * @param string $uri
+     *
+     * @throws Exception
+     */
+    public function testDemoAssertJSONResponsePOST(string $uri, array $post_data)
+    {
+        $response = $this->getResponseFromRequestFormPOST($uri, $post_data);
+        $this->assertEquals(200, $response->getStatusCode(), ' Status error on '.$uri);
+        $this->assertRegExp($this->regexJSON, $response->getBody()->getContents(), ' RegExp error on '.$uri);
+    }
+
+    public function JSONResponsePOSTDataProvider()
+    {
+        $files = [];
+
+        // IMPORT FROM FILESYSTEM
+        $files[] = [
+            'actions.php?atk_admin_gridlayout_basic_button_click=ajax&__atk_callback=1',
+            [],
+        ]; // btn confirm
+
+        $files[] = [
+            'actions.php?atk_admin_gridlayout_argumentform_form_submit=ajax&__atk_callback=1',
+            [
+                'path'                                          => '.',
+                'atk_admin_gridlayout_argumentform_form_submit' => 'submit',
+            ],
+        ]; // btn run
+        //
+        $files[] = [
+            'actions.php?atk_admin_gridlayout_preview_button_click=ajax&__atk_callback=1',
+            [],
+        ]; // btn confirm (console)
+
+        $files[] = [
+            'actions.php?atk_admin_crud_view_view_paginator=1&__atk_m=atk_admin_crud_modal&atk_admin_crud_modal_view_callbacklater=ajax&atk_admin_crud_modal_view_basic_button_click=ajax&atk_admin_crud_view_table_actions=1&__atk_callback=1',
+            [
+
+            ],
+        ];
+
+        // Grid buttons
+        $files[] = [
+            'actions.php?atk_admin_crud_edit=cut&atk_admin_crud_edit_form_submit=ajax&atk_admin_crud=1&__atk_callback=1',
+            [
+                'name'                            => 'index.php',
+                'type'                            => 'php',
+                'parent_folder_id'                => '1',
+                'atk_admin_crud_edit_form_submit' => 'submit',
+            ],
+        ]; // edit
+
+        $files[] = [
+            'actions.php?atk_admin_crud_view_view_paginator=1&__atk_m=atk_admin_crud_modal&atk_admin_crud_modal_view_callbacklater=ajax&atk_admin_crud_modal_view_basic_button_click=ajax&atk_admin_crud_view_table_actions=1&__atk_callback=1',
+            [],
+        ]; // download : confirm
+
+        // JS ACTIONS
+        $files[] = [
+            'jsactions.php?atk_admin_button_2_jscallback=ajax&__atk_callback=1',
+            [
+                'atk_event_id' => '',
+                'path'         => '.',
+            ],
+        ];
+
+        $files[] = [
+            'jsactions.php?atk_admin_card_view_view_button_jscallback=ajax&__atk_callback=1',
+            [
+                'atk_event_id' => '',
+            ],
+        ];
+
+        $files[] = [
+            'notify.php?__atk_m=atk_admin_modal&atk_admin_modal_view_callbacklater=ajax&atk_admin_modal_view_form_submit=ajax&__atk_callback=1',
+            [
+                'name'                             => 'test',
+                'atk_admin_modal_view_form_submit' => 'submit',
+            ],
+        ];
+
+        $files[] = [
+            'notify2.php?atk_admin_form_submit=ajax&__atk_callback=1',
+            [
+                'text'                  => 'This text will appear in notification',
+                'icon'                  => 'warning sign',
+                'color'                 => 'green',
+                'transition'            => 'jiggle',
+                'width'                 => '25%',
+                'position'              => 'topRight',
+                'attach'                => 'Body',
+                'atk_admin_form_submit' => 'submit',
+            ],
+        ];
+
+        $files[] = [
+            'tablefilter.php?atk_admin_filterpopup_5_form_submit=ajax&__atk_callback=1',
+            [
+                'op'                                  => '=',
+                'value'                               => '374',
+                'range'                               => '',
+                'atk_admin_filterpopup_5_form_submit' => 'submit',
+            ],
+        ];
+
+        $files[] = [
+            'tablefilter.php?atk_admin_filterpopup_4_form_submit=ajax&__atk_callback=1',
+            [
+                'op'                                  => 'between',
+                'value'                               => '10',
+                'range'                               => '20',
+                'atk_admin_filterpopup_5_form_submit' => 'submit',
+            ],
+        ];
 
         return $files;
     }
