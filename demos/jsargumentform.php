bccbf27b0945e6211c8905f3224ecdf3df7d9995
Alain Belair <belair.alain@gmail.com>
2019-10-10 14:49:11 -0400

    start implementation

---
+++ demos/jsargumentform.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'init.php';
+require 'database.php';
+
+use atk4\ui\ActionExecutor\jsArgumentForm;
+use atk4\ui\Form;
+
+$country = new Country($db);
+
+$app->add(new \atk4\ui\Header(['Enter Country model id', 'size' => 4]));
+$field = $app->add(new \atk4\ui\FormField\Line(['caption' => 'Enter model id']))->set(12);
+
+
+$btn_edit = $app->add(['Button', 'Edit']);
+$btn_add = $app->add(['Button', 'Add New']);
+
+$vp_edit = $app->add(['VirtualPage']);
+$vp_add = $app->add(['VirtualPage']);
+
+$form = new Form();
+$form->addHook('formInit', function($f) {
+    // setup special form content.
+    $t = 't';
+});
+
+$btn_edit->on('click', new jsArgumentForm($country->getAction('edit'), $vp_edit, $field->jsInput()->val(), $form));
+$btn_add->on('click', new jsArgumentForm($country->getAction('add'), $vp_add));
---
+++ src/ActionExecutor/jsArgumentForm.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * A js action executor that require a form.
+ */
+
+namespace atk4\ui\ActionExecutor;
+
+use atk4\core\HookTrait;
+use atk4\ui\jsExpression;
+use atk4\ui\jsModal;
+use atk4\ui\jsToast;
+
+class jsArgumentForm extends jsModal
+{
+    use HookTrait;
+
+    public $vp = null;
+    public $action = null;
+    public $form = null;
+
+    public function __construct($action, $page, $modelId = null, $form = null)
+    {
+        $this->vp = $page;
+        $this->action = $action;
+        $this->form = $form;
+
+        parent::__construct($action->caption, $page, ['atk_event_id' => $modelId]);
+
+        $this->initVp();
+    }
+
+    public function initVp()
+    {
+        $this->vp->set(function($p) {
+            $form = null;
+            if (!is_object($this->form) || !$this->form->_initialized) {
+                $form = $p->add($this->form ?: 'Form');
+            }
+
+            $id = $p->stickyGet('atk_event_id');
+
+            if ($id && $this->action->scope === 'single') {
+                $this->action->owner->tryLoad($id);
+            }
+
+            // TODO How do we know if arguement is need over model field in action?
+            $form->setModel($this->action->owner);
+
+            $form->hook('formInit');
+
+            $form->onSubmit(function($f) {
+                $this->action->fields = array_keys($f->model->getFields('editable'));
+                $return = $this->action->execute();
+
+                $js = [
+                    new jsExpression('$(".atk-dialog-content").trigger("close")'),
+                    $this->hook('afterExecute', [$return]) ?: new jsToast('Success'.(is_string($return) ? (': '.$return) : ''))
+                ];
+
+                return $js;
+            });
+        });
+    }
+}
