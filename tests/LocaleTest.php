c4785f03923fcafbf11ea5a28887753a1aec18f9
Romans Malinovskis <me@nearly.guru>
2019-10-21 05:52:44 +0000

    Apply fixes from StyleCI

--- tests/LocaleTest.php
+++ tests/LocaleTest.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace atk4\data\tests;
+
 use atk4\data\Exception;
 use atk4\data\Locale;
+
 class LocaleTest extends \PHPUnit_Framework_TestCase
 {
     public function testException()
@@ -9,6 +12,7 @@ class LocaleTest extends \PHPUnit_Framework_TestCase
         $this->expectException(Exception::class);
         $exc = new Locale();
     }
+
     public function testGetPath()
     {
         $path = implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'src', '..', 'locale']).DIRECTORY_SEPARATOR;
