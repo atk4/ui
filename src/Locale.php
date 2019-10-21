c38e3300393c2e9ac9b0eafcf7f246dafba6ae66
Romans Malinovskis <me@nearly.guru>
2019-10-21 05:51:31 +0000

    Apply fixes from StyleCI

--- src/Locale.php
+++ src/Locale.php
@@ -1,12 +1,15 @@
 
 <?php
+
 namespace atk4\ui;
+
 class Locale
 {
     public function __construct()
     {
         throw new Exception('Class Locale is needed only for locating the default translations');
     }
+
     /**
      * Get absolute Path of default translations.
      *
