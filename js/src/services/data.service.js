a96c8d0c6bd862c687363551c7eef5cfe44403dd
Alain Belair <belair.alain@gmail.com>
2019-10-22 11:30:40 -0400

    js cleanup and comment

--- js/src/services/data.service.js
+++ js/src/services/data.service.js
@@ -12,8 +12,6 @@ class DataService {
     if (!this.instance) {
       this.instance = this;
       this.hasStorage = this.storageAvailable('localStorage') && this.storageAvailable('sessionStorage');
-      this.hasLocal = this.storageAvailable('localStorage');
-      this.hasSession = this.storageAvailable('sessionStorage');
       this.storage  = {session: sessionStorage, local: localStorage};
     }
     return this.instance;
@@ -51,7 +49,7 @@ class DataService {
   }
 
   /**
-   * Check for valide json string.
+   * Check for valid json string.
    * @param str
    * @returns {boolean}
    */
@@ -59,12 +57,21 @@ class DataService {
     try {
       JSON.parse(str);
     } catch (e) {
-      console.error('Invalid json strign supply.');
+      console.error('Invalid json string.');
       return false;
     }
     return true;
   }
 
+  /**
+   * Set Item data value to local or web storage.
+   * The item is the key associated with the data value in web or local storage.
+   * Will add item value or replace it if already exist.
+   *
+   * @param item
+   * @param value
+   * @param type
+   */
   setData(item, value, type = 'local') {
       if (this.hasStorage) {
         this.storage[type].setItem(item, value);
@@ -73,35 +80,13 @@ class DataService {
       }
   }
 
-  setJsonData(item, value, type = 'local') {
-    if (!this.isJsonString(value)) {
-      return;
-    }
-
-    if (this.hasStorage) {
-      this.storage[type].setItem(item, value);
-    }  else {
-      console.error('Session storage is not available in your Browser.')
-    }
-  }
-
-  addJsonData(item, value, type = 'local') {
-    if (!this.isJsonString(value)) {
-      return;
-    }
-
-    if (this.hasStorage) {
-      let previousData = JSON.parse(this.storage[type].getItem(item));
-      if (previousData) {
-        this.storage[type].setItem(item, JSON.stringify(Object.assign(previousData, JSON.parse(value))));
-      } else {
-        this.storage[type].setItem(item, value);
-      }
-    } else {
-      console.error('Session storage is not available in your Browser.')
-    }
-  }
-
+  /**
+   * Get data value using an item as key.
+   *
+   * @param item
+   * @param type
+   * @returns {null}
+   */
   getData(item, type = 'local') {
     let value = null;
     if (this.hasStorage) {
@@ -110,10 +95,54 @@ class DataService {
     return value;
   }
 
+  /**
+   * Clear associated data using item as key.
+   *
+   * @param item
+   * @param type
+   */
   clearData(item, type = 'local') {
-    this.storage[type].removeItem(item);
+    if (this.hasStorage) {
+      this.storage[type].removeItem(item);
+    }
   }
 
+  /**
+   * Similar to set data but make sure that value is
+   * a valid json string prior to set data.
+   *
+   * @param item
+   * @param value
+   * @param type
+   */
+  setJsonData(item, value, type = 'local') {
+    if (!this.isJsonString(value)) {
+      return;
+    }
+    this.setData(item, value, type);
+  }
+
+  /**
+   * Will either create or merge with existing data.
+   * Merging is done with Object assign, prioritizing new value.
+   * Previous data, if exist, and value must be a valid json string.
+   *
+   * @param item
+   * @param value
+   * @param type
+   */
+  addJsonData(item, value, type = 'local') {
+    const previous = this.getData(item, type);
+    if (!this.isJsonString(value) || !this.isJsonString(previous)) {
+      return;
+    }
+
+    if (previous) {
+      this.setData(item, JSON.stringify(Object.assign(JSON.parse(previous), JSON.parse(value))), type);
+    } else {
+      this.setData(item, value, type);
+    }
+  }
 }
 
 const dataService = new DataService();
