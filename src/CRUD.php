e64858fcedf31515d71ff70236ef3023d53c023d
Alain Belair <belair.alain@gmail.com>
2019-11-27 16:46:30 -0500

    CRUD refactoring

--- src/CRUD.php
+++ src/CRUD.php
@@ -4,12 +4,11 @@
 
 namespace atk4\ui;
 
-use atk4\data\UserAction\Action;
+use atk4\data\Model;
 use atk4\data\UserAction\Generic;
 use atk4\ui\ActionExecutor\jsInterface_;
 use atk4\ui\ActionExecutor\jsUserAction;
 use atk4\ui\ActionExecutor\UserAction;
-use atk4\ui\ActionExecutor\UserConfirmation;
 
 /**
  * Implements a more sophisticated and interactive Data-Table component.
@@ -19,11 +18,11 @@ class CRUD extends Grid
     /** @var array of fields to display in Grid */
     public $displayFields = null;
 
-    /** @var array of fields to edit in Form */
+    /** @var null|array of fields to edit in Form */
     public $editFields = null;
 
     /** @var array Default notifier to perform when adding or editing is successful * */
-    public $notifyDefault = ['jsToast', 'settings' => ['message' => 'Data is saved!', 'class' => 'success']];
+    public $notifyDefault = ['jsToast'];
 
     /** @var string default js action executor class in UI for model action. */
     public $jsExecutor = jsUserAction::class;
@@ -37,12 +36,48 @@ class CRUD extends Grid
     /** @var array Collection of NO_RECORDS Scope Model action menu item */
     private $menuItems = [];
 
+    /** @var array Model single scope action to include in table action column. Will include all single scope actions if empty.*/
+    public $singleScopeActions = [];
+
+    /** @var array Model no_record scope action to include in menu. Will include all no record scope actions if empty. */
+    public $noRecordScopeActions = [];
+
+    /** @var string Message to display when record is add or edit successfully. */
+    public $saveMsg = 'Record has been saved!';
+
+    /** @var string Message to display when record is delete successfully. */
+    public $deleteMsg = 'Record has been deleted!';
+
+    /** @var string Generic display message for no record scope action where model is not loaded. */
+    public $defaultMsg = 'Done!';
+
     public function init()
     {
         parent::init();
 
         if ($sortBy = $this->getSortBy()) {
             $this->app->stickyGet($this->name.'_sort', $sortBy);
+        }
+
+    }
+
+    /**
+     * Apply ordering to the current model as per the sort parameters.
+     */
+    public function applySort()
+    {
+        parent::applySort();
+
+        if ($this->getSortBy() && !empty($this->menuItems)) {
+            foreach ($this->menuItems as $k => $item) {
+                //Remove previous click handler and attach new one using sort argument.
+                $this->container->js(true, $item['item']->js()->off('click.atk_crud_item'));
+                $ex = $item['action']->ui['executor'];
+                if ($ex instanceof jsInterface_) {
+                    $ex->stickyGet($this->name.'_sort', $this->getSortBy());
+                    $this->container->js(true, $item['item']->js()->on('click.atk_crud_item', new jsFunction($ex->jsExecute())));
+                }
+            }
         }
     }
 
@@ -71,40 +106,106 @@ class CRUD extends Grid
             $this->useMenuActions = count($m->getActions()) > 4;
         }
 
-        foreach ($m->getActions(Generic::SINGLE_RECORD) as $single_record_action) {
-            $executor = $this->getActionExecutor($single_record_action);
-            $single_record_action->fields = ($executor instanceof jsUserAction || $executor instanceof UserConfirmation) ? false : ($this->editFields ?? []);
-            $single_record_action->ui['executor'] = $executor;
-            $executor->addHook('afterExecute', function ($x, $m, $id) {
-                return $m->loaded() ? $this->jsSave($this->notifyDefault) : $this->jsDelete();
+        foreach ($this->_getModelActions(Generic::SINGLE_RECORD) as $action) {
+            $executor = $this->getActionExecutor($action);
+            $action->fields = $this->_getActionFields($action);
+            $action->ui['executor'] = $executor;
+            $executor->addHook('afterExecute', function ($ex, $return, $id) use ($action) {
+                return $this->getJsExecute($return, $action);
             });
             if ($this->useMenuActions) {
-                $this->addActionMenuItem($single_record_action);
+                $this->addActionMenuItem($action);
             } else {
-                $this->addAction($single_record_action);
+                $this->addAction($action);
             }
         }
 
-        foreach ($m->getActions(Generic::NO_RECORDS) as $k => $single_record_action) {
-            $executor = $this->factory($this->getActionExecutor($single_record_action));
-            if ($executor instanceof View) {
-                $executor->stickyGet($this->name.'_sort', $this->getSortBy());
+        if ($this->menu) {
+            foreach ($this->_getModelActions(Generic::NO_RECORDS) as $k => $action) {
+                $executor = $this->getActionExecutor($action);
+                if ($executor instanceof View) {
+                    $executor->stickyGet($this->name.'_sort', $this->getSortBy());
+                }
+                $action->fields = $this->_getActionFields($action);
+                $action->ui['executor'] = $executor;
+                $executor->addHook('afterExecute', function ($ex, $return, $id) use ($action) {
+                    return $this->getJsExecute($return, $action);
+                });
+                $this->menuItems[$k]['item'] = $this->menu->addItem([$action->getDescription(), 'icon' => 'plus']);
+                $this->menuItems[$k]['action'] = $action;
             }
-            $single_record_action->fields = ($executor instanceof jsUserAction) ? false : ($this->editFields ?? []);
-            $single_record_action->ui['executor'] = $executor;
-            $executor->addHook('afterExecute', function ($x, $m, $id) {
-                return $m->loaded() ? $this->jsSave($this->notifyDefault) : $this->jsDelete();
-            });
-            $this->menuItems[$k]['item'] = $this->menu->addItem([$single_record_action->getDescription(), 'icon' => 'plus']);
-            $this->menuItems[$k]['action'] = $single_record_action;
+            $this->setItemsAction();
         }
-        $this->setItemsAction();
 
         return $this->model;
     }
 
     /**
-     * Setup js for firing action.
+     * Return proper js statement for afterExecute hook on action executor
+     * depending on return type, model loaded and action scope.
+     *
+     * @param $return
+     * @param $action
+     *
+     * @return array|object
+     * @throws \atk4\core\Exception
+     */
+    protected function getJsExecute($return, $action)
+    {
+        if (is_string($return)) {
+            return  $this->getJsNotify($this->notifyDefault, $return, $action);
+        } elseif (is_array($return) || $return instanceof jsExpressionable) {
+            return $return;
+        } elseif ($return instanceof Model) {
+            $msg = $return->loaded() ? $this->saveMsg : ($action->scope === Generic::SINGLE_RECORD ? $this->deleteMsg : $this->defaultMsg);
+            return $this->jsModelReturn($action, $msg);
+        } else {
+            return $this->getJsNotify($this->notifyDefault, $this->defaultMsg, $action);
+        }
+    }
+
+    /**
+     * Return jsNotifier object.
+     * Override this method for setting notifier based on action or model value.
+     *
+     * @param array        $notifier_seed Notifier Object seed.
+     * @param null|string  $msg           The message to display.
+     * @param null|Generic $action        The action short name.
+     *
+     * @throws \atk4\core\Exception
+     *
+     * @return object
+     */
+    protected function getJsNotify($notifier_seed, $msg = null, $action = null)
+    {
+        $notifier = $this->factory($notifier_seed, null, 'atk4\ui');
+        if ($msg) {
+            $notifier->setMessage($msg);
+        }
+
+        return $notifier;
+    }
+
+    /**
+     * js expression return when action afterHook executor return a Model.
+     *
+     * @param Model $model
+     * @param null $action
+     * @param string $msg
+     *
+     * @return array
+     * @throws \atk4\core\Exception
+     */
+    protected function jsModelReturn(Generic $action = null, $msg = 'Done!')
+    {
+        $js[] = $this->getJsNotify($this->notifyDefault, $msg, $action);
+        $js[] = $action->owner->loaded() ? $this->container->jsReload($this->_getReloadArgs()) : (new jQuery())->closest('tr')->transition('fade left');
+
+        return $js;
+    }
+
+    /**
+     * Setup js for firing menu action.
      *
      * @throws \atk4\core\Exception
      */
@@ -113,11 +214,6 @@ class CRUD extends Grid
         foreach ($this->menuItems as $k => $item) {
             $this->container->js(true, $item['item']->on('click.atk_crud_item', $item['action']));
         }
-    }
-
-    public function renderView()
-    {
-        return parent::renderView(); // TODO: Change the autogenerated stub
     }
 
     /**
@@ -141,52 +237,66 @@ class CRUD extends Grid
     }
 
     /**
-     * Apply ordering to the current model as per the sort parameters.
+     * Set proper action fields based on executor and action.
+     * Prioritizing $this->editFields over action->field.
+     *
+     * @param $action
+     *
+     * @return array|bool
      */
-    public function applySort()
+    private function _getActionFields($action)
     {
-        parent::applySort();
-
-        if ($this->getSortBy() && !empty($this->menuItems)) {
-            foreach ($this->menuItems as $k => $item) {
-                //Remove previous click handler and attach new one using sort argument.
-                $this->container->js(true, $item['item']->js()->off('click.atk_crud_item'));
-                $ex = $item['action']->ui['executor'];
-                if ($ex instanceof jsInterface_) {
-                    $ex->stickyGet($this->name.'_sort', $this->getSortBy());
-                    $this->container->js(true, $item['item']->js()->on('click.atk_crud_item', new jsFunction($ex->jsExecute())));
-                }
-            }
+        $fields = false;
+        if ($this->editFields) {
+            $fields = $this->editFields;
+        } elseif (is_array($action->fields) && !empty($action->fields)) {
+            $fields = $action->fields;
+        } elseif ($action->fields === true) {
+            $fields = true;
         }
+        return $fields;
     }
 
     /**
-     * Default js action when saving.
+     * Return reload argument based on CRUD condition.
      *
-     * @param mixed $notifier
+     * @return mixed
+     */
+    private function _getReloadArgs()
+    {
+        $args[$this->name.'_sort'] =  $this->getSortBy();
+        if ($this->paginator) {
+            $args[$this->paginator->name] = $this->paginator->getCurrentPage();
+        }
+
+        return $args;
+    }
+
+    /**
+     * Return proper action need to setup menu or action column.
      *
-     * @throws \atk4\core\Exception
+     * @param string $scope
      *
      * @return array
+     * @throws \atk4\core\Exception
+     * @throws \atk4\data\Exception
      */
-    public function jsSave($notifier) : array
+    private function _getModelActions(string $scope) : array
     {
-        return [
-            $this->factory($notifier, null, 'atk4\ui'),
-            // reload Grid Container.
-            $this->container->jsReload([$this->name.'_sort' => $this->getSortBy()]),
-        ];
-    }
+        $actions = [];
+        if ($scope === Generic::SINGLE_RECORD && !empty($this->singleScopeActions)) {
+            foreach ($this->singleScopeActions as $action) {
+                $actions[]  = $this->model->getAction($action);
+            }
+        } elseif ($scope === Generic::NO_RECORDS && !empty($this->noRecordScopeActions)) {
+            foreach ($this->noRecordScopeActions as $action) {
+                $actions[]  = $this->model->getAction($action);
+            }
+        } else {
+            $actions = $this->model->getActions($scope);
+        }
 
-    /**
-     *  Return js statement necessary to remove a row in Grid when
-     *  use in $(this) context.
-     *
-     * @return jQuery
-     */
-    public function jsDelete() : jQuery
-    {
-        return (new jQuery())->closest('tr')->transition('fade left');
+        return $actions;
     }
 
     /**
@@ -196,6 +306,8 @@ class CRUD extends Grid
      * @param callable $fx
      *
      * @throws Exception
+     * @throws \atk4\core\Exception
+     * @throws \atk4\data\Exception
      */
     public function onEditAction(callable $fx)
     {
@@ -209,6 +321,8 @@ class CRUD extends Grid
      * @param callable $fx
      *
      * @throws Exception
+     * @throws \atk4\core\Exception
+     * @throws \atk4\data\Exception
      */
     public function onAddAction(callable $fx)
     {
@@ -222,6 +336,8 @@ class CRUD extends Grid
      * @param callable $fx
      *
      * @throws Exception
+     * @throws \atk4\core\Exception
+     * @throws \atk4\data\Exception
      */
     public function onAction(callable $fx)
     {
