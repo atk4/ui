fc63be67b90aa1b3b8757bcdf6ef6eab3da7b92f
Romans Malinovskis <romans@agiletoolkit.org>
2019-12-10 00:06:28 +0000

    fix

--- demos/db.github.php
+++ demos/db.github.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = new \atk4\data\Persistence_SQL('mysql:dbname=atk4;host=mysql', 'root', 'password');
+$db = new \atk4\data\Persistence\SQL('mysql:dbname=atk4;host=mysql', 'root', 'password');
