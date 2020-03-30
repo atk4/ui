0b73f48576740954a1fc809901c0f02421524bb9
GitHub Action <noreply@github.com>
2020-03-30 21:34:09 +0000

    Update versions for develop

--- src/App.php
+++ src/App.php
@@ -33,14 +33,14 @@ class App
 
     /** @var array|false Location where to load JS/CSS files */
     public $cdn = [
-        'atk'              => 'https://cdn.jsdelivr.net/gh/atk4/ui@2.0.4/public',
+        'atk'              => 'https://cdn.jsdelivr.net/gh/atk4/ui@2.1.0/public',
         'jquery'           => 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1',
         'serialize-object' => 'https://cdnjs.cloudflare.com/ajax/libs/jquery-serialize-object/2.5.0',
         'semantic-ui'      => 'https://cdn.jsdelivr.net/npm/fomantic-ui@2.7.2/dist',
     ];
 
     /** @var string Version of Agile UI */
-    public $version = '2.0.4';
+    public $version = '2.1.0';
 
     /** @var string Name of application */
     public $title = 'Agile UI - Untitled Application';
