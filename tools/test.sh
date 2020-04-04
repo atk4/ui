d6fc936caefc9d5f6400594c4ffb2c034ad183ad
Michael Voříšek <mvorisek@mvorisek.cz>
2020-04-04 15:37:43 +0200

    Cleanup coverage testing (#1067)

--- tools/test.sh
+++ tools/test.sh
@@ -6,17 +6,16 @@ chmod 777 coverage
 cp behat-travis.yml behat.yml
 cp tools/coverage.php demos/coverage.php
 sed -i "s|node_modules_path.*|node_modules_path: `npm get prefix`/lib/node_modules|g" behat.yml
-(cd js; npm install; npm run build)
+(cd js; npm install --loglevel=error; npm run build)
 
 echo "Running UI tests"
 ./vendor/bin/behat || exit -1
 
-
-sleep 3
+killall -s SIGINT --wait php # flush coverage files
 
 mv phpunit.cov coverage/
-ls -l coverage/
-#mv coverage build/
+#ls -l coverage/
+ls -l coverage/ | wc -l
 ./vendor/bin/phpcov  merge coverage/ --clover clover.xml
 
 #wc -l clover.xml
