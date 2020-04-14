7607c6597aeb92fb437d46b1d4b9950b84c064f3
Alain Belair <belair.alain@gmail.com>
2020-04-14 14:26:00 -0400

    [Fix] Issue #967

--- js/babel.config.js
+++ js/babel.config.js
@@ -9,7 +9,7 @@ const presets = [
     "@babel/env",
     {
       targets: "> 1% , not dead",
-      "corejs": { version: 3, proposals: true },
+      "corejs": { version: '3.6', proposals: true },
       "useBuiltIns": "usage"
     },
   ],
