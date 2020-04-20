a69f9429665ed18f2e113cfb36b01f1bba01d7f4
Michael Voříšek <mvorisek@mvorisek.cz>
2020-04-20 16:14:13 +0200

    Remove strange/skipped tests

--- tests/ListerTest.php
+++ tests/ListerTest.php
@@ -30,22 +30,6 @@ class ListerTest extends AtkPhpunit\TestCase
         $this->assertEquals('hello, world, world', $v->render());
     }
 
-    /**
-     * Or clone lister's template from parent.
-     *
-     * @incomplete
-     */
-    public function testListerRender3()
-    {
-        $this->markTestIncomplete('Very strange test.');
-
-        $v = new \atk4\ui\View(['template'=>new \atk4\ui\Template('hello{list}, world{/list}')]);
-        $v->init();
-        $l = \atk4\ui\Lister::addTo($v, ['defaultTemplate'=>'lister.html']);
-        $l->setSource(['foo', 'bar']);
-        $this->assertMatchesRegularExpression('|<div class="content"><a class="header" href="foo">bar</a>|i', $l->render());
-    }
-
     public function testAddAfterRender()
     {
         $this->expectException(Exception::class);
