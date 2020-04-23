6493a9667262d6ac1f07b0a9397e8f98c3b37a95
Alain Belair <belair.alain@gmail.com>
2020-04-23 13:16:56 -0400

    update virtual demo

--- demos/interactive/virtual.php
+++ demos/interactive/virtual.php
@@ -9,17 +9,22 @@ require_once 'init.php';
 
 // define virtual page.
 $vp = \atk4\ui\VirtualPage::addTo($layout);
+$vp->cb->urlTrigger = 'in';
 
 // Add content to virtual page.
+\atk4\ui\Header::addTo($vp, [$_GET['p_id']]);
 \atk4\ui\LoremIpsum::addTo($vp);
 $vp_btn = \atk4\ui\Button::addTo($vp, ['Back', 'icon' => 'left arrow']);
 $vp_btn->link('virtual.php');
-$vp->ui = 'red inverted segment';
+$vp->ui = 'grey inverted segment';
 
 $msg = \atk4\ui\Message::addTo($app, ['Virtual Page']);
 $msg->text->addParagraph('Virtual page content are not rendered on page load. They will ouptput their content when trigger.');
 $msg->text->addParagraph('Click button below to trigger it.');
 
 // button that trigger virtual page.
-$btn = \atk4\ui\Button::addTo($app, ['Trigger Virtual Page']);
-$btn->link($vp->cb->getUrl());
+$btn = \atk4\ui\Button::addTo($app, ['More info on Car']);
+$btn->link($vp->cb->getUrl() . '&p_id=Car');
+
+$btn = \atk4\ui\Button::addTo($app, ['More info on Bike']);
+$btn->link($vp->cb->getUrl() . '&p_id=Bike');
