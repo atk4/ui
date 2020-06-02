f625465e760bcb1edad96e8ff2fd13bd6c4d28b9
Imants Horsts <DarkSide666@users.noreply.github.com>
2020-06-02 13:15:37 +0300

    cs

--- src/CRUD.php
+++ src/CRUD.php
@@ -60,12 +60,14 @@ class CRUD extends Grid
 
     /**
      * @var array Action name container that will reload Table after executing
+     *
      * @deprecated Use action modifier instead.
      */
     public $reloadTableActions = [];
 
     /**
      * @var array Action name container that will remove the corresponding table row after executing
+     *
      * @deprecated Use action modifier instead.
      */
     public $removeRowActions = [];
