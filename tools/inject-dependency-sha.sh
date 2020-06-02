ab050321c8b83f649414e971169c3dd7ac968955
Romans Malinovskis <me@nearly.guru>
2020-06-02 17:27:44 +0100

    wip

--- tools/inject-dependency-sha.sh
+++ tools/inject-dependency-sha.sh
@@ -3,6 +3,6 @@
 cat composer.json | \
   jq '.require["atk4/core"]=.require["atk4/core"]+"#'`git ls-remote git@github.com:atk4/core.git develop | cut -f1`'"' |\
   jq '.require["atk4/data"]=.require["atk4/data"]+"#'`git ls-remote git@github.com:atk4/data.git develop | cut -f1`'"' \
-  > composer.tmp.json
+  > composer.tmp.json && mv composer.tmp.json composer.json
 
 
