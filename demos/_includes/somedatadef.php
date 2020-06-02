370aeb02f87329f013e56ee29398d34a504bed3e
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-02 18:07:42 +0200

    do not use class_exists

--- demos/_includes/somedatadef.php
+++ demos/_includes/somedatadef.php
@@ -2,89 +2,87 @@
 
 namespace atk4\ui\demo;
 
-if (!class_exists('SomeData')) {
-    class SomeData extends \atk4\data\Model
+class SomeData extends \atk4\data\Model
+{
+    public function __construct()
     {
-        public function __construct()
-        {
-            $p = new Persistence_Faker();
+        $p = new Persistence_Faker();
 
-            parent::__construct($p);
-        }
+        parent::__construct($p);
+    }
 
-        public function init(): void
-        {
-            parent::init();
-            $m = $this;
+    public function init(): void
+    {
+        parent::init();
+        $m = $this;
 
-            $m->addField('title');
-            $m->addField('name');
-            $m->addField('surname', ['actual' => 'name']);
-            $m->addField('date', ['type' => 'date']);
-            $m->addField('salary', ['type' => 'money', 'actual' => 'randomNumber']);
-            $m->addField('logo_url');
+        $m->addField('title');
+        $m->addField('name');
+        $m->addField('surname', ['actual' => 'name']);
+        $m->addField('date', ['type' => 'date']);
+        $m->addField('salary', ['type' => 'money', 'actual' => 'randomNumber']);
+        $m->addField('logo_url');
+    }
+}
+
+class Persistence_Faker extends \atk4\data\Persistence
+{
+    public $faker;
+
+    public $count = 5;
+
+    public function __construct($opts = [])
+    {
+        //parent::__construct($opts);
+
+        if (!$this->faker) {
+            $this->faker = \Faker\Factory::create();
         }
     }
 
-    class Persistence_Faker extends \atk4\data\Persistence
+    public function prepareIterator($m)
     {
-        public $faker;
+        foreach ($this->export($m) as $row) {
+            yield $row;
+        }
+    }
 
-        public $count = 5;
-
-        public function __construct($opts = [])
-        {
-            //parent::__construct($opts);
-
-            if (!$this->faker) {
-                $this->faker = \Faker\Factory::create();
+    public function export($m, $fields = [])
+    {
+        if (!$fields) {
+            foreach ($m->getFields() as $name => $e) {
+                $fields[] = $name;
             }
         }
 
-        public function prepareIterator($m)
-        {
-            foreach ($this->export($m) as $row) {
-                yield $row;
-            }
-        }
+        $data = [];
+        for ($i = 0; $i < $this->count; ++$i) {
+            $row = [];
+            foreach ($fields as $field) {
+                $type = $field;
 
-        public function export($m, $fields = [])
-        {
-            if (!$fields) {
-                foreach ($m->getFields() as $name => $e) {
-                    $fields[] = $name;
+                if ($field === $m->id_field) {
+                    $row[$field] = $i + 1;
+
+                    continue;
+                }
+
+                $actual = $m->getField($field)->actual;
+                if ($actual) {
+                    $type = $actual;
+                }
+
+                if ($type === 'logo_url') {
+                    $row[$field] = '../images/' . ['default.png', 'logo.png'][random_int(0, 1)]; // one of these
+                } else {
+                    $row[$field] = $this->faker->{$type};
                 }
             }
-
-            $data = [];
-            for ($i = 0; $i < $this->count; ++$i) {
-                $row = [];
-                foreach ($fields as $field) {
-                    $type = $field;
-
-                    if ($field === $m->id_field) {
-                        $row[$field] = $i + 1;
-
-                        continue;
-                    }
-
-                    $actual = $m->getField($field)->actual;
-                    if ($actual) {
-                        $type = $actual;
-                    }
-
-                    if ($type === 'logo_url') {
-                        $row[$field] = '../images/' . ['default.png', 'logo.png'][random_int(0, 1)]; // one of these
-                    } else {
-                        $row[$field] = $this->faker->{$type};
-                    }
-                }
-                $data[] = $row;
-            }
-
-            return array_map(function ($r) use ($m) {
-                return $this->typecastLoadRow($m, $r);
-            }, $data);
+            $data[] = $row;
         }
+
+        return array_map(function ($r) use ($m) {
+            return $this->typecastLoadRow($m, $r);
+        }, $data);
     }
 }
