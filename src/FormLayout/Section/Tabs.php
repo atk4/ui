fde8a43dac50f9a2fedc589d408f7b5487ee24a0
Alain Belair <belair.alain@gmail.com>
2020-06-04 16:17:23 -0400

    update form section

--- src/FormLayout/Section/Tabs.php
+++ src/FormLayout/Section/Tabs.php
@@ -12,14 +12,15 @@ class Tabs extends \atk4\ui\Tabs
      *
      * @param string|Tab $name     Name of tab or Tab object
      * @param callable   $callback Callback action or URL (or array with url + parameters)
+     * @param callable   $settings  Tab settings.
      *
      * @throws Exception
      *
      * @return \atk4\ui\FormLayout\Generic
      */
-    public function addTab($name, $callback = null)
+    public function addTab($name, $callback = null, $settings = [])
     {
-        $c = parent::addTab($name, $callback);
+        $c = parent::addTab($name, $callback, $settings);
 
         return $c->add([$this->formLayout, 'form' => $this->form]);
     }
