b9064dbc026fd104506779c879aedc8eb3d1cedc
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-05 12:30:44 +0200

    Refactor exception constructor calls to not use array

--- src/Exception/NoRenderTree.php
+++ src/Exception/NoRenderTree.php
@@ -6,6 +6,7 @@ class NoRenderTree extends \atk4\ui\Exception
 {
     public function __construct($object, $action = '')
     {
-        parent::__construct(['You must use either add($obj) or $obj->init() before ' . ($action ?: 'performing this action'), 'obj' => $object]);
+        parent::__construct('You must use either add($obj) or $obj->init() before ' . ($action ?: 'performing this action'));
+        $this->addMoreInfo('obj', $object);
     }
 }
