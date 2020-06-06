fd4d036172a5e743a48426949a4d73b011fce545
Imants Horsts <DarkSide666@users.noreply.github.com>
2020-06-06 16:09:35 +0300

    Update CheckBox.php

--- src/TableColumn/CheckBox.php
+++ src/TableColumn/CheckBox.php
@@ -2,6 +2,10 @@
 
 namespace atk4\ui\TableColumn;
 
+use atk4\data\Field;
+use atk4\ui\Exception;
+use atk4\ui\jsExpression;
+
 /**
  * Implements CheckBox column for selecting rows.
  */
@@ -16,7 +20,7 @@ class CheckBox extends Generic
      */
     public function jsChecked()
     {
-        return new \atk4\ui\jsExpression(' $(' . $this->table->jsRender() . ").find('.checked." . $this->class . "').closest('tr').map(function(){ " .
+        return new jsExpression(' $(' . $this->table->jsRender() . ").find('.checked." . $this->class . "').closest('tr').map(function(){ " .
             "return $(this).data('id');}).get().join(',')");
     }
 
@@ -28,10 +32,10 @@ class CheckBox extends Generic
         }
     }
 
-    public function getHeaderCellHTML(\atk4\data\Field $f = null, $value = null)
+    public function getHeaderCellHTML(Field $f = null, $value = null)
     {
         if (isset($f)) {
-            throw (new \atk4\ui\Exception('CheckBox must be placed in an empty column. Don\'t specify any field.'))
+            throw (new Exception('CheckBox must be placed in an empty column. Don\'t specify any field.'))
                 ->addMoreInfo('field', $f);
         }
         $this->table->js(true)->find('.' . $this->class)->checkbox();
@@ -39,7 +43,7 @@ class CheckBox extends Generic
         return parent::getHeaderCellHTML($f);
     }
 
-    public function getDataCellTemplate(\atk4\data\Field $f = null)
+    public function getDataCellTemplate(Field $f = null)
     {
         return $this->app->getTag('div', ['class' => 'ui checkbox ' . $this->class], [['input', ['type' => 'checkbox']]]);
     }
