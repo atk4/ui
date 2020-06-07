f34424994c98afb420b0b84d7d731ee627516eeb
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-07 13:41:49 +0200

    Fix missing DIContainerTrait when StaticAddToTrait is used

--- demos/_unit-test/console_run.php
+++ demos/_unit-test/console_run.php
@@ -6,9 +6,8 @@ use atk4\ui\jsSSE;
 
 require_once __DIR__ . '/../atk-init.php';
 
-$testRun = get_class(new class() extends \atk4\data\Model {
+$testRun = new class() extends \atk4\data\Model {
     use \atk4\core\DebugTrait;
-    use \atk4\core\StaticAddToTrait;
 
     public function test()
     {
@@ -24,10 +23,10 @@ $testRun = get_class(new class() extends \atk4\data\Model {
 
         return 123;
     }
-});
+};
 
 $sse = jsSSE::addTo($app);
 $sse->urlTrigger = 'console_test';
 
 $console = \atk4\ui\Console::addTo($app, ['sse' => $sse]);
-$console->runMethod($testRun::addTo($app), 'test');
+$console->runMethod($app->add($testRun), 'test');
