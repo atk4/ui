dbe15f3ece2a8890b054cc35fec05eee9f347c52
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-10 22:59:06 +0200

    Fix current_row - it represents object

--- demos/collection/table2.php
+++ demos/collection/table2.php
@@ -26,8 +26,8 @@ $table->template->appendHTML('Body', '<tr class="center aligned"><td colspan=2>T
 // Hook can be used to display data before row. You can also inject and format extra rows.
 $table->onHook(\atk4\ui\Lister::HOOK_BEFORE_ROW, function (\atk4\ui\Table $table) {
     if ($table->current_id === 2) {
-        $table->template->appendHTML('Body', '<tr class="center aligned"><td colspan=2>This goes above row with ID=2 (' . $table->current_row['action'] . ')</th></tr>');
-    } elseif ($table->current_row['action'] === 'Tax') {
+        $table->template->appendHTML('Body', '<tr class="center aligned"><td colspan=2>This goes above row with ID=2 (' . $table->current_row->get('action') . ')</th></tr>');
+    } elseif ($table->current_row->get('action') === 'Tax') {
         // renders current row
         $table->renderRow();
 
--- src/Lister.php
+++ src/Lister.php
@@ -141,7 +141,8 @@ class Lister extends View
 
         // Iterate data rows
         $this->_rendered_rows_count = 0;
-        foreach ($this->model as $this->current_id => $this->current_row) {
+        foreach ($this->model as $this->current_id => $ignore) {
+            $this->current_row = $this->model;
             if ($this->hook(self::HOOK_BEFORE_ROW) === false) {
                 continue;
             }
