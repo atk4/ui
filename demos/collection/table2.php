5a1febb92d40b3d4ff046ed8f7151bb63a48d058
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-11 13:23:08 +0200

    Fix current_row type mismatches (#1212)
    * Fix current_row - it represents object
    
    * current_id is totally not needed

--- demos/collection/table2.php
+++ demos/collection/table2.php
@@ -25,9 +25,9 @@ $table->template->appendHTML('Body', '<tr class="center aligned"><td colspan=2>T
 
 // Hook can be used to display data before row. You can also inject and format extra rows.
 $table->onHook(\atk4\ui\Lister::HOOK_BEFORE_ROW, function (\atk4\ui\Table $table) {
-    if ($table->current_id === 2) {
-        $table->template->appendHTML('Body', '<tr class="center aligned"><td colspan=2>This goes above row with ID=2 (' . $table->current_row['action'] . ')</th></tr>');
-    } elseif ($table->current_row['action'] === 'Tax') {
+    if ($table->current_row->id === 2) {
+        $table->template->appendHTML('Body', '<tr class="center aligned"><td colspan=2>This goes above row with ID=2 (' . $table->current_row->get('action') . ')</th></tr>');
+    } elseif ($table->current_row->get('action') === 'Tax') {
         // renders current row
         $table->renderRow();
 
