6ec10732cd5e1401114f15e86011dd8a5049a8c3
DarkSide <imants.horsts@inbox.lv>
2020-06-11 15:31:00 +0300

    support callback to disable actions

--- src/TableColumn/Actions.php
+++ src/TableColumn/Actions.php
@@ -2,12 +2,28 @@
 
 namespace atk4\ui\TableColumn;
 
+use atk4\data\Field;
+use atk4\data\Model;
+use atk4\ui\Button;
+
 /**
  * Formatting action buttons column.
  */
 class Actions extends Generic
 {
+    /**
+     * Stores all the buttons that have been added.
+     *
+     * @var array
+     */
     public $actions = [];
+
+    /**
+     * Callbacks as defined in $isDisabled for evaluating row-specific if an action is enabled.
+     *
+     * @var array
+     */
+    protected $callbacks = [];
 
     public function init()
     {
@@ -18,20 +34,27 @@ class Actions extends Generic
     /**
      * Adds a new button which will execute $callback when clicked.
      *
-     * Returns button object
+     * @param bool|callable $isDisabled Should button be disabled?
+     *
+     * @return Button
      */
-    public function addAction($button, $callback, $confirm = false)
+    public function addAction($button, $callback, $confirm = false, $isDisabled = false)
     {
         $name = $this->name.'_action_'.(count($this->actions) + 1);
 
         if (!is_object($button)) {
-            $button = new \atk4\ui\Button($button);
+            $button = new Button($button);
         }
         $button->app = $this->table->app;
 
-        $this->actions[$name] = $button;
-        $button->addClass('b_'.$name);
-        $button->addClass('compact');
+        $this->actions[$name] = $button->addClass('{$_' . $name . '_disabled} compact b_' . $name);
+
+        if ($isDisabled === true) {
+            $button->addClass('disabled');
+        } elseif (is_callable($isDisabled)) {
+            $this->callbacks[$name] = $isDisabled;
+        }
+
         $this->table->on('click', '.b_'.$name, $callback, [$this->table->jsRow()->data('id'), 'confirm' => $confirm]);
 
         return $button;
@@ -69,7 +92,7 @@ class Actions extends Generic
         return parent::getTag($position, $value, $attr);
     }
 
-    public function getDataCellTemplate(\atk4\data\Field $f = null)
+    public function getDataCellTemplate(Field $f = null)
     {
         if (!$this->actions) {
             return '';
@@ -82,6 +105,19 @@ class Actions extends Generic
         }
 
         return '<div class="ui buttons">'.$output.'</div>';
+    }
+
+    public function getHTMLTags(Model $row, $field)
+    {
+        $tags = [];
+        foreach ($this->callbacks as $name => $callback) {
+            // if action is disabled then set disabled class
+            if (call_user_func($callback, $row)) {
+                $tags['_' . $name . '_disabled'] = 'disabled';
+            }
+        }
+
+        return $tags;
     }
 
     // rest will be implemented for crud
