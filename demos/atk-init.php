b4b96dd61b1ae33e1323233a68a7e817bbded98c
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-15 17:08:11 +0200

    Fix demos without dev deps when as a root project (#1287)
    * Fix demos without dev deps when as a root project
    
    * Use standard connection string if "DSN" env var is not set
    
    * Use empty sqlite if "DSN" env var is not set
    
    * Display nice error, but still hide credentials

--- demos/atk-init.php
+++ demos/atk-init.php
@@ -7,7 +7,7 @@ date_default_timezone_set('UTC');
 $isRootProject = file_exists(__DIR__ . '/../vendor/autoload.php');
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require dirname(__DIR__, $isRootProject ? 1 : 4) . '/vendor/autoload.php';
-if (!$isRootProject || !class_exists(\atk4\ui\tests\ViewTest::class)) {
+if (!$isRootProject && !class_exists(\atk4\ui\tests\ViewTest::class)) {
     throw new \Error('Demos can be run only if atk4/ui is a root composer project or if dev files are autoloaded');
 }
 $loader->setPsr4('atk4\ui\demo\\', __DIR__ . '/_includes');
@@ -38,7 +38,11 @@ if (file_exists(__DIR__ . '/coverage.php')) {
 }
 // END - PHPUNIT & COVERAGE SETUP
 
-require_once __DIR__ . '/database.php';
+try {
+    require_once __DIR__ . '/database.php';
+} catch (\Throwable $e) {
+    throw new \atk4\ui\Exception('Database error: ' . $e->getMessage());
+}
 
 $app->db = $db;
 $app->title = 'Agile UI Demo v' . $app->version;
