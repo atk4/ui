d9fc4e4c0bc634e21d3a2402e2331dcd65691a19
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-16 19:14:55 +0200

    better gen for PK+AI col

--- demos/_demo-data/create-sqlite-db.php
+++ demos/_demo-data/create-sqlite-db.php
@@ -16,17 +16,24 @@ foreach (preg_split('~;\s*(\n\s*|$)~', file_get_contents($srcFile)) as $query) {
     if (preg_match('~^(CREATE TABLE[^()]+\()(.+)(\).*?)$~is', $query, $createMatches)) {
         $createRows = [];
         foreach (preg_split('~\s*,\s*\n\s*~', trim($createMatches[2])) as $row) {
+            $row = preg_replace('~ \w*int(?:\(\d+\))~is', ' INTEGER', $row);
             $row = preg_replace('~ UNSIGNED~is', '', $row);
-            $row = preg_replace('~ AUTO_INCREMENT~is', ' PRIMARY KEY', $row);
+            $row = preg_replace('~ AUTO_INCREMENT~is', ' PRIMARY KEY AUTOINCREMENT', $row);
             $row = preg_replace('~ ENUM\((.+?)\)~is', ' TEXT', $row);
 
+            if (!preg_match('~ INTEGER ~is', $row)) {
+                $row = $row . ' COLLATE NOCASE';
+            }
+
             if (!preg_match('~^(PRIMARY )?KEY ~is', $row)) {
-                $createRows[] = $row . ' COLLATE NOCASE';
+                $createRows[] = $row;
             }
         }
 
         $createMatches[3] = preg_replace('~(?<!\w)ENGINE=InnoDB(?!\w)~is', '', $createMatches[3]);
         $query = $createMatches[1] . "\n    " . implode(",\n    ", $createRows) . "\n" . $createMatches[3];
+
+        // echo $query . "\n\n";
     }
 
     $query = preg_replace('~\\\\\'~is', '\'\'', $query);
