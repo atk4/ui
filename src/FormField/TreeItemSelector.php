3592f47bb60deeb7c7c2d96c22d7cc43406de28c
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-25 19:54:05 +0200

    remove doc from deprecated classes

--- src/FormField/TreeItemSelector.php
+++ src/FormField/TreeItemSelector.php
@@ -1,18 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/**
- * Display items in a hierarchical (tree) view structure.
- *
- * When an item contains nodes with non empty values, it will automatically be treat as a group level;
- *
- * The input value is store as an array type when allowMultiple is set to true, otherwise, will
- * store one single value when set to false.
- *
- * Only item id are store within the input field.
- *
- * see demos/tree-item-selector.php to see how tree items are build.
- */
 
 namespace atk4\ui\FormField;
 
