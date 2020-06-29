23f0e349eb651721259877d14481a2386a3cbdee
DarkSide <imants.horsts@inbox.lv>
2020-06-30 01:56:46 +0300

    fix property name

--- src/UserAction/ArgumentFormExecutor.php
+++ src/UserAction/ArgumentFormExecutor.php
@@ -39,7 +39,7 @@ class ArgumentFormExecutor extends BasicExecutor
         foreach ($this->action->args as $key => $val) {
             if (is_numeric($key)) {
                 throw (new Exception('Action arguments must be named'))
-                    ->addMoreInfo('args', $this->actions->args);
+                    ->addMoreInfo('args', $this->action->args);
             }
 
             if ($val instanceof Model) {
