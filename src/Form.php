51c7833bcd21b024370821b76a891d1743decce7
Michael Voříšek <mvorisek@mvorisek.cz>
2020-07-09 01:25:07 +0200

    fix CS

--- src/Form.php
+++ src/Form.php
@@ -633,21 +633,8 @@ class Form extends View
     {
         $output = parent::renderTemplateToHtml();
 
-        $innerFormTags = ['button',
-            'datalist',
-            'fieldset',
-            'input',
-            'keygen',
-            'label',
-            'legend',
-            'meter',
-            'optgroup',
-            'option',
-            'output',
-            'progress',
-            'select',
-            'textarea'
-        ];
+        $innerFormTags = ['button', 'datalist', 'fieldset', 'input', 'keygen', 'label', 'legend',
+            'meter', 'optgroup', 'option', 'output', 'progress', 'select', 'textarea', ];
         $output = preg_replace('~<(' . implode('|', $innerFormTags) . ')(?!\w| form=")~i', '$0 form="' . $this->formElement->name . '"', $output);
 
         return $output;
