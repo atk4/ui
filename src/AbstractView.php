16db1bc5670522cfb002a5aeddaa60be50f797dd
Michael Voříšek <mvorisek@mvorisek.cz>
2020-10-12 20:42:21 +0200

    Fix owner prop to getter phpdoc

--- src/AbstractView.php
+++ src/AbstractView.php
@@ -15,7 +15,7 @@ use atk4\core\TrackableTrait;
 /**
  * Abstract view tree item (used only for View and Callback, you want probably to extend one of these).
  *
- * @property View   $owner
+ * @method View getOwner()
  * @property View[] $elements
  */
 abstract class AbstractView
