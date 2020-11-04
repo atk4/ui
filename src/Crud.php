7b0c75c488116f6c2a1107c649e768a370958b57
Michael Voříšek <mvorisek@mvorisek.cz>
2020-11-04 14:39:02 +0100

    Removed deprecated things, fix deprecated comments

--- src/Crud.php
+++ src/Crud.php
@@ -59,14 +59,14 @@ class Crud extends Grid
     /**
      * @var array Action name container that will reload Table after executing
      *
-     * @deprecated use action modifier instead
+     * @deprecated use action modifier instead, will be removed in v2.5
      */
     public $reloadTableActions = [];
 
     /**
      * @var array Action name container that will remove the corresponding table row after executing
      *
-     * @deprecated use action modifier instead
+     * @deprecated use action modifier instead, will be removed in v2.5
      */
     public $removeRowActions = [];
 
