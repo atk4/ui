8f77bad2b6105d22d7e5a603e23891825fc955c7
Alain Belair <belair.alain@gmail.com>
2020-12-01 14:49:06 -0500

    re enable all demo action

--- demos/_includes/DemoActionsUtil.php
+++ demos/_includes/DemoActionsUtil.php
@@ -8,103 +8,103 @@ class DemoActionsUtil
 {
     public static function setupDemoActions(CountryLock $country): void
     {
-//        $country->addUserAction(
-//            'callback',
-//            ['description' => 'Callback',
-//                'callback' => function ($model) {
-//                    return 'callback execute using country ' . $model->getTitle();
-//                },
-//            ]
-//        );
-//
-//        $country->addUserAction(
-//            'preview',
-//            [
-//                'description' => 'Display Preview prior to run the action',
-//                'preview' => function ($model) {
-//                    return 'Previewing country ' . $model->getTitle();
-//                },
-//                'callback' => function ($model) {
-//                    return 'Done previewing ' . $model->getTitle();
-//                },
-//            ]
-//        );
-//
-//        $country->addUserAction(
-//            'disabled_action',
-//            [
-//                'description' => 'This action is disabled.',
-//                'caption' => 'Disabled',
-//                'enabled' => false,
-//                'callback' => function () {
-//                    return 'ok';
-//                },
-//            ]
-//        );
-//
-//        $country->addUserAction(
-//            'edit_argument',
-//            [
-//                'caption' => 'Argument',
-//                'description' => 'Ask for argument "Age" prior to execute the action.',
-//                'args' => [
-//                    'age' => ['type' => 'integer', 'required' => true],
-//                ],
-//                'callback' => function ($model, $age) {
-//                    if ($age < 18) {
-//                        $text = 'Sorry not old enough to visit ' . $model->getTitle();
-//                    } else {
-//                        $text = $age . ' is old enough to visit ' . $model->getTitle();
-//                    }
-//
-//                    return $text;
-//                },
-//            ]
-//        );
-//
-//        $country->addUserAction(
-//            'edit_argument_prev',
-//            [
-//                'caption' => 'Argument/Preview',
-//                'description' => 'Ask for argument "Age" and display preview prior to execute',
-//                'args' => ['age' => ['type' => 'integer', 'required' => true]],
-//                'preview' => function ($model, $age) {
-//                    return 'You age is: ' . $age;
-//                },
-//                'callback' => function ($model, $age) {
-//                    return 'age = ' . $age;
-//                },
-//            ]
-//        );
-//
-//        $country->addUserAction(
-//            'edit_iso',
-//            [
-//                'caption' => 'Edit ISO3',
-//                'description' => function ($action) {
-//                    return 'Edit ISO3 for country: ' . $action->getModel()->getTitle();
-//                },
-//                'fields' => ['iso3'],
-//                'callback' => function () {
-//                    return 'ok';
-//                },
-//            ]
-//        );
-//
-//        $country->addUserAction(
-//            'Ouch',
-//            [
-//                'caption' => 'Exception',
-//                'description' => 'Throw an exception when executing an action',
-//                'args' => ['age' => ['type' => 'integer']],
-//                'preview' => function () {
-//                    return 'Be careful with this action.';
-//                },
-//                'callback' => function () {
-//                    throw new \atk4\ui\Exception('Told you, didn\'t I?');
-//                },
-//            ]
-//        );
+        $country->addUserAction(
+            'callback',
+            ['description' => 'Callback',
+                'callback' => function ($model) {
+                    return 'callback execute using country ' . $model->getTitle();
+                },
+            ]
+        );
+
+        $country->addUserAction(
+            'preview',
+            [
+                'description' => 'Display Preview prior to run the action',
+                'preview' => function ($model) {
+                    return 'Previewing country ' . $model->getTitle();
+                },
+                'callback' => function ($model) {
+                    return 'Done previewing ' . $model->getTitle();
+                },
+            ]
+        );
+
+        $country->addUserAction(
+            'disabled_action',
+            [
+                'description' => 'This action is disabled.',
+                'caption' => 'Disabled',
+                'enabled' => false,
+                'callback' => function () {
+                    return 'ok';
+                },
+            ]
+        );
+
+        $country->addUserAction(
+            'edit_argument',
+            [
+                'caption' => 'Argument',
+                'description' => 'Ask for argument "Age" prior to execute the action.',
+                'args' => [
+                    'age' => ['type' => 'integer', 'required' => true],
+                ],
+                'callback' => function ($model, $age) {
+                    if ($age < 18) {
+                        $text = 'Sorry not old enough to visit ' . $model->getTitle();
+                    } else {
+                        $text = $age . ' is old enough to visit ' . $model->getTitle();
+                    }
+
+                    return $text;
+                },
+            ]
+        );
+
+        $country->addUserAction(
+            'edit_argument_prev',
+            [
+                'caption' => 'Argument/Preview',
+                'description' => 'Ask for argument "Age" and display preview prior to execute',
+                'args' => ['age' => ['type' => 'integer', 'required' => true]],
+                'preview' => function ($model, $age) {
+                    return 'You age is: ' . $age;
+                },
+                'callback' => function ($model, $age) {
+                    return 'age = ' . $age;
+                },
+            ]
+        );
+
+        $country->addUserAction(
+            'edit_iso',
+            [
+                'caption' => 'Edit ISO3',
+                'description' => function ($action) {
+                    return 'Edit ISO3 for country: ' . $action->getModel()->getTitle();
+                },
+                'fields' => ['iso3'],
+                'callback' => function () {
+                    return 'ok';
+                },
+            ]
+        );
+
+        $country->addUserAction(
+            'Ouch',
+            [
+                'caption' => 'Exception',
+                'description' => 'Throw an exception when executing an action',
+                'args' => ['age' => ['type' => 'integer']],
+                'preview' => function () {
+                    return 'Be careful with this action.';
+                },
+                'callback' => function () {
+                    throw new \atk4\ui\Exception('Told you, didn\'t I?');
+                },
+            ]
+        );
 
         $country->addUserAction(
             'confirm',
