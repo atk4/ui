67f37873d943a5e4872474d0e210d49b5fe84f2d
Alain Belair <belair.alain@gmail.com>
2020-12-04 15:13:55 -0500

    add register trigger test

--- tests/ExecutorFactoryTest.php
+++ tests/ExecutorFactoryTest.php
@@ -110,4 +110,42 @@ class ExecutorFactoryTest extends AtkPhpunit\TestCase
         $this->assertInstanceOf(Item::class, $tableMenuItem, 'Not Table item');
         $this->assertSame($factory->getCaption($editAction, $factory::TABLE_MENU_ITEM), $tableMenuItem->content);
     }
+
+    public function testRegisterTrigger()
+    {
+        $factory = $this->app->getExecutorFactory();
+        $factory->useTriggerDefault($factory::TABLE_BUTTON);
+        $factory->useTriggerDefault($factory::MENU_ITEM);
+
+        $editAction = $this->model->getUserAction('edit');
+
+        $p = new Array_();
+        $otherModelClass = get_class(new class() extends Model {
+        });
+        $secondEditAction = (new $otherModelClass($p))->getUserAction('edit');
+
+        $specialClass = get_class(new class() extends Model {
+            public $caption = 'Special Test';
+        });
+        $specialEditAction = (new $specialClass($p))->getUserAction('edit');
+
+        $factory->registerTrigger($factory::MENU_ITEM, [Item::class, 'edit_item', 'icon' => 'pencil'], $editAction);
+        $editItem = $factory->createTrigger($editAction, $factory::MENU_ITEM);
+
+        $this->assertInstanceOf(Item::class, $editItem, 'Not Menu item class');
+        $this->assertSame('edit_item', $editItem->content);
+        $this->assertSame('pencil', $editItem->icon);
+
+        $factory->registerTrigger($factory::TABLE_BUTTON, [Button::class, 'edit_button'], $editAction);
+        $factory->registerTrigger($factory::TABLE_BUTTON, [Button::class, 'specific_edit_button'], $specialEditAction, true);
+
+        $editButton = $factory->createTrigger($editAction, $factory::TABLE_BUTTON);
+        $secondEditButon = $factory->createTrigger($secondEditAction, $factory::TABLE_BUTTON);
+        $specialEditButton = $factory->createTrigger($specialEditAction, $factory::TABLE_BUTTON);
+
+        $this->assertInstanceOf(Button::class, $specialEditButton, 'Not a button');
+        $this->assertSame('specific_edit_button', $specialEditButton->content);
+        $this->assertSame($editButton->content, $secondEditButon->content);
+        $this->assertNotSame($editButton->content, $specialEditButton->content);
+    }
 }
