53187fb7c487421aa0aef1e6a9902fd4ed253f7b
Michael Voříšek <mvorisek@mvorisek.cz>
2020-12-11 21:08:02 +0100

    fix for data/806

--- src/View.php
+++ src/View.php
@@ -187,6 +187,7 @@ class View extends AbstractView implements JsExpressionable
     {
         $this->setModel(new Model(new Static_($data)), $fields);
         $this->model->getField($this->model->id_field)->type = null; // TODO probably unwanted
+        $this->model->getField($this->model->id_field)->required = false; // TODO probably unwanted
 
         return $this->model;
     }
