70b63a52b779b41212cf4cda8f5e0abf5560ba5a
Michael Voříšek <mvorisek@mvorisek.cz>
2021-01-06 00:51:30 +0100

    Fix autoload with classmap authoritative

--- demos/init-autoloader.php
+++ demos/init-autoloader.php
@@ -10,5 +10,6 @@ $loader = require dirname(__DIR__, $isRootProject ? 1 : 4) . '/vendor/autoload.p
 if (!$isRootProject && !class_exists(\Atk4\Ui\Tests\ViewTest::class)) {
     throw new \Error('Demos can be run only if atk4/ui is a root composer project or if dev files are autoloaded');
 }
+$loader->setClassMapAuthoritative(false);
 $loader->setPsr4('Atk4\Ui\Demos\\', __DIR__ . '/_includes');
 unset($isRootProject, $loader);
