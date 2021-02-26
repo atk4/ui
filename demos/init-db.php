abb66af154b27861d86acb0981c1a8ecb250cb6e
Michael Voříšek <mvorisek@mvorisek.cz>
2021-02-26 11:24:27 +0100

    checked fixes

--- demos/init-db.php
+++ demos/init-db.php
@@ -196,7 +196,7 @@ class Stat extends ModelWithPrefixedFields
 
         $this->hasOne($this->fieldName()->client_country_iso, [
             'model' => [Country::class],
-            'our_field' => Country::hinting()->fieldName()->id,
+            'our_field' => $this->fieldName()->client_country_iso,
             'their_field' => Country::hinting()->fieldName()->iso,
             'type' => 'string',
             'ui' => [
