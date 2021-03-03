4f73b0c1c7fd0a4883d5b455147ec14f7db558a2
DarkSide <imants.horsts@inbox.lv>
2021-03-04 00:58:05 +0200

    remove all

--- demos/init-db.php
+++ demos/init-db.php
@@ -273,7 +273,6 @@ class File extends ModelWithPrefixedFields
 
         $this->hasOne($this->fieldName()->parent_folder_id, [
             'model' => [Folder::class],
-            'our_field' => $this->fieldName()->parent_folder_id,
         ])
             ->addTitle();
     }
@@ -378,7 +377,6 @@ class SubCategory extends ModelWithPrefixedFields
 
         $this->hasOne($this->fieldName()->product_category_id, [
             'model' => [Category::class],
-            'our_field' => $this->fieldName()->product_category_id,
         ]);
         $this->hasMany($this->fieldName()->Products, [
             'model' => [Product::class],
@@ -404,11 +402,9 @@ class Product extends ModelWithPrefixedFields
         $this->addField($this->fieldName()->brand);
         $this->hasOne($this->fieldName()->product_category_id, [
             'model' => [Category::class],
-            'our_field' => $this->fieldName()->product_category_id,
         ])->addTitle();
         $this->hasOne($this->fieldName()->product_sub_category_id, [
             'model' => [SubCategory::class],
-            'our_field' => $this->fieldName()->product_sub_category_id,
         ])->addTitle();
     }
 }
