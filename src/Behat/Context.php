ad123452a4e26fe6b8a0909066c987aa14978ae4
Michael Voříšek <mvorisek@mvorisek.cz>
2021-05-12 23:48:30 +0200

    Remove manual waits

--- src/Behat/Context.php
+++ src/Behat/Context.php
@@ -97,9 +97,9 @@ class Context extends RawMinkContext implements BehatContext
     /**
      * Sleep for a certain time in ms.
      *
-     * @Then I wait :arg1 ms
+     * @Then I sleep :arg1 ms
      */
-    public function iWait(int $arg1): void
+    public function iSleep(int $arg1): void
     {
         $this->getSession()->wait($arg1);
     }
@@ -324,29 +324,6 @@ class Context extends RawMinkContext implements BehatContext
     }
 
     /**
-     * Get a node element by it's selector.
-     * Will try to get element for 20ms.
-     * Exemple: Use with a modal window where reloaded content
-     * will resize it's window thus making it not accessible at first.
-     */
-    private function waitForNodeElement(string $selector, int $ms = 20): ?NodeElement
-    {
-        $counter = 0;
-        $element = null;
-        while ($counter < $ms) {
-            $element = $this->getSession()->getPage()->find('css', $selector);
-            if ($element === null) {
-                usleep(1000);
-                ++$counter;
-            } else {
-                break;
-            }
-        }
-
-        return $element;
-    }
-
-    /**
      * @Then Active tab should be :arg1
      */
     public function activeTabShouldBe(string $arg1): void
@@ -476,183 +453,6 @@ class Context extends RawMinkContext implements BehatContext
         }
 
         $icon->click();
-    }
-
-    /**
-     * Generic ScopeBuilder rule with select operator and input value.
-     *
-     * @Then /^rule "([^"]*)" operator is "([^"]*)" and value is "([^"]*)"$/
-     */
-    public function scopeBuilderRule(string $name, string $operator, string $value): void
-    {
-        $rule = $this->getScopeBuilderRuleElem($name);
-        $this->assertSelectedValue($rule, $operator, '.vqb-rule-operator select');
-        $this->assertInputValue($rule, $value);
-    }
-
-    /**
-     * hasOne reference or enum type rule for ScopeBuilder.
-     *
-     * @Then /^reference rule "([^"]*)" operator is "([^"]*)" and value is "([^"]*)"$/
-     */
-    public function scopeBuilderReferenceRule(string $name, string $operator, string $value): void
-    {
-        $rule = $this->getScopeBuilderRuleElem($name);
-        $this->assertSelectedValue($rule, $operator, '.vqb-rule-operator select');
-        $this->assertDropdownValue($rule, $value, '.vqb-rule-input .active.item');
-    }
-
-    /**
-     * hasOne select or enum type rule for ScopeBuilder.
-     *
-     * @Then /^select rule "([^"]*)" operator is "([^"]*)" and value is "([^"]*)"$/
-     */
-    public function scopeBuilderSelectRule(string $name, string $operator, string $value): void
-    {
-        $rule = $this->getScopeBuilderRuleElem($name);
-        $this->assertSelectedValue($rule, $operator, '.vqb-rule-operator select');
-        $this->assertSelectedValue($rule, $value, '.vqb-rule-input select');
-    }
-
-    /**
-     * Date, Time or Datetime rule for ScopeBuilder.
-     *
-     * @Then /^date rule "([^"]*)" operator is "([^"]*)" and value is "([^"]*)"$/
-     */
-    public function scopeBuilderDateRule(string $name, string $operator, string $value): void
-    {
-        $rule = $this->getScopeBuilderRuleElem($name);
-        $this->assertSelectedValue($rule, $operator, '.vqb-rule-operator select');
-        $this->assertInputValue($rule, $value, 'input.form-control');
-    }
-
-    /**
-     * Boolean type rule for ScopeBuilder.
-     *
-     * @Then /^bool rule "([^"]*)" has value "([^"]*)"$/
-     */
-    public function scopeBuilderBoolRule(string $name, string $value): void
-    {
-        $this->getScopeBuilderRuleElem($name);
-        $idx = ($value === 'Yes') ? 0 : 1;
-        $isChecked = $this->getSession()->evaluateScript('return $(\'[data-name="' . $name . '"]\').find(\'input\')[' . $idx . '].checked');
-        if (!$isChecked) {
-            throw new Exception('Radio value selected is not: ' . $value);
-        }
-    }
-
-    /**
-     * @Then /^I check if text in "([^"]*)" match text in "([^"]*)"/
-     */
-    public function compareElementText(string $compareSelector, string $compareToSelector): void
-    {
-        $compareContainer = $this->getSession()->getPage()->find('css', $compareSelector);
-        if (!$compareContainer) {
-            throw new Exception('Unable to find compare container: ' . $compareSelector);
-        }
-
-        $expectedText = $compareContainer->getText();
-
-        $compareToContainer = $this->getSession()->getPage()->find('css', $compareToSelector);
-        if (!$compareToContainer) {
-            throw new Exception('Unable to find compare to container: ' . $compareToSelector);
-        }
-
-        $compareToText = $compareToContainer->getText();
-
-        if ($expectedText !== $compareToText) {
-            throw new Exception('Data word does not match: ' . $compareToText . ' expected: ' . $expectedText);
-        }
-    }
-
-    /**
-     * @Then /^I check if input value for "([^"]*)" match text in "([^"]*)"$/
-     */
-    public function compareInputValueToElementText(string $inputName, string $selector): void
-    {
-        $expected = $this->getSession()->getPage()->find('css', $selector)->getText();
-        $input = $this->getSession()->getPage()->find('css', 'input[name="' . $inputName . '"]');
-        if (!$input) {
-            throw new Exception('Unable to find input name: ' . $inputName);
-        }
-
-        if (preg_replace('~\s*~', '', $expected) !== preg_replace('~\s*~', '', $input->getValue())) {
-            throw new Exception('Input value does not match: ' . $input->getValue() . ' expected: ' . $expected);
-        }
-    }
-
-    /**
-     * @Then /^text in container using \'([^\']*)\' should contains \'([^\']*)\'$/
-     */
-    public function textInContainerUsingShouldContains(string $containerCss, string $text): void
-    {
-        $container = $this->getSession()->getPage()->find('css', $containerCss);
-        if (!$container) {
-            throw new Exception('Unable to find container: ' . $containerCss);
-        }
-
-        if (trim($container->getText()) !== $text) {
-            throw new Exception('Text not in container ' . $text . ' - ' . $container->getText());
-        }
-    }
-
-    /**
-     * Find a dropdown component within an html element
-     * and check if value is set in dropdown.
-     */
-    private function assertDropdownValue(NodeElement $element, string $value, string $selector): void
-    {
-        $dropdown = $element->find('css', $selector);
-        if (!$dropdown) {
-            throw new Exception('Dropdown input not found using selector: ' . $selector);
-        }
-
-        $dropdownValue = $dropdown->getHtml();
-        if ($dropdownValue !== $value) {
-            throw new Exception('Value: "' . $value . '" not set using selector: ' . $selector);
-        }
-    }
-
-    /**
-     * Find a select input type within an html element
-     * and check if value is selected.
-     */
-    private function assertSelectedValue(NodeElement $element, string $value, string $selector): void
-    {
-        $select = $element->find('css', $selector);
-        if (!$select) {
-            throw new Exception('Select input not found using selector: ' . $selector);
-        }
-        $selectValue = $select->getValue();
-        if ($selectValue !== $value) {
-            throw new Exception('Value: "' . $value . '" not set using selector: ' . $selector);
-        }
-    }
-
-    /**
-     * Find an input within an html element and check
-     * if value is set.
-     */
-    private function assertInputValue(NodeElement $element, string $value, string $selector = 'input'): void
-    {
-        $input = $element->find('css', $selector);
-        if (!$input) {
-            throw new Exception('Input not found in selector: ' . $selector);
-        }
-        $inputValue = $input->getValue();
-        if ($inputValue !== $value) {
-            throw new Exception('Input value not is not: ' . $value);
-        }
-    }
-
-    private function getScopeBuilderRuleElem(string $ruleName): NodeElement
-    {
-        $rule = $this->getSession()->getPage()->find('css', '.vqb-rule[data-name=' . $ruleName . ']');
-        if (!$rule) {
-            throw new Exception('Rule not found: ' . $ruleName);
-        }
-
-        return $rule;
     }
 
     /**
