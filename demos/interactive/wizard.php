0e6c9bbc27c601f13ea5d8b66d60e1abf4bc5c6f
Alain Belair <belair.alain@gmail.com>
2021-07-07 12:14:08 -0400

    Remove callback from Wizard

--- demos/interactive/wizard.php
+++ demos/interactive/wizard.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/../init-app.php';
 /**
  * Demonstrates how to use a wizard.
  */
-$wizard = Wizard::addTo($app, ['stepCallback' => Callback::addTo($app, ['urlTrigger' => 'demo_wizard'])]);
+$wizard = Wizard::addTo($app, ['urlTrigger' => 'demo_wizard']);
 // First step will automatcally be active when you open page first. It
 // will contain the 'Next' button with a link.
 $wizard->addStep('Welcome', function (Wizard $wizard) {
