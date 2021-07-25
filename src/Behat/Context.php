e5b4b0e8c7a1907087951037e1657c5ff06d1dd1
Alain Belair <belair.alain@gmail.com>
2021-07-25 10:50:54 -0400

    add panel executor test
    - remove need of having to reload page between scenario;
    - find field in active modal or panel;

--- src/Behat/Context.php
+++ src/Behat/Context.php
@@ -278,6 +278,16 @@ class Context extends RawMinkContext implements BehatContext
     }
 
     /**
+     * @When I fill Modal field :arg1 with :arg2
+     */
+    public function iFillModalField(string $fieldName, string $value): void
+    {
+        $modal = $this->getElementInPage('.modal.transition.visible.active.front');
+        $field = $modal->find('named', array('field', $fieldName));
+        $field->setValue($value);
+    }
+
+    /**
      * @Then I hide js modal
      *
      * Hide js modal.
@@ -307,6 +317,16 @@ class Context extends RawMinkContext implements BehatContext
     {
         $panel = $this->getElementInPage('.atk-right-panel.atk-visible');
         $this->getElementInElement($panel, '//' . $tag . '[text()="' . $text . '"]', 'xpath');
+    }
+
+    /**
+     * @When I fill Panel field :arg1 with :arg2
+     */
+    public function iFillPanelField(string $fieldName, string $value): void
+    {
+        $panel = $this->getElementInPage('.atk-right-panel.atk-visible');
+        $field = $panel->find('named', array('field', $fieldName));
+        $field->setValue($value);
     }
 
     /**
