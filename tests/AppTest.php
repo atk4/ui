a999e05197565ba3c1ec3323637e672a42b5c08e
Michael Voříšek <mvorisek@mvorisek.cz>
2021-11-27 18:46:55 +0100

    Add test for unrecoverable App error (#1709)

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -6,11 +6,12 @@ namespace Atk4\Ui\Tests;
 
 use Atk4\Core\Phpunit\TestCase;
 use Atk4\Ui\App;
+use Atk4\Ui\Exception\LateOutputError;
 use Atk4\Ui\HtmlTemplate;
 
 class AppTest extends TestCase
 {
-    protected function getApp()
+    protected function getApp(): App
     {
         return new App([
             'catch_exceptions' => false,
@@ -40,5 +41,23 @@ class AppTest extends TestCase
             get_class($anotherTemplateClass),
             $app->loadTemplate('html.html')
         );
+    }
+
+    public function testUnexpectedOutputLateError(): void
+    {
+        $app = $this->getApp();
+
+        ob_start();
+        $testStr = 'direct output test';
+        try {
+            echo $testStr;
+
+            $this->expectException(LateOutputError::class);
+            $this->expectExceptionMessage('Unexpected output detected');
+            $app->terminateHtml('');
+        } finally {
+            $this->assertSame($testStr, ob_get_contents());
+            ob_end_clean();
+        }
     }
 }
