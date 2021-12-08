959dbc790ce9aed33a55e0db268b3aacd4a38464
Francesco Danti <fdanti@gmail.com>
2021-12-08 22:30:11 +0100

    Try Fix MemLeak in UnitTest 8.1 for Psr7 response

--- tests/AppPsr7Test.php
+++ tests/AppPsr7Test.php
@@ -11,63 +11,65 @@ use Atk4\Ui\Layout;
 
 class AppPsr7Test extends TestCase
 {
-    protected function getApp(): App
+    /**
+     * @var App
+     */
+    private $app;
+
+    protected function setUp(): void
     {
-        $app = new class(['call_exit' => false, 'catch_exceptions' => false, 'always_run' => false]) extends App {
+        parent::setUp();
+
+        $this->app = new class(['call_exit' => false, 'catch_exceptions' => false, 'always_run' => false]) extends App {
             protected function emitResponse(): void
             {
                 // no emitting to allow fast unit test
             }
         };
-        $app->initLayout([Layout::class]);
-
-        return $app;
+        $this->app->initLayout([Layout::class]);
     }
 
     public function testResponseHeaders(): void
     {
-        $app = $this->getApp();
-        $app->setResponseHeader('heAder-test', 'value1');
-        $app->setResponseHeader('Header-Test', 'value2');
+        $this->app->setResponseHeader('heAder-test', 'value1');
+        $this->app->setResponseHeader('Header-Test', 'value2');
 
         try {
-            $app->terminateHtml('', [
+            $this->app->terminateHtml('', [
                 'Header-Test' => 'value3',
             ]);
         } catch (ExitApplicationError $e) {
         }
 
-        $this->assertSame('value3', $app->getResponse()->getHeaderLine('header-test'));
+        $this->assertSame('value3', $this->app->getResponse()->getHeaderLine('header-test'));
     }
 
     public function testResponseHeadersRespectCase(): void
     {
-        $app = $this->getApp();
-        $app->setResponseHeader('heAder-test', 'value1');
-        $app->setResponseHeader('Header-Test', 'value2');
+        $this->app->setResponseHeader('heAder-test', 'value1');
+        $this->app->setResponseHeader('Header-Test', 'value2');
 
         try {
-            $app->terminateHtml('', [
+            $this->app->terminateHtml('', [
                 'Header-Test' => 'value3',
             ]);
         } catch (ExitApplicationError $e) {
         }
 
-        $this->assertSame('value3', $app->getResponse()->getHeaderLine('header-test'));
+        $this->assertSame('value3', $this->app->getResponse()->getHeaderLine('header-test'));
     }
 
     public function testResponseHeadersRespectContentOnTerminate(): void
     {
-        $app = $this->getApp();
-        $app->setResponseHeader('content-type', 'text/plain');
+        $this->app->setResponseHeader('content-type', 'text/plain');
 
         try {
-            $app->terminateHtml('', [
+            $this->app->terminateHtml('', [
                 'Content-type' => 'application/json',
             ]);
         } catch (ExitApplicationError $e) {
         }
 
-        $this->assertSame('text/html', $app->getResponse()->getHeaderLine('Content-Type'));
+        $this->assertSame('text/html', $this->app->getResponse()->getHeaderLine('Content-Type'));
     }
 }
