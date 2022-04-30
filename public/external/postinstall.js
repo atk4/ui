69af6367990ea4d06a59e09e1ae98917aafc5914
Michael Voříšek <mvorisek@mvorisek.cz>
2022-04-30 12:18:43 +0200

    move non-minified only if exists

--- public/external/postinstall.js
+++ public/external/postinstall.js
@@ -18,17 +18,19 @@ const updateFileSync = function (f, callback) {
     }
 };
 
+// copy non-minified JS to make it available from the same directory as the minified version
+if (fs.existsSync(path.join(__dirname, 'node_modules/form-serializer/jquery.serialize-object.js'))) {
+    fs.renameSync(
+        path.join(__dirname, 'node_modules/form-serializer/jquery.serialize-object.js'),
+        path.join(__dirname, 'node_modules/form-serializer/dist/jquery.serialize-object.js'),
+    );
+}
+
 // download Fomantic-UI license
 // remove once https://github.com/fomantic/Fomantic-UI/issues/2356 is fixed and v2.9.0 is released
 https.get(
     'https://raw.githubusercontent.com/fomantic/Fomantic-UI/2.8.8/LICENSE.md',
-    (response) => response.pipe(fs.createWriteStream(path.join(__dirname, 'node_modules/fomantic-ui-css/LICENSE.md')))
-);
-
-// copy non-minified JS to make it available from the same directory as the minified version
-fs.copyFileSync(
-    path.join(__dirname, 'node_modules/form-serializer/jquery.serialize-object.js'),
-    path.join(__dirname, 'node_modules/form-serializer/dist/jquery.serialize-object.js'),
+    (response) => response.pipe(fs.createWriteStream(path.join(__dirname, 'node_modules/fomantic-ui-css/LICENSE.md'))),
 );
 
 // normalize EOL of text files
