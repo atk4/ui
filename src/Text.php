1104e0e3c637eeaa7b0b0eb4fc13d6ba26f2c8bc
Michael Voříšek <mvorisek@mvorisek.cz>
2022-10-27 01:00:24 +0200

    no "forceReturn" param for rendering

--- src/Text.php
+++ src/Text.php
@@ -11,7 +11,7 @@ class Text extends View
 {
     public $defaultTemplate = false;
 
-    public function render(bool $forceReturn = true): string
+    public function render(): string
     {
         return $this->content;
     }
