0ea68c9a880a28a74a5c20efd51951ea5cc068a9
mhuser <mhuser@users.noreply.github.com>
2022-10-28 17:11:14 +0200

    Add demo to process multiple selection in grid

--- demos/collection/grid.php
+++ demos/collection/grid.php
@@ -11,10 +11,14 @@ use Atk4\Ui\Jquery;
 use Atk4\Ui\JsExpression;
 use Atk4\Ui\JsReload;
 use Atk4\Ui\JsToast;
+use Atk4\Ui\JsModal;
 use Atk4\Ui\Message;
 use Atk4\Ui\Table;
 use Atk4\Ui\UserAction\BasicExecutor;
 use Atk4\Ui\View;
+use Atk4\Ui\VirtualPage;
+use Atk4\Ui\Modal;
+use Atk4\Ui\Form;
 
 /** @var \Atk4\Ui\App $app */
 require_once __DIR__ . '/../init-app.php';
@@ -65,10 +69,49 @@ $deleteExecutor->onHook(BasicExecutor::HOOK_AFTER_EXECUTE, function () {
 // $grid->addExecutorButton($deleteExecutor, new Button(['icon' => 'times circle outline']));
 
 $sel = $grid->addSelection();
+// Executing a modal on a bulk selection
+$callback = function ($m, $ids) use ($grid) {
+    if(!$ids){
+        $msg = Message::addTo($m, [
+            'No records were selected.',
+            'type' => 'error',
+            'icon' => 'times',
+        ]);
+    } else {
+        $msg = Message::addTo($m, [
+            'The selected records will be permanently deleted.',
+            'type' => 'warning',
+            'icon' => 'warning',
+        ]);
+        $msg->text->addParagraph('Ids that will be deleted:');
+        foreach($ids as $id){
+            $msg->text->addParagraph($id);
+        }
+        
+        $f = Form::addTo($m);
+        $f->buttonSave->set('Delete');
+        $f->buttonSave->icon = 'trash';
+        $f->onSubmit(function ($f) use ($grid, $ids) {
+            // iterate trough the selected id and delete them.
+            foreach($ids as $id){
+                $grid->model->delete($id);
+            }
+            return [[$grid->jsReload(), $f->success()]];
+        });
+    }
+};
+$modal = Modal::addTo($grid, ['Delete selected']);
+$modal->set(function (View $t) use ($callback, $grid) {
+    $callback($t, $t->stickyGet($grid->name)?explode(',', $t->stickyGet($grid->name)):False);
+});
+
+$grid->menu->addItem(['Delete selected', 'icon' => 'trash', 'class.orange active' => true])
+    ->on('click', $modal->jsShow(array_merge([$grid->name => $grid->selection->jsChecked()])), []);
 $grid->menu->addItem('show selection')->on('click', new JsExpression(
     'alert(\'Selected: \' + [])',
     [$sel->jsChecked()]
 ));
+
 
 // Setting ipp with an array will add an ItemPerPageSelector to paginator.
 $grid->setIpp([10, 25, 50, 100]);
