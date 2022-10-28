8844ce2b994b04139fd3d353dbc3029de7dea374
mhuser <mhuser@users.noreply.github.com>
2022-10-28 17:20:11 +0200

    ordered imports

--- demos/collection/grid.php
+++ demos/collection/grid.php
@@ -6,19 +6,18 @@ namespace Atk4\Ui\Demos;
 
 use Atk4\Data\Model;
 use Atk4\Ui\Button;
+use Atk4\Ui\Form;
 use Atk4\Ui\Grid;
 use Atk4\Ui\Jquery;
 use Atk4\Ui\JsExpression;
 use Atk4\Ui\JsReload;
 use Atk4\Ui\JsToast;
-use Atk4\Ui\JsModal;
 use Atk4\Ui\Message;
+use Atk4\Ui\Modal;
 use Atk4\Ui\Table;
 use Atk4\Ui\UserAction\BasicExecutor;
 use Atk4\Ui\View;
-use Atk4\Ui\VirtualPage;
-use Atk4\Ui\Modal;
-use Atk4\Ui\Form;
+
 
 /** @var \Atk4\Ui\App $app */
 require_once __DIR__ . '/../init-app.php';
