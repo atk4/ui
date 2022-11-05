1b1838211e67cca5760d43f49bedbe9e491b5d9d
mhuser <mhuser@users.noreply.github.com>
2022-11-05 21:15:15 +0100

    more js syntax

--- js/src/helpers/grid-checkbox.helper.js
+++ js/src/helpers/grid-checkbox.helper.js
@@ -4,18 +4,18 @@
  * content checkboxes to check or uncheck. A partially checked master checkbox
  * is displayed if appopriate.
  */
-function InitGridMasterCheckbox () {
+function InitGridMasterCheckbox() {
     $('.table .master.checkbox').checkbox({
         // check all children
         onChecked: function () {
-            let $childCheckbox = $(this).closest('.table').find('.child.checkbox');
+            const $childCheckbox = $(this).closest('.table').find('.child.checkbox');
             $childCheckbox.checkbox('check');
         },
         // uncheck all children
         onUnchecked: function () {
-            let $childCheckbox = $(this).closest('.table').find('.child.checkbox');
+            const $childCheckbox = $(this).closest('.table').find('.child.checkbox');
             $childCheckbox.checkbox('uncheck');
-        }
+        },
     });
 }
 
@@ -26,31 +26,28 @@ function InitGridChildCheckbox() {
 
         // Change parent state on each child checkbox change
         onChange: function () {
-            let $listGroup = $(this).closest('.table');
-            let $parentCheckbox = $listGroup.find('.master.checkbox');
+            const $listGroup = $(this).closest('.table');
+            const $parentCheckbox = $listGroup.find('.master.checkbox');
             let $checkbox = $listGroup.find('.child.checkbox');
             let allChecked = true;
             let allUnchecked = true;
-                
+
             // check to see if all other siblings are checked or unchecked
             $checkbox.each(function () {
                 if ($(this).checkbox('is checked')) {
                     allUnchecked = false;
-                }
-                else {
+                } else {
                     allChecked = false;
                 }
             });
             // set parent checkbox state, but don't trigger its onChange callback
-            if  (allChecked) {
+            if (allChecked) {
                 $parentCheckbox.checkbox('set checked');
-            }
-            else if (allUnchecked) {
+            } else if (allUnchecked) {
                 $parentCheckbox.checkbox('set unchecked');
-            }
-            else {
+            } else {
                 $parentCheckbox.checkbox('set indeterminate');
             }
-        }
+        },
     });
 }
