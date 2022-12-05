9104d2118af086b9d859f585f3d4a6b26627eb2a
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-05 02:33:47 +0100

    use $this + find for CRUD row with ID location

--- src/Crud.php
+++ src/Crud.php
@@ -194,7 +194,7 @@ class Crud extends Grid
             case Model\UserAction::MODIFIER_DELETE:
                 // use deleted record id to remove row, fallback to closest tr if id is not available.
                 $js = $this->deletedId
-                    ? new Jquery('tr[data-id="' . $this->deletedId . '"]')
+                    ? $this->js(false, null, 'tr[data-id="' . $this->deletedId . '"]')
                     : (new Jquery())->closest('tr');
                 $js = $js->transition('fade left', new JsFunction([], [new JsExpression('this.remove()')]));
 
