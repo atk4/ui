8132fb9095b30703b3f8b7c3df625972808402d1
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-14 19:48:02 +0100

    fix font-family-no-missing-generic-family-keyword

--- js/.stylelintrc.js
+++ js/.stylelintrc.js
@@ -16,7 +16,6 @@ module.exports = {
 
         // TODO
         'at-rule-no-unknown': null, // 1 error: Unexpected unknown at-rule "@atkFooterHeight" (a stylelint bug?)
-        'font-family-no-missing-generic-family-keyword': null, // 1 error that should be ignored with a comment
         'function-no-unknown': null, // 2 errors
         'no-duplicate-selectors': null, // 3 errors
         'selector-class-pattern': null, // 14 errors, example: Expected class selector ".atk-topMenu" to be kebab-case
