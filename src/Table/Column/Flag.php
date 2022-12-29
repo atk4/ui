c9b6dd007bac379afca889f30e7a204db0712aac
Michael Voříšek <mvorisek@mvorisek.cz>
2022-12-29 15:42:45 +0100

    Apply suggestions from code review

--- src/Table/Column/Flag.php
+++ src/Table/Column/Flag.php
@@ -8,16 +8,13 @@ use Atk4\Data\Field;
 use Atk4\Data\Model;
 use Atk4\Ui\Table;
 
-/**
- * Column for formatting country code as flags.
- */
-class Flag extends Table\Column
+class CountryFlag extends Table\Column
 {
-    /** @var string Name of model field which contains country ALPHA-2 (2 letter) codes. */
-    public $codeField;
+    /** Name of country code model field (in ISO 3166-1 alpha-2 format) */
+    public string $codeField;
 
-    /** @var string|null Optional name of model field which contains country names. */
-    public $nameField;
+    /** Optional name of model field which contains full country name. */
+    public ?string $nameField = null;
 
     public function getHtmlTags(Model $row, ?Field $field): array
     {
@@ -25,10 +22,12 @@ class Flag extends Table\Column
         $countryName = $this->nameField ? $row->get($this->nameField) : null;
 
         return [
-            $field->shortName => $countryCode === null ? '' : $this->getApp()->getTag('i', [
-                'class' => strtolower($countryCode) . ' flag',
-                'title' => strtoupper($countryCode) . ($countryName === null ? '' : ' - ' . $countryName),
-            ]),
+            $field->shortName => $countryCode === null
+                ? ''
+                : $this->getApp()->getTag('i', [
+                    'class' => strtolower($countryCode) . ' flag',
+                    'title' => strtoupper($countryCode) . ($countryName === null ? '' : ' - ' . $countryName),
+                ]),
         ];
     }
 }
