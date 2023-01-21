c7484b4c0d585232d84ea876022144d72cee78f6
Michael Krecek <michael@krecek.net>
2023-01-21 08:26:00 +0100

    Fix non-breaking horizontal menus on mobiles

--- src/Menu.php
+++ src/Menu.php
@@ -9,7 +9,7 @@ use Atk4\Ui\Js\JsExpressionable;
 
 class Menu extends View
 {
-    public $ui = 'menu';
+    public $ui = 'menu stackable';
 
     /**
      * If you set this to false, then upon clicking on the item, it won't
