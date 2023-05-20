458e8b00fb3fde9e4fd6b20544ce11909aa8f209
Michael Voříšek <mvorisek@mvorisek.cz>
2023-05-20 11:29:00 +0200

    fix static::assertSameTemplate by regex

--- tests/HtmlTemplateTest.php
+++ tests/HtmlTemplateTest.php
@@ -24,7 +24,7 @@ class HtmlTemplateTest extends TestCase
 
     protected static function assertSameTagTree(string $expectedTemplateStr, HtmlTemplate\TagTree $tagTree): void
     {
-        static::assertSameTemplate(
+        self::assertSameTemplate(
             $expectedTemplateStr,
             $tagTree->getParentTemplate()->cloneRegion($tagTree->getTag())
         );
@@ -35,7 +35,7 @@ class HtmlTemplateTest extends TestCase
         $t = new HtmlTemplate('hello, {foo}world{/}');
         $t->set('foo', 'bar');
 
-        static::assertSameTemplate('hello, {foo}bar{/}', $t);
+        self::assertSameTemplate('hello, {foo}bar{/}', $t);
     }
 
     public function testGetTagTree(): void
@@ -48,7 +48,7 @@ class HtmlTemplateTest extends TestCase
         static::assertSameTagTree('hello', $tagTreeFoo);
 
         $tagTreeFoo->getChildren()[0]->set('good bye');
-        static::assertSameTemplate('{foo}good bye{/}, cruel {bar}world{/}. {foo}good bye{/}', /* not possible with dual renderer $t */ $tagTreeFoo->getParentTemplate());
+        self::assertSameTemplate('{foo}good bye{/}, cruel {bar}world{/}. {foo}good bye{/}', /* not possible with dual renderer $t */ $tagTreeFoo->getParentTemplate());
     }
 
     public function testGetTagRefNotFoundException(): void
@@ -97,39 +97,39 @@ class HtmlTemplateTest extends TestCase
         // del tests
         $t->del('foo');
         $t->del(['foo']);
-        static::assertSameTemplate('{$foo} guys', $t);
+        self::assertSameTemplate('{$foo} guys', $t);
         $t->tryDel('foo');
         $t->tryDel('non_existent_tag');
         $t->tryDel(['a', 'b']);
-        static::assertSameTemplate('{$foo} guys', $t);
+        self::assertSameTemplate('{$foo} guys', $t);
 
         // set tests
         $t->set('foo', 'Hello');
-        static::assertSameTemplate('{foo}Hello{/} guys', $t);
+        self::assertSameTemplate('{foo}Hello{/} guys', $t);
         $t->set('foo', 'Hi');
-        static::assertSameTemplate('{foo}Hi{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi{/} guys', $t);
         $t->dangerouslySetHtml('foo', '<b>Hi</b>');
-        static::assertSameTemplate('{foo}<b>Hi</b>{/} guys', $t);
+        self::assertSameTemplate('{foo}<b>Hi</b>{/} guys', $t);
         $t->trySet('non_existent_tag', 'ignore this');
-        static::assertSameTemplate('{foo}<b>Hi</b>{/} guys', $t);
+        self::assertSameTemplate('{foo}<b>Hi</b>{/} guys', $t);
         $t->tryDangerouslySetHtml('non_existent_tag', '<b>ignore</b> this');
-        static::assertSameTemplate('{foo}<b>Hi</b>{/} guys', $t);
+        self::assertSameTemplate('{foo}<b>Hi</b>{/} guys', $t);
 
         // append tests
         $t->set('foo', 'Hi');
-        static::assertSameTemplate('{foo}Hi{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi{/} guys', $t);
         $t->append('foo', ' and');
-        static::assertSameTemplate('{foo}Hi and{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi and{/} guys', $t);
         $t->dangerouslyAppendHtml('foo', ' <b>welcome</b> my');
-        static::assertSameTemplate('{foo}Hi and <b>welcome</b> my{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi and <b>welcome</b> my{/} guys', $t);
         $t->tryAppend('foo', ' dear');
-        static::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear{/} guys', $t);
         $t->tryAppend('non_existent_tag', 'ignore this');
-        static::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear{/} guys', $t);
         $t->tryDangerouslyAppendHtml('foo', ' and <b>smart</b>');
-        static::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear and <b>smart</b>{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear and <b>smart</b>{/} guys', $t);
         $t->tryDangerouslyAppendHtml('non_existent_tag', '<b>ignore</b> this');
-        static::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear and <b>smart</b>{/} guys', $t);
+        self::assertSameTemplate('{foo}Hi and <b>welcome</b> my dear and <b>smart</b>{/} guys', $t);
     }
 
     public function testValueEncoded(): void
@@ -159,12 +159,12 @@ class HtmlTemplateTest extends TestCase
         $t = new HtmlTemplate('{foo}{inner}hello{/}{/} guys');
 
         $topClone1 = clone $t;
-        static::assertSameTemplate('{foo}{inner}hello{/}{/} guys', $topClone1);
+        self::assertSameTemplate('{foo}{inner}hello{/}{/} guys', $topClone1);
         $topClone2 = $t->cloneRegion('_top');
-        static::assertSameTemplate('{foo}{inner}hello{/}{/} guys', $topClone2);
-        static::assertSameTemplate('{inner}hello{/}', $t->cloneRegion('foo'));
-        static::assertSameTemplate('{inner}hello{/}', $topClone1->cloneRegion('foo'));
-        static::assertSameTemplate('{inner}hello{/}', $topClone2->cloneRegion('foo'));
+        self::assertSameTemplate('{foo}{inner}hello{/}{/} guys', $topClone2);
+        self::assertSameTemplate('{inner}hello{/}', $t->cloneRegion('foo'));
+        self::assertSameTemplate('{inner}hello{/}', $topClone1->cloneRegion('foo'));
+        self::assertSameTemplate('{inner}hello{/}', $topClone2->cloneRegion('foo'));
     }
 
     public function testRenderRegion(): void
@@ -180,7 +180,7 @@ class HtmlTemplateTest extends TestCase
             'foo' => 'Hello',
             'bar' => 'welcome',
         ]);
-        static::assertSameTemplate('{foo}Hello{/} guys and {bar}welcome{/} here', $t);
+        self::assertSameTemplate('{foo}Hello{/} guys and {bar}welcome{/} here', $t);
     }
 
     public function testSetFromEntity(): void
@@ -195,7 +195,7 @@ class HtmlTemplateTest extends TestCase
         $t = new HtmlTemplate('{$foo} {$bar}');
         $t->setApp($this->createApp());
         $t->set($entity);
-        static::assertSameTemplate('{foo}Hello{/foo} {bar}&lt;br&gt;{/bar}', $t);
+        self::assertSameTemplate('{foo}Hello{/foo} {bar}&lt;br&gt;{/bar}', $t);
     }
 
     public function testTagNotDefinedException(): void
