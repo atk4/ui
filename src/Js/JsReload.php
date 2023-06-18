samsouthardjr/atk4-ui

Revert "track running callbacks using static variable"

src/JsReload.php
@@ -59,7 +59,7 @@ class JsReload implements JsExpressionable
         $final = (new Jquery($this->view))
             ->atkReloadView(
                 [
-                    'uri' => $this->view->jsUrl(array_merge(['__atk_reload' => $this->view->name], Callback::getRunningCallbackArgs())),
+                    'uri' => $this->view->jsUrl(['__atk_reload' => $this->view->name]),
                     'uri_options' => !empty($this->args) ? $this->args : null,
                     'afterSuccess' => $this->afterSuccess ? $this->afterSuccess->jsRender() : null,
                     'apiConfig' => !empty($this->apiConfig) ? $this->apiConfig : null,

atk4/ui

src/Js/JsReload.php
@@ -49,7 +49,7 @@ class JsReload implements JsExpressionable
         $final = (new Jquery($this->view))
             ->atkReloadView(
                 [
-                    'url' => $this->view->jsUrl(array_merge(['__atk_reload' => $this->view->name], Callback::getRunningCallbackArgs())),
+                    'url' => $this->view->jsUrl(['__atk_reload' => $this->view->name]),
                     'urlOptions' => $this->args !== [] ? $this->args : null,
                     'afterSuccess' => $this->afterSuccess ? $this->afterSuccess->jsRender() : null,
                     'apiConfig' => $this->apiConfig !== [] ? $this->apiConfig : null,