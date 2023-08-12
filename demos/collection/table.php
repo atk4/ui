32d9b7e4c4e9f87629aa697bc40017c32f75fc19
Michael Voříšek <mvorisek@mvorisek.cz>
2023-08-12 22:56:47 +0200

    FIX REVERT to match new file paths

--- demos/collection/table.php
+++ demos/collection/table.php
@@ -1,62 +1,32 @@
 <?php
 
-declare(strict_types=1);
+date_default_timezone_set('UTC');
+include 'init.php';
 
-namespace Atk4\Ui\Demos;
+$bb = $app->add(['View', 'ui' => 'buttons']);
+$table = $app->add(['Table', 'celled' => true]);
 
-use Atk4\Data\Model;
-use Atk4\Ui\Button;
-use Atk4\Ui\Js\JsReload;
-use Atk4\Ui\Js\JsToast;
-use Atk4\Ui\Table;
-use Atk4\Ui\View;
+$bb->add(['Button', 'Refresh Table', 'icon' => 'refresh'])
+    ->on('click', new \atk4\ui\jsReload($table));
 
-/** @var \Atk4\Ui\App $app */
-require_once __DIR__ . '/../init-app.php';
+$bb->on('click', $table->js()->reload());
 
-if ($_GET['id'] ?? null) {
-    $app->layout->js(true, new JsToast('Details link is in simulation mode.'));
-}
+$table->setModel(new SomeData(), false);
 
-$bb = View::addTo($app, ['ui' => 'buttons']);
-
-$table = Table::addTo($app, ['class.celled' => true]);
-Button::addTo($bb, ['Refresh Table', 'icon' => 'refresh'])
-    ->on('click', new JsReload($table));
-
-$table->setModel(new SomeData(), []);
-
-$table->addColumn('name', new Table\Column\Link(['table', 'foo' => 'bar'], ['person_id' => 'id'], ['target' => '_blank']));
-$table->addColumn('surname', new Table\Column\Template('{$surname}'))->addClass('warning');
-$table->addColumn('title', new Table\Column\Status([
+$table->addColumn('name', new \atk4\ui\TableColumn\Link(['details', 'id' => '{$id}']));
+$table->addColumn('surname', new \atk4\ui\TableColumn\Template('{$surname}'))->addClass('warning');
+$table->addColumn('title', new \atk4\ui\TableColumn\Status([
     'positive' => ['Prof.'],
     'negative' => ['Dr.'],
 ]));
 
 $table->addColumn('date');
-$table->addColumn('salary', new Table\Column\Money());
-$table->addColumn('logo_url', [Table\Column\Image::class, 'caption' => 'Our Logo']);
+$table->addColumn('salary', new \atk4\ui\TableColumn\Money()); //->addClass('right aligned single line', 'all'));
 
-$table->onHook(Table\Column::HOOK_GET_HTML_TAGS, function (Table $table, Model $row) {
-    switch ($row->getId()) {
-        case 1:
-            $color = 'yellow';
-
-            break;
-        case 2:
-            $color = 'grey';
-
-            break;
-        case 3:
-            $color = 'brown';
-
-            break;
-        default:
-            $color = '';
-    }
-    if ($color) {
+$table->addHook('getHTMLTags', function ($table, $row) {
+    if ($row->id == 1) {
         return [
-            'name' => $table->getApp()->getTag('div', ['class' => 'ui ribbon ' . $color . ' label'], $row->get('name')),
+            'name' => $table->app->getTag('div', ['class' => 'ui ribbon label'], $row['name']),
         ];
     }
 });
@@ -66,19 +36,18 @@ $table->addTotals([
     'salary' => ['sum'],
 ]);
 
-$myArray = [
-    ['name' => 'Vinny', 'surname' => 'Sihra', 'birthdate' => '1973-02-03', 'cv' => 'I am <strong>BIG</strong> Vinny'],
-    ['name' => 'Zoe', 'surname' => 'Shatwell', 'birthdate' => '1958-08-21', 'cv' => null],
-    ['name' => 'Darcy', 'surname' => 'Wild', 'birthdate' => '1968-11-01', 'cv' => 'I like <i style="color: orange;>icecream</i>'],
-    ['name' => 'Brett', 'surname' => 'Bird', 'birthdate' => '1988-12-20', 'cv' => null],
-];
+    $my_array = [
+        ['name' => 'Vinny', 'surname' => 'Sihra', 'birthdate' => new \DateTime('1973-02-03')],
+        ['name' => 'Zoe', 'surname' => 'Shatwell', 'birthdate' => new \DateTime('1958-08-21')],
+        ['name' => 'Darcy', 'surname' => 'Wild', 'birthdate' => new \DateTime('1968-11-01')],
+        ['name' => 'Brett', 'surname' => 'Bird', 'birthdate' => new \DateTime('1988-12-20')],
+    ];
 
-$table = Table::addTo($app);
-$table->setSource($myArray, ['name']);
+    $table = $app->add('Table');
+    $table->setSource($my_array, ['name']);
 
-// $table->addColumn('name');
-$table->addColumn('surname', [Table\Column\Link::class, 'url' => 'table.php?id={$surname}']);
-$table->addColumn('birthdate', [], ['type' => 'date']);
-$table->addColumn('cv', [Table\Column\Html::class]);
+    //$table->addColumn('name');
+    $table->addColumn('surname', ['Link', 'url' => 'details.php?surname={$surname}']);
+    $table->addColumn('birthdate', null, ['type' => 'date']);
 
-$table->getColumnDecorators('name')[0]->addClass('disabled');
+    $table->getColumnDecorators('name')[0]->addClass('disabled');
