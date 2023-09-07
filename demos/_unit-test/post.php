c778a65b7ae726030c2b26fe9286865e1d2524d1
abbadon1334 <fdanti@gmail.com>
2023-09-07 23:29:37 +0200

    Fix JSON POST test

--- demos/_unit-test/post.php
+++ demos/_unit-test/post.php
@@ -10,6 +10,14 @@ use Atk4\Ui\Js\JsToast;
 /** @var \Atk4\Ui\App $app */
 require_once __DIR__ . '/../init-app.php';
 
+// Creation of request from globals depenmds on getallheaders
+// which is not available in CLI mode
+if (\PHP_SAPI === 'cli') {
+    $app->setDefaults([
+        'request' => $app->getRequest()->withParsedBody($_POST),
+    ]);
+}
+
 $form = Form::addTo($app);
 $form->cb->setUrlTrigger('test_submit');
 
