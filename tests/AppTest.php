1e411bfe22e8c10d4e3a029c1f4fa62e879ed9ad
Michael Voříšek <mvorisek@mvorisek.cz>
2023-09-17 15:58:57 +0200

    fix typo

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -118,7 +118,7 @@ class AppTest extends TestCase
      * @param array<0|string, string|int|false> $page
      * @param array<string, string>             $extraRequestUrlArgs
      */
-    public function testUrl(string $requestUrl, array $appStickyGetArguments, array $page, array $extraRequestUrlArgs, string $exceptedUrl): void
+    public function testUrl(string $requestUrl, array $appStickyGetArguments, array $page, array $extraRequestUrlArgs, string $expectedUrl): void
     {
         $request = (new Psr17Factory())->createServerRequest('GET', $requestUrl);
 
@@ -126,12 +126,12 @@ class AppTest extends TestCase
             'request' => $request,
             'stickyGetArguments' => $appStickyGetArguments,
         ]);
-        self::assertSame($exceptedUrl, $app->url($page, $extraRequestUrlArgs));
+        self::assertSame($expectedUrl, $app->url($page, $extraRequestUrlArgs));
         $pageAssocOnly = array_diff_key($page, [true]);
-        self::assertSame($exceptedUrl, $app->url(($page[0] ?? '') . (count($pageAssocOnly) > 0 ? '?' . implode('&', array_map(static fn ($k) => $k . '=' . $pageAssocOnly[$k], array_keys($pageAssocOnly))) : ''), $extraRequestUrlArgs));
-        self::assertSame($exceptedUrl, $app->jsUrl($page, array_merge(['__atk_json' => null], $extraRequestUrlArgs)));
+        self::assertSame($expectedUrl, $app->url(($page[0] ?? '') . (count($pageAssocOnly) > 0 ? '?' . implode('&', array_map(static fn ($k) => $k . '=' . $pageAssocOnly[$k], array_keys($pageAssocOnly))) : ''), $extraRequestUrlArgs));
+        self::assertSame($expectedUrl, $app->jsUrl($page, array_merge(['__atk_json' => null], $extraRequestUrlArgs)));
 
-        $replaceExpectedUrlFx = static function (string $indexPage, string $ext) use ($page, $exceptedUrl) {
+        $replaceExpectedUrlFx = static function (string $indexPage, string $ext) use ($page, $expectedUrl) {
             return preg_replace_callback('~^[^?]*?\K([^/?]*)(\.php)(?=\?|$)~', static function ($matches) use ($page, $indexPage, $ext) {
                 if ($matches[1] === 'index' && !preg_match('~(^|/)index(\.php)?(?=\?|$)~', $page[0] ?? '')) {
                     $matches[1] = $indexPage;
@@ -141,7 +141,7 @@ class AppTest extends TestCase
                 }
 
                 return $matches[1] . $matches[2];
-            }, $exceptedUrl, 1);
+            }, $expectedUrl, 1);
         };
 
         $app = $this->createApp([
@@ -150,8 +150,8 @@ class AppTest extends TestCase
             'urlBuildingIndexPage' => 'default',
             'urlBuildingExt' => '.php8',
         ]);
-        $exceptedUrlCustom = $replaceExpectedUrlFx('default', '.php8');
-        self::assertSame($exceptedUrlCustom, $app->url($page, $extraRequestUrlArgs));
+        $expectedUrlCustom = $replaceExpectedUrlFx('default', '.php8');
+        self::assertSame($expectedUrlCustom, $app->url($page, $extraRequestUrlArgs));
 
         $app = $this->createApp([
             'request' => $request,
@@ -159,8 +159,8 @@ class AppTest extends TestCase
             'urlBuildingIndexPage' => '',
             'urlBuildingExt' => '',
         ]);
-        $exceptedUrlAutoindex = $replaceExpectedUrlFx('', '');
-        self::assertSame($exceptedUrlAutoindex, $app->url($page, $extraRequestUrlArgs));
+        $expectedUrlAutoindex = $replaceExpectedUrlFx('', '');
+        self::assertSame($expectedUrlAutoindex, $app->url($page, $extraRequestUrlArgs));
 
         $app = $this->createApp([
             'request' => $request,
@@ -168,7 +168,7 @@ class AppTest extends TestCase
             'urlBuildingIndexPage' => '',
             'urlBuildingExt' => '.html',
         ]);
-        $exceptedUrlAutoindex2 = $replaceExpectedUrlFx('', '.html');
-        self::assertSame($exceptedUrlAutoindex2, $app->url($page, $extraRequestUrlArgs));
+        $expectedUrlAutoindex2 = $replaceExpectedUrlFx('', '.html');
+        self::assertSame($expectedUrlAutoindex2, $app->url($page, $extraRequestUrlArgs));
     }
 }
