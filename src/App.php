0ac8e443f1e55e2784ed2db76dd8457937ebcd3a
Michael Voříšek <mvorisek@mvorisek.cz>
2023-12-25 14:45:55 +0100

    Handle fatal error as exception

--- src/App.php
+++ src/App.php
@@ -36,12 +36,14 @@ class App
     use DynamicMethodTrait;
     use HookTrait;
 
-    private const UNSUPPRESSEABLE_ERROR_LEVELS = \PHP_MAJOR_VERSION >= 8 ? (\E_ERROR | \E_PARSE | \E_CORE_ERROR | \E_COMPILE_ERROR | \E_USER_ERROR | \E_RECOVERABLE_ERROR) : 0;
+    private const UNHANDLEABLE_ERROR_LEVELS = \E_ERROR | \E_PARSE | \E_CORE_ERROR | \E_CORE_WARNING | \E_COMPILE_ERROR | \E_COMPILE_WARNING;
+    private const UNSUPPRESSIBLE_ERROR_LEVELS = \PHP_MAJOR_VERSION >= 8 ? (\E_ERROR | \E_PARSE | \E_CORE_ERROR | \E_COMPILE_ERROR | \E_USER_ERROR | \E_RECOVERABLE_ERROR) : 0;
 
     public const HOOK_BEFORE_EXIT = self::class . '@beforeExit';
     public const HOOK_BEFORE_RENDER = self::class . '@beforeRender';
 
     private static ?string $shutdownReservedMemory; // @phpstan-ignore-line
+    private static ?int $errorReportingLevel = null;
 
     /** @var array|false Location where to load JS/CSS files */
     public $cdn = [
@@ -185,8 +187,13 @@ class App
         // set our exception handler
         if ($this->catchExceptions) {
             set_exception_handler(\Closure::fromCallable([$this, 'caughtException']));
-            set_error_handler(static function (int $severity, string $msg, string $file, int $line): bool {
-                if ((error_reporting() & ~self::UNSUPPRESSEABLE_ERROR_LEVELS) === 0) {
+
+            $createErrorExceptionArgsFx = static function (int $severity, string $msg, string $file, int $line) {
+                return [$msg, 0, $severity, $file, $line];
+            };
+
+            set_error_handler(function (int $severity, string $msg, string $file, int $line) use ($createErrorExceptionArgsFx): bool {
+                if ((error_reporting() & ~self::UNSUPPRESSIBLE_ERROR_LEVELS) === 0) {
                     $isFirstFrame = true;
                     foreach (array_slice(debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS, 10), 1) as $frame) {
                         // allow to suppress any warning outside Atk4
@@ -205,8 +212,17 @@ class App
                     }
                 }
 
-                throw new \ErrorException($msg, 0, $severity, $file, $line);
+                $this->throwOrCaughtExceptionIfInShutdown(new \ErrorException(...$createErrorExceptionArgsFx($severity, $msg, $file, $line)));
             });
+
+            register_shutdown_function(function () use ($createErrorExceptionArgsFx): void {
+                $error = error_get_last();
+                if ($error !== null && ($error['type'] & self::UNHANDLEABLE_ERROR_LEVELS) !== 0) {
+                    $this->throwOrCaughtExceptionIfInShutdown(new \ErrorException(...$createErrorExceptionArgsFx($error['type'], $error['message'], $error['file'], $error['line'])));
+                }
+            });
+            self::$errorReportingLevel = error_reporting();
+            error_reporting(self::$errorReportingLevel & ~self::UNHANDLEABLE_ERROR_LEVELS);
 
             http_response_code(500);
             header('Content-Type: text/plain');
@@ -280,6 +296,22 @@ class App
 
             exit;
         }
+    }
+
+    /**
+     * @return never
+     */
+    private function throwOrCaughtExceptionIfInShutdown(\Throwable $exception): void
+    {
+        if (\PHP_VERSION_ID < 80300 && self::$shutdownReservedMemory === null) {
+            // remove method once PHP 8.2 support is dropped
+            // https://github.com/php/php-src/issues/10695
+            $this->caughtException($exception);
+
+            exit(1);
+        }
+
+        throw $exception;
     }
 
     protected function caughtException(\Throwable $exception): void
@@ -1235,5 +1267,9 @@ class App
 
     register_shutdown_function(static function (): void {
         App::$shutdownReservedMemory = null;
+
+        if (App::$errorReportingLevel !== null) {
+            error_reporting(error_reporting() | (App::$errorReportingLevel & App::UNHANDLEABLE_ERROR_LEVELS));
+        }
     });
 }, null, App::class)();
