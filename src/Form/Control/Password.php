dd32436367cb6525ae44d4c6956565ca564df8f2
Rickard Osser <ricky@osser.se>
2024-08-18 14:01:25 +0200

    Added 'eye-function' to toggle password visibility to form-passwords.

--- src/Form/Control/Password.php
+++ src/Form/Control/Password.php
@@ -7,4 +7,39 @@ namespace Atk4\Ui\Form\Control;
 class Password extends Line
 {
     public string $inputType = 'password';
+
+    /**
+     * @var bool By setting this variable to true you get an Eye-button on the right side which can toggle the visibility of the entered password.
+     */
+    public $eye;
+
+    protected function init(): void
+    {
+        parent::init();
+
+        if($this->eye)
+        {
+            $button = new Button([
+                'class.grey' => true,
+                'iconRight' => 'eye slash',
+            ]);
+            
+            $this->action = $button;
+            
+            $button->on('click', [
+                $jsExpression = new \atk4\ui\JsExpression("
+var input_element = document.querySelector(`[id$='form_layout_" . $this->shortName ."_input']`);
+var icon_element = document.querySelector(`[id$='" . $this->shortName . "_button_icon']`);
+
+if(input_element.getAttribute('type') == 'password') {
+  input_element.setAttribute('type', 'text');
+  icon_element.classList.remove(['slash']);
+} else {
+  input_element.setAttribute('type', 'password');
+  icon_element.classList.add(['slash']);
+}
+"),
+            ]);
+        }
+    }
 }
