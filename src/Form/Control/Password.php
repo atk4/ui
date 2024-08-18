2603659806873bfcaf239acdca347a0c1f52b143
Rickard Osser <ricky@osser.se>
2024-08-18 14:23:26 +0200

    Fixed coding-style.

--- src/Form/Control/Password.php
+++ src/Form/Control/Password.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace Atk4\Ui\Form\Control;
 
+use Atk4\ui\JsExpression;
+use Atk4\Ui\Button;
+
 class Password extends Line
 {
     public string $inputType = 'password';
 
     /**
-     * @var bool By setting this variable to true you get an Eye-button on the right side which can toggle the visibility of the entered password.
+     * @var bool by setting this variable to true you get an Eye-button on the right side which can toggle the visibility of the entered password
      */
     public $eye;
 
-    protected function init(): void
-    {
+    protected function init(): void {
         parent::init();
 
         if($this->eye)
@@ -27,7 +29,7 @@ class Password extends Line
             $this->action = $button;
             
             $button->on('click', [
-                $jsExpression = new \atk4\ui\JsExpression("
+                $jsExpression = new JsExpression("
 var input_element = document.querySelector(`[id$='form_layout_" . $this->shortName ."_input']`);
 var icon_element = document.querySelector(`[id$='" . $this->shortName . "_button_icon']`);
 
