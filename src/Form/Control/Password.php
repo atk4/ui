b46ad364b7aa53deb511065e881557ee999fcb6a
Rickard Osser <ricky@osser.se>
2024-08-19 02:28:13 +0200

    More coding-style fixes.

--- src/Form/Control/Password.php
+++ src/Form/Control/Password.php
@@ -12,14 +12,14 @@ class Password extends Line
     public string $inputType = 'password';
 
     /** By setting this variable to true you get an Eye-button on the right side which can toggle the visibility of the entered password */
-    public bool $eye;
+    public bool $revealEye = false;
 
     #[\Override]
     protected function init(): void
     {
         parent::init();
 
-        if ($this->eye) {
+        if ($this->revealEye) {
             $button = new Button([
                 'class.grey' => true,
                 'iconRight' => 'eye slash',
@@ -30,18 +30,18 @@ class Password extends Line
             $button->on(
                 'click',
                 new JsExpression(
-                    <<<EOF
-                        let input_element = document.querySelector(`[id$='form_layout_{$this->shortName}_input']`);
-                        let icon_element = document.querySelector(`[id$='{$this->shortName}_button_icon']`);
+                    <<<"EOF"
+                    let input_element = document.getElementById('{$this->name}_input');
+                    let icon_element = document.querySelector('[id$="{$this->shortName}_button_icon"]');
 
-                        if(input_element.getAttribute('type') == 'password') {
-                          input_element.setAttribute('type', 'text');
-                          icon_element.classList.remove(['slash']);
-                        } else {
-                          input_element.setAttribute('type', 'password');
-                          icon_element.classList.add(['slash']);
-                        }
-                        EOF
+                    if(input_element.getAttribute('type') === 'password') {
+                        input_element.setAttribute('type', 'text');
+                        icon_element.classList.remove(['slash']);
+                    } else {
+                        input_element.setAttribute('type', 'password');
+                        icon_element.classList.add(['slash']);
+                    }
+                    EOF
                 )
             );
         }
