f94b9cd13819dbf7bdb65dcb689e2b7baa47af49
Rickard Osser <ricky@osser.se>
2024-08-21 00:46:24 +0200

    Test for review of changed revealEye function

---
+++ src/Form/Control/Password2.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Ui\Form\Control;
+
+use Atk4\Ui\Button;
+use Atk4\Ui\Js\JsExpression;
+use Atk4\Ui\View;
+use Atk4\Ui\Dropdown as UiDropdown;
+
+class Password2 extends Line
+{
+    public string $inputType = 'password';
+
+    /** Enable password reveal */
+    public bool $revealEye = false;
+
+    /** Reveal button */
+    private $revealEyeButton;
+
+    #[\Override]
+    protected function init(): void
+    {
+        parent::init();
+
+        if ($this->revealEye) {
+            $this->icon = 'gray eye slash link';
+        }
+    }
+
+    #[\Override]
+    protected function recursiveRender(): void
+    {
+        parent::recursiveRender();
+
+        if ($this->revealEye && !$this->disabled) {
+            $this->on( // TODO $this->action->on() call is too late here and must throw
+                'click',
+                new JsExpression(
+                    <<<'EOF'
+                        let inputElem = document.getElementById([]);
+                        let iconElem = document.getElementById([]);
+
+                        if (inputElem.getAttribute('type') === 'password') {
+                            inputElem.setAttribute('type', 'text');
+                            iconElem.classList.remove(['slash']);
+                        } else {
+                            inputElem.setAttribute('type', 'password');
+                            iconElem.classList.add(['slash']);
+                        }
+                        EOF,
+                    [
+                        $this->getHtmlId() . '_input',
+                        View::assertInstanceOf($this->elements['icon'])->getHtmlId()
+                    ]
+                )
+            );
+        }
+    }
+}
+                    
