157792ec335e3d4d8533d40475cb48bbe49f1901
Michael Voříšek <mvorisek@mvorisek.cz>
2024-09-10 18:33:32 +0200

    fix&improve cs

--- src/Form/Control/Password.php
+++ src/Form/Control/Password.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Atk4\Ui\Form\Control;
 
 use Atk4\Ui\Icon;
-use Atk4\Ui\Js\JsReload;
 use Atk4\Ui\Js\JsExpression;
-use Atk4\Ui\Js\JsBlock;
 
 class Password extends Line
 {
@@ -16,8 +14,7 @@ class Password extends Line
     /** Enable password reveal */
     public bool $revealEye = false;
 
-    /** @var Icon|array|null */
-    private $revealEyeIcon;
+    private ?Icon $revealEyeIcon;
 
     #[\Override]
     protected function init(): void
@@ -35,38 +32,30 @@ class Password extends Line
     protected function recursiveRender(): void
     {
         if ($this->revealEye) {
-            $this->revealEyeIcon = Icon::addTo(
-                $this,
-                [
-                     'grey eye link slash',
-                ],
-                [
-                    'AfterInput',
-                ]
-            );
+            $this->revealEyeIcon = Icon::addTo($this, ['grey eye link slash'], ['AfterInput']);
+
+            if (!$this->disabled) {
+                $this->revealEyeIcon->on(
+                    'click',
+                    new JsExpression(
+                        <<<'EOF'
+                            let inputElem = document.getElementById([] + '_input');
+                            let iconElem = document.getElementById([]);
+
+                            if (inputElem.getAttribute('type') === 'password') {
+                                inputElem.setAttribute('type', 'text');
+                                iconElem.classList.remove(['slash']);
+                            } else {
+                                inputElem.setAttribute('type', 'password');
+                                iconElem.classList.add(['slash']);
+                            }
+                            EOF,
+                        [$this->name, $this->revealEyeIcon->name]
+                    )
+                );
+            }
         }
 
-        if ($this->revealEye && !$this->disabled) {
-            $this->revealEyeIcon->on(
-                'click',
-                new JsExpression(
-                    <<<'EOF'
-                        let inputElem = document.getElementById([] + '_input');
-                        let iconElem = document.getElementById([]);
-                        //let iconElem = document.getElementById(iconElemName);
-
-                        if (inputElem.getAttribute('type') === 'password') {
-                            inputElem.setAttribute('type', 'text');
-                            iconElem.classList.remove(['slash']);
-                        } else {
-                            inputElem.setAttribute('type', 'password');
-                            iconElem.classList.add(['slash']);
-                        }
-                        EOF,
-                    [$this->name, $this->revealEyeIcon->name]
-                )
-            );
-        }
         parent::recursiveRender();
     }
 }
