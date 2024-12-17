10ec91c0025970b1f22c96b58d7f1fca4024b971
Michael Krecek <michael@krecek.net>
2024-12-17 12:09:53 +0100

    Example changed

--- demos/basic/menu.php
+++ demos/basic/menu.php
@@ -26,7 +26,7 @@ $dropdown->onChange(static function (string $itemId) {
 });
 
 $model = new Category($app->db);
-$dropdown2 = UiDropdown::addTo($app, ['Model with Callback', 'dropdownOptions' => ['on' => 'hover']]);
+$dropdown2 = UiDropdown::addTo($menu, ['Model with Callback', 'dropdownOptions' => ['on' => 'hover']]);
 $dropdown2->setModel($model);
 $dropdown2->onChange(static function ($itemId) {
     return new JsToast('New selected item ID: ' . $itemId);
