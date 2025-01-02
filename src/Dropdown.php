c1fc213e5b45db24c1e4214cdbb50f3dbc9fdd53
Michael Voříšek <mvorisek@mvorisek.cz>
2025-01-02 04:15:56 +0100

    use non-display typecast

--- src/Dropdown.php
+++ src/Dropdown.php
@@ -57,7 +57,7 @@ class Dropdown extends Lister
         $this->cb->set(static function (Jquery $j, $value) use ($fx) {
             return $fx($value);
         }, ['item' => new JsCallbackLoadableValue(null, function ($v) {
-            return $this->getApp()->uiPersistence->typecastLoadField(
+            return $this->getApp()->uiPersistence->typecastAttributeLoadField(
                 $this->model->getIdField(),
                 $v
             );
@@ -75,7 +75,7 @@ class Dropdown extends Lister
     #[\Override]
     protected function renderTRow(): void
     {
-        $this->tRow->set('id', $this->getApp()->uiPersistence->typecastSaveField($this->model->getIdField(), $this->currentRow->getId()));
+        $this->tRow->set('id', $this->getApp()->uiPersistence->typecastAttributeSaveField($this->model->getIdField(), $this->currentRow->getId()));
         $this->tRow->set('name', $this->getApp()->uiPersistence->typecastSaveField($this->model->getField($this->model->titleField), $this->currentRow->getTitle()));
     }
 }
