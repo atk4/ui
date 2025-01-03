11e6f4bfff664d1975f7ec371342348d430e188a
Michael Voříšek <mvorisek@mvorisek.cz>
2025-01-03 11:29:02 +0100

    x

--- src/Form.php
+++ src/Form.php
@@ -193,10 +193,11 @@ class Form extends View
     }
 
     /**
-     * @param never $model
+     * @param never             $entity
+     * @param list<string>|null $fields
      */
     #[\Override] // @phpstan-ignore method.childParameterType
-    public function setModel(Model $model): void
+    public function setModel(Model $entity, ?array $fields = null): void
     {
         throw new Exception('Use Form::setEntity() method instead for entity set');
     }
