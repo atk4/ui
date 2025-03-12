adfdf6c851dce4724b8c5459169e73758cf1188a
Michael Voříšek <mvorisek@mvorisek.cz>
2025-03-13 00:35:38 +0100

    replace POST hook in Multiline with proper setInputValue impl.

--- src/Form/Control/Multiline.php
+++ src/Form/Control/Multiline.php
@@ -212,27 +212,6 @@ class Multiline extends Form\Control
 
         $this->renderCallback = JsCallback::addTo($this);
 
-        // load the data associated with this input and validate it
-        $this->form->onHook(Form::HOOK_LOAD_POST, function (Form $form, array &$postRawData) {
-            $this->rowData = $this->typeCastLoadValues($this->getApp()->decodeJson($this->getApp()->getRequestPostParam($this->shortName)));
-            if ($this->rowData) {
-                $this->rowErrors = $this->validate($this->rowData);
-                if ($this->rowErrors) {
-                    throw new ValidationException([$this->shortName => 'multiline error']);
-                }
-            }
-
-            // remove __atml ID from array field
-            if ($this->form->entity->getField($this->shortName)->type === 'json') {
-                $rows = [];
-                foreach ($this->rowData as $cols) {
-                    unset($cols['__atkml']);
-                    $rows[] = $cols;
-                }
-                $postRawData[$this->shortName] = $this->getApp()->encodeJson($rows);
-            }
-        });
-
         // change form error handling
         $this->form->onHook(Form::HOOK_DISPLAY_ERROR, function (Form $form, $fieldName, $str) {
             // when errors are coming from this Multiline field, then notify Multiline component about them
@@ -269,6 +248,31 @@ class Multiline extends Form\Control
         }
 
         return $dataRows;
+    }
+
+    #[\Override]
+    public function setInputValue(string $value): void
+    {
+        $this->rowData = $this->typeCastLoadValues($this->getApp()->decodeJson($value));
+        if ($this->rowData) {
+            $this->rowErrors = $this->validate($this->rowData);
+            if ($this->rowErrors) {
+                throw new ValidationException([$this->shortName => 'multiline error']);
+            }
+        }
+
+        // remove __atkml ID from array field
+        if ($this->form->entity->getField($this->shortName)->type === 'json') {
+            $rows = [];
+            foreach ($this->rowData as $cols) {
+                unset($cols['__atkml']);
+                $rows[] = $cols;
+            }
+
+            $value = $this->getApp()->encodeJson($rows);
+        }
+
+        parent::setInputValue($value);
     }
 
     /**
