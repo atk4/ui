bd5962f092ae8c283b19a42a7279fa9f7b2f918b
Michael Voříšek <mvorisek@mvorisek.cz>
2025-09-15 23:05:15 +0200

    improve comment wording

--- src/HtmlTemplate/Value.php
+++ src/HtmlTemplate/Value.php
@@ -26,7 +26,7 @@ class Value
     public function set(string $value): self
     {
         if (\PHP_VERSION_ID < 8_02_00
-                ? preg_match('~~u', $value) === false // much faster in PHP 8.1 and lower
+                ? preg_match('~~u', $value) === false // much faster in PHP 8.1 or lower
                 : !mb_check_encoding($value, 'UTF-8')
         ) {
             throw new Exception('Value is not valid UTF-8');
