f2f6461a85edc9363e064ee378a2e050d7c4c704
Michael Voříšek <mvorisek@mvorisek.cz>
2025-10-17 15:20:47 +0200

    refactor - remove mlid early from row data

--- demos/form-control/multiline.php
+++ demos/form-control/multiline.php
@@ -44,9 +44,9 @@ $column = $sublayout->addColumn(4);
 $controlTotal = $column->addControl('total', ['readOnly' => true])->set($total);
 
 // update total when qty and box value in any row has changed
-$multiline->onLineChange(static function (array $rows, Form $form) use ($controlTotal) {
+$multiline->onLineChange(static function (array $rows, array $mlids, Form $form) use ($controlTotal) {
     $total = 0;
-    foreach ($rows as $row => $cols) {
+    foreach ($rows as $cols) {
         $total += $cols[MultilineItem::hinting()->fieldName()->qty] * $cols[MultilineItem::hinting()->fieldName()->box];
     }
 
