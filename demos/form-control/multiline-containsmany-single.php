27f32854fdd8edb4814f3d381ecda25ad6e1d92c
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-19 17:06:09 +0100

    Fix Multiline expression calc when there is no row (#2306)

--- demos/form-control/multiline-containsmany-single.php
+++ demos/form-control/multiline-containsmany-single.php
@@ -7,7 +7,6 @@ namespace Atk4\Ui\Demos;
 use Atk4\Ui\App;
 use Atk4\Ui\Form;
 use Atk4\Ui\Js\JsToast;
-use Doctrine\DBAL\Platforms\MySQLPlatform;
 
 /** @var App $app */
 require_once __DIR__ . '/../init-app.php';
@@ -20,19 +19,5 @@ $form->onSubmit(static function (Form $form) {
     $form->entity->save();
     $form->entity->setNull($form->entity->idField);
 
-    // fix Behat for MySQL with unordered/normalized JSON
-    $data = $form->entity->get();
-    if ($form->getApp()->db->getDatabasePlatform() instanceof MySQLPlatform) {
-        assert($form->entity instanceof MultilineDelivery);
-        $itemKey = $form->entity->fieldName()->item;
-        if ($data[$itemKey] !== null) {
-            uksort($data[$itemKey], static function ($a, $b) use ($form) {
-                $fieldNamesOrder = array_flip(array_values(array_map(static fn ($v) => $v->getPersistenceName(), $form->entity->item->getFields())));
-
-                return $fieldNamesOrder[$a] <=> $fieldNamesOrder[$b];
-            });
-        }
-    }
-
-    return new JsToast($form->getApp()->encodeJson($data));
+    return new JsToast($form->getApp()->encodeJson($form->entity->get()));
 });
