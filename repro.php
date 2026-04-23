cbe52bede857b0b2ed678433dce893f6e028ca3c
Michael Voříšek <mvorisek@mvorisek.cz>
2026-04-23 23:48:49 +0200

    ii

--- repro.php
+++ repro.php
@@ -24,6 +24,26 @@ class DemosHttpTest
     /** @var int */
     protected $port = 9687;
 
+    protected function getResponseFromRequest(string $path): array
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'http://' . $this->host . ':' . $this->port . $path);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_POST, 1);
+
+//        $headers = [];
+//        $headers[] = 'Authorization: Bearer ' . $token;
+//        $headers[] = 'Dropbox-API-Arg: {"path":"' . $in_filepath . '"}';
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        $code = curl_getinfo($ch, \CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return [$code, $result];
+    }
+
     public function setupWebserver(): void
     {
         // setup session storage
@@ -51,7 +71,7 @@ class DemosHttpTest
         while (true) {
             usleep(20_000);
             try {
-                $this->getResponseFromRequest('?ping');
+                $this->getResponseFromRequest('/demos/?ping');
 
                 break;
             } catch (ConnectException $e) {
@@ -64,22 +84,7 @@ class DemosHttpTest
 
     public function test(): void
     {
-        $path = '/demos/_unit-test/fatal-error.php?type=oom';
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, 'http://' . $this->host . ':' . $this->port . $path);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // curl_setopt($ch, CURLOPT_POST, 1);
-
-//        $headers = [];
-//        $headers[] = 'Authorization: Bearer ' . $token;
-//        $headers[] = 'Dropbox-API-Arg: {"path":"' . $in_filepath . '"}';
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        $result = curl_exec($ch);
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+        [$code, $result] = $this->getResponseFromRequest('/demos/_unit-test/fatal-error.php?type=oom');
 
         var_dump($code);
         var_dump($result);
